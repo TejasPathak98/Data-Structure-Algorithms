4b994ff18aa99f78d770acb147c44d69969d7b3f
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-10-02 00:07:30 -0700

    Time: 929 ms (24.70%) | Memory: 36.3 MB (48.93%) - LeetSync

---
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -0,0 +1,27 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        if not nums or len(nums) <= 1:
+            return False
+        if len(nums) == 2:
+            if sum(nums) % k == 0:
+                return True
+            else:
+                return False
+        
+        remainder_map = {0:-1}
+        cs_sum = 0
+
+        for i in range(len(nums)):
+            cs_sum += nums[i]
+
+            cs_sum = cs_sum % k
+            
+            if cs_sum in remainder_map:
+                if i - remainder_map[cs_sum] > 1:
+                    return True
+            else:
+                remainder_map[cs_sum] = i
+        
+        return False
+
+        
