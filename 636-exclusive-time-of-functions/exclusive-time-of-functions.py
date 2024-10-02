687f6cf99994eadf42baf9522e16270fb604c548
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-10-02 01:47:19 -0700

    Time: 76 ms (24.54%) | Memory: 16.6 MB (76.09%) - LeetSync

--- 636-exclusive-time-of-functions/exclusive-time-of-functions.py
+++ 636-exclusive-time-of-functions/exclusive-time-of-functions.py
@@ -1,22 +1,22 @@
 class Solution:
     def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
+        if n == 0:
+            return 0
         stack = deque()
-        ans = [0] * n 
+        ans = [0] * n
 
         for log in logs:
-            id,action,timestamp = log.split(":")
-            id,timestamp = int(id) , int(timestamp) 
+            id,activity,timestamp = log.split(":")
+            id = int(id)
+            timestamp = int(timestamp)
 
-            if action == 'start':
+            if activity == "start":
                 stack.append((id,timestamp))
             else:
-                id, start_time = stack.pop() 
-                elapsed_time = timestamp - start_time + 1
-                ans[id] += elapsed_time
+                id, start_time = stack.pop()
+                ans[id] += timestamp - start_time + 1
 
                 if stack:
-                    ans[stack[-1][0]] -= elapsed_time 
+                    ans[stack[-1][0]] -= timestamp - start_time + 1
         
         return ans
-
-        
