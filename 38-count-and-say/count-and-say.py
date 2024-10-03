d3509204460163d673acb2f77691d30b341cb7fe
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-10-02 18:56:00 -0700

    Time: 55 ms (5.94%) | Memory: 16.8 MB (7.99%) - LeetSync

--- 38-count-and-say/count-and-say.py
+++ 38-count-and-say/count-and-say.py
@@ -1,47 +1,8 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
         if n == 1:
-            return "1"
-
-        sequence = "1"
-        t = 0
-        fs = ""
-        
-
-        def helper(sequence):
-            nonlocal t,fs
-            t += 1
-            if t == n:
-                fs = sequence
-                return
-            
-            temp = ""
-            cnt = 1
-            i = 0
-
-            if len(sequence) == 1:
-                temp = "11"
-            else:
-                while i <= len(sequence) - 1:
-                    while i < len(sequence) - 1 and sequence[i] == sequence[i + 1]:
-                        cnt += 1
-                        i += 1
-                    temp = temp + str(cnt) + str(sequence[i])
-                    cnt = 1
-                    i += 1
-            print(temp)
-            helper(temp)
-        
-        helper(sequence)
-        return fs
-        
-
-
-
-       
-            
-            
-
-
-
-        
+            return '1'
+        return self.helper(self.countAndSay(n - 1))
+    
+    def helper(self,n:string) -> string:
+        return ''.join(f'{sum(1 for _ in gr)}{key}' for key,gr in groupby(n))
