ea0205d77443ee6090c931b1df06ae736a719d07
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-10-02 19:36:34 -0700

    Time: 23 ms (99.49%) | Memory: 17 MB (23.12%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -1,48 +1,54 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        value = 0 
-        sign = 1
-        state = 0
-        pos = 0
-
         if len(s) == 0:
-            return 0 
+            return 0
         
-        while pos < len(s):
-            ch = s[pos] 
+        n = len(s)
+        i = 0
+        temp = ""
+        neg = False
+        sign_set = False
 
-            if state == 0:
-                if ch == " ":
-                    pass 
-                elif ch == "+" or ch == "-":
-                    state = 1 
-                    sign = 1 if ch == "+" else -1 
-                elif ch.isdigit():
-                    state = 2 
-                    value = 10*value + int(ch) 
+        while i < n:
+            if s[i] == " ":
+                if temp or sign_set:
+                    break
+                i += 1
+                continue
+            elif s[i] == '-':
+                if temp or sign_set:
+                    break
                 else:
-                    return 0 
-           
-            elif state == 1:
-                if ch.isdigit():
-                    state = 2 
-                    value = 10*value + int(ch)
+                    sign_set = True
+                    neg = True
+            elif s[i] == '+':
+                if temp or sign_set:
+                    break
                 else:
-                    return 0 
-
-            elif state == 2:
-                if ch.isdigit():
-                    state = 2 
-                    value = 10*value + int(ch)
-                else:
-                    break  
+                    sign_set = True
+            elif s[i].isdigit():
+                temp = temp + s[i]
             else:
-                return 0
+                break
+            print(i)
+            i += 1
+        
+        if len(temp) == 0:
+            return 0
+
+        number = int(temp)
+        if neg:
+            number = -1 * number
+        
+        if number < 0:
+            number = max(number,-1 * (2 ** 31))
+        else:
+            number = min(number,2 ** 31 - 1)
+
+        return number
+
+                    
             
-            pos += 1
+            
 
-        value = sign * value
-        value = min(value,2**31 - 1)
-        value = max(value,-1 * (2**31)) 
-
-        return value   
+        
