2f6c431d024ca057ef0d1ed6a042ff04d9b0f03d
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-10-04 20:55:27 -0700

    Time: 103 ms (19.90%) | Memory: 17.4 MB (99.97%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -1,31 +1,29 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
         n = len(s)
-
-        st = []
+        stack = []
         i = 0
-
+        
         while i < len(s):
-            x = ord(s[i]) - ord('a')
-            if 0 <= x < 26:
+            orr = ord(s[i]) - ord('a')
+            
+            if 0 <= orr < 26:
                 i += 1
                 continue
             elif s[i] == "(":
-                st.append((s[i],i))
+                stack.append((s[i],i))
             elif s[i] == ")":
-                if st:
-                    st.pop()
+                if stack:
+                    stack.pop()
                 else:
                     s = s[:i] + s[i + 1:]
                     i -= 1
-                    
-            i +=1
+            i += 1
+        
+        for _, index in reversed(stack):
+            s = s[:index] + s[index + 1:]
 
-        while st:
-            _,j = st.pop()
-            s = s[:j] + s[j + 1:]
-            
+
         return s
-
 
         
