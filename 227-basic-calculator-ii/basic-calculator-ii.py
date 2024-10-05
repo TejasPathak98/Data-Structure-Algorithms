6780f441c729bdda5c66ac4770bb3b0e405e943b
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-10-04 21:50:40 -0700

    Time: 127 ms (32.09%) | Memory: 19.8 MB (69.58%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -1,33 +1,29 @@
 class Solution:
     def calculate(self, s: str) -> int:
         s = s.replace(" ","")
-
-        current_num = 0
-        my_stack = []
+        stack = []
         operator = "+"
+        n = len(s)
+        i = 0
+        num = 0
 
         for i,char in enumerate(s):
             if s[i].isdigit():
-                current_num = 10*current_num + int(s[i])
+                num = num*10 + int(s[i])
             
             if not s[i].isdigit() or i == len(s) - 1:
                 if operator == "+":
-                    my_stack.append(current_num)
+                    stack.append(num)
                 elif operator == "-":
-                    my_stack.append(-current_num)
+                    stack.append(-num)
                 elif operator == "*":
-                    my_stack[-1] = my_stack[-1] * current_num
+                    stack[-1] = stack[-1] * num
                 elif operator == "/":
-                    print(my_stack[-1])
-                    print(current_num)
-                    my_stack[-1] = int(my_stack[-1] / current_num)
-                    print(my_stack[-1])
-                
-                #print(my_stack)
-                
+                    stack[-1] = int(stack[-1] / num)
+            
                 operator = char
-                current_num = 0
+                num = 0
         
-        return sum(my_stack)
+        return sum(stack)
 
         
