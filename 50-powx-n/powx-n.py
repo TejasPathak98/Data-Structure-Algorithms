21e51c18320b952810611036d178568acfaeb342
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-10-04 23:00:00 -0700

    Time: 39 ms (27.05%) | Memory: 16.9 MB (19.77%) - LeetSync

---
+++ 50-powx-n/powx-n.py
@@ -0,0 +1,13 @@
+class Solution:
+    @cache
+    def myPow(self, x: float, n: int) -> float:
+        if n == -1:
+            return 1 /x
+
+        if n == 1:
+            return x
+        if n == 0:
+            return 1
+        
+        return self.myPow(x, n // 2) * self.myPow(x, n // 2) * self.myPow(x, n % 2)
+        
