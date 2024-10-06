a21e1966fbdab4ca50d01ad7aad68c3900fb3436
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-10-05 17:10:54 -0700

    Time: 382 ms (86.50%) | Memory: 16.9 MB (77.75%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -1,37 +1,30 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        n = len(grid) - 1
-        if grid[0][0] == 1 or grid[n][n] == 1:
+        n = len(grid)
+        if grid[0][0] == 1 or grid[n - 1][n - 1] == 1:
             return -1
-        if n == 0:
-            return 1
 
-        queue = deque([(0,0)])
-        level = 1
-        dir = [[-1,0],[-1,-1],[-1,1],[1,0],[1,-1],[0,1],[0,-1],[1,1]]
+        queue = deque([(0,0,1)])
+        dir = [(-1,0),(1,0),(0,1),(0,-1),(1,1),(1,-1),(-1,1),(-1,-1)]
+        visited = [[False] * n for _ in range(n)]
+        visited[0][0] = True
 
         while queue:
-
             for _ in range(len(queue)):
-                x,y = queue.popleft()
-
-                if x == n and y == n:
-                    return level
-
-                for i in range(8):
-                    dx = x + dir[i][0]
-                    dy = y + dir[i][1]
-
-                    if dx < 0 or dx > n or dy < 0 or dy > n or grid[dx][dy] != 0:
+                x,y,steps = queue.popleft()
+            
+                if x == n - 1 and y == n - 1:
+                    return steps
+                
+                for dx,dy in dir:
+                    x_,y_ = x + dx, y + dy
+                    if x_ < 0 or y_ < 0 or x_ >= n or y_ >= n or grid[x_][y_] == 1 or visited[x_][y_] == True:
                         continue
                     else:
-                        grid[dx][dy] = 2
-                        queue.append([dx,dy])
-                    
-            level +=1
+                        queue.append((x_,y_,steps + 1))
+                        visited[x_][y_] = True
         
         return -1
-                
-
+            
 
         
