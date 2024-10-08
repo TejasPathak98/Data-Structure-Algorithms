b1c8cdb019169beb583102e014095630f85dc995
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-10-08 14:06:36 -0700

    Time: 583 ms (45.61%) | Memory: 78.2 MB (49.87%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -2,59 +2,61 @@ class Node:
     def __init__(self,key,value):
         self.key = key
         self.value = value
-        self.next = None
         self.prev = None
+        self.next = None
 
 class LRUCache:
+
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = {}
-        self.head = Node(0,0)
-        self.tail = Node(0,0)
+        self.dic = dict() #key to node
+        self.head = Node(-1,-1)
+        self.tail = Node(-1,-1)
         self.head.next = self.tail
         self.tail.prev = self.head
-    
-    def add_node(self,node):
-        node.prev = self.head
-        node.next = self.head.next
-        self.head.next.prev = node
-        self.head.next = node 
-    
-    def remove_node(self,node):
-        p = node.prev 
-        n = node.next
-        p.next = n
-        n.prev = p
-    
-    def move_node(self,node):
-        self.remove_node(node)
-        self.add_node(node)
- 
-    def pop_node(self):
-        node = self.tail.prev 
-        self.remove_node(node)
-        return node        
+        
 
     def get(self, key: int) -> int:
-        if key in self.cache:
-            self.move_node(self.cache.get(key))
-            return self.cache[key].value
-        else:
+        if key not in self.dic:
             return -1
-
-    def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self.cache[key].value = value
-            self.move_node(self.cache.get(key))
         else:
+            node = self.dic[key]
+            self.removeNode(node)
+            self.addNodetoHead(node)
+            return node.value
+        
+    def put(self, key: int, value: int) -> None:
+        if key in self.dic:
+            node = self.dic[key]
+            self.removeNode(node)
+            self.addNodetoHead(node)
+            node.value = value
+        else:
+            if len(self.dic) >= self.capacity:
+                self.removefromTail()
             node = Node(key,value)
-            self.add_node(node)
-            self.cache[key] = node
-            if len(self.cache) > self.capacity:
-                temp_node = self.pop_node()
-                del self.cache[temp_node.key]
-            
+            self.dic[key] = node
+            self.addNodetoHead(node)
 
+    def removeNode(self,node):
+        node.next.prev = node.prev
+        node.prev.next = node.next
+    
+    def addNodetoHead(self,node):
+        temp = self.head.next
+        self.head.next = node
+        node.next = temp
+        node.prev = self.head
+        temp.prev = node
+    
+    def removefromTail(self):
+        if len(self.dic) == 0:
+            return
+        tail_node = self.tail.prev
+        del self.dic[tail_node.key]
+        self.removeNode(tail_node)
+
+    
 
         
 
