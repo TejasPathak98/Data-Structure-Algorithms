1251c73cc94c82170c02b6ad7c9800449e583ec7
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-10-08 15:12:30 -0700

    Time: 47 ms (99.82%) | Memory: 16.8 MB (12.30%) - LeetSync

--- 16-3sum-closest/3sum-closest.py
+++ 16-3sum-closest/3sum-closest.py
@@ -1,42 +1,41 @@
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
         nums.sort()
-        return self.Kclosest(nums,3,target)
-    
-    def Kclosest(self,nums,k,target):
+        return self.Kclosest(nums,target,3)
+
+    def Kclosest(self,nums,target,k):
         N = len(nums)
 
-        if N<k:
-            return (10**4)+1001
-
+        if N < k:
+            return 10 ** 4 + (1001)
+        
         if N == k:
-            return sum(nums[:k])
+            return sum(nums)
         
-        if sum(nums[:k]) >= target:
-            return sum(nums[:k])
-        
-        if sum(nums[-k:]) <= target:
-            return sum(nums[-k:])
-        
+        if N > k:
+            if sum(nums[:k]) >= target:
+                return sum(nums[:k])
+            if sum(nums[-k:]) <= target:
+                return sum(nums[-k:])
+
         if k == 1:
-            var = [(x,abs(x - target)) for x in nums]
-            return min(var , key = lambda x : x[1])[0]
+            var =  [(x,abs(target - x)) for x in nums]
+            return min(var,key = lambda x : x[1])[0]
+
         
         closest = sum(nums[:k])
-        for i , x in enumerate(nums):
+        
+        for i, x in enumerate(nums):
             if i > 0 and nums[i - 1] == x:
                 continue
             
-            bs = self.Kclosest(nums[i + 1:], k - 1, target - x)
-            curr = bs + x
+            bs = self.Kclosest(nums[i + 1:], target - x, k - 1)
+            t = bs + x
 
-            if abs(curr - target) < abs(target - closest):
-                if curr == target:
+            if abs(t - target) < abs(target - closest):
+                if t == target:
                     return target
-                else:
-                    closest = curr
+                closest = t
         
         return closest
-
-
         
