0ac7be482d7ca92527c653b4206a1efea40f811c
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-10-12 11:24:26 -0700

    Time: 79 ms (59.66%) | Memory: 18.2 MB (59.27%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -4,19 +4,19 @@ class Solution:
         s_list = list(s)
 
         i = 0
-        while i < len(s_list):
-            if s_list[i] == "(":
+        while i < len(s):
+            if s[i] == "(":
                 stack.append(i)
-            elif s_list[i] == ")":
+            elif s[i] == ")":
                 if stack:
                     stack.pop()
                 else:
-                    s_list = s_list[:i] + s_list[i + 1:]
+                    s = s[:i] + s[i + 1:]
                     i -= 1
             i += 1
 
         while stack:
             pos = stack.pop()
-            s_list = s_list[:pos] + s_list[pos + 1:]
+            s = s[:pos] + s[pos + 1:]
 
-        return "".join(s_list)   
+        return s   
