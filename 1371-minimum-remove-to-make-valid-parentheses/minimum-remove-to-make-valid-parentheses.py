884a00a89d0cb600d2e743b9656ebadab19b0918
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-10-12 11:21:41 -0700

    Time: 588 ms (8.60%) | Memory: 20.9 MB (5.30%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -1,24 +1,26 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
         stack = []
-        invalid_indices = set()
+        s_list = list(s)
+        print(s_list)
 
-        for i, ch in enumerate(s):
-            if ch == "(":
-                stack.append(i)
-            elif ch == ")":
+        i = 0
+        j = 0
+
+        while i < len(s_list):
+            if s_list[i] == "(":
+                stack.append((s_list[i],i))
+            elif s_list[i] == ")":
                 if stack:
                     stack.pop()
                 else:
-                    invalid_indices.add(i)
-        
-        invalid_indices.update(stack)
+                    s_list = s_list[:i] + s_list[i + 1:]
+                    i -= 1
+            i += 1
 
-        p = ""
+        while stack:
+            _, pos = stack.pop()
+            s_list = s_list[:pos] + s_list[pos + 1:]
 
-        for i in range(len(s)):
-            if i not in invalid_indices:
-                p = p + s[i]
-        
-        return p
-        
+        print(s_list)
+        return "".join(s_list)   
