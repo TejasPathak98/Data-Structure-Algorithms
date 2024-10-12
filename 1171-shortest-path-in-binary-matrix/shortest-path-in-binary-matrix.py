d3d49fa0085aa1ed1ca3b3e91c11d73441c9cf22
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-10-12 15:31:44 -0700

    Time: 507 ms (28.56%) | Memory: 17.9 MB (59.34%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -1,30 +1,37 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        
+        
         n = len(grid)
-        if grid[0][0] == 1 or grid[n - 1][n - 1] == 1:
+        m = len(grid[0])
+        queue = deque()
+        queue.append((0,0,1))
+
+        if grid[0][0] == 1 or grid[n - 1][m - 1] == 1:
             return -1
 
-        queue = deque([(0,0,1)])
-        dir = [(-1,0),(1,0),(0,1),(0,-1),(1,1),(1,-1),(-1,1),(-1,-1)]
-        visited = [[False] * n for _ in range(n)]
+        directions = [(-1,0),(1,0),(0,1),(0,-1),(1,1),(1,-1),(-1,1),(-1,-1)]
+        visited = [[[False] for _ in range(m)] for _ in range(n)]
         visited[0][0] = True
 
         while queue:
             for _ in range(len(queue)):
-                x,y,steps = queue.popleft()
-            
-                if x == n - 1 and y == n - 1:
-                    return steps
+                x,y,step = queue.popleft()
+                print(x,y,step)
+
+                if x == n - 1 and y == m - 1:
+                    return step
                 
-                for dx,dy in dir:
-                    x_,y_ = x + dx, y + dy
-                    if x_ < 0 or y_ < 0 or x_ >= n or y_ >= n or grid[x_][y_] == 1 or visited[x_][y_] == True:
+                for dx,dy in directions:
+                    rx = dx + x
+                    ry = dy + y
+
+                    if rx < 0 or rx >= n or ry < 0 or ry >= m or grid[rx][ry] == 1 or visited[rx][ry] == True:
                         continue
                     else:
-                        queue.append((x_,y_,steps + 1))
-                        visited[x_][y_] = True
+                        visited[rx][ry] = True
+                        queue.append((rx,ry,step + 1))
         
         return -1
-            
 
-        
+
