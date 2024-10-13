0d08e38a03c9d313b2cc1f320a6e6a03c821c407
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-10-13 02:59:18 -0700

    Time: 74 ms (66.14%) | Memory: 17.9 MB (57.28%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -2,41 +2,53 @@ class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         if not nums:
             return [-1,-1]
-        
         if len(nums) == 1:
             if nums[0] == target:
                 return [0,0]
             else:
                 return [-1,-1]
         
-        l = 0
-        h = len(nums) - 1
-        pos = -1
-
-        while l <= h:
-            mid = (l + h) // 2
-            if nums[mid] == target:
-                pos = mid
-                break
-            elif nums[mid] > target:
-                h = mid - 1
-            else:
-                l = mid + 1
-
-        print(mid)
+        left_pos = -1
+        right_pos = -1
         
-        if pos == -1:
-            return [-1,-1]
-        else:
-            j = pos
-            while j > 0 and nums[j] == nums[j - 1]:
-                j -= 1
-            fp = j
-            i = pos
-            while i < len(nums) - 1 and nums[i] == nums[i + 1]:
-                i += 1
-            sp = i
-            return [fp,sp]
+        def left_search():
+            nonlocal left_pos
+            l = 0
+            h = len(nums) - 1
+            while l <= h:
+                mid = (l + h) // 2
+                if nums[mid] == target:
+                    left_pos = mid
+                    h = mid - 1 
+                elif nums[mid] > target:
+                    h = mid - 1
+                else:
+                    l = mid + 1
+            return left_pos
+        
+        def right_search():
+            nonlocal right_pos
+            l = 0
+            h = len(nums) - 1
+            while l <= h:
+                mid = (l + h) // 2
+                if nums[mid] == target:
+                    right_pos = mid
+                    l = mid + 1
+                elif nums[mid] > target:
+                    h = mid - 1
+                else:
+                    l = mid + 1
+            return right_pos
+
+        left_search()
+        right_search()
+        return [left_pos,right_pos]
+        
+
+
+        
+        
 
 
 
