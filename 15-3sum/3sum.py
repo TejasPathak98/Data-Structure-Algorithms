bb12d95b5f1908026d5cbef193fcf3d885e5609d
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-10-12 18:08:56 -0700

    Time: 718 ms (47.26%) | Memory: 20.9 MB (21.10%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,37 +1,44 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        ans = []
         nums = sorted(nums)
+        ans = []
 
-        for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i - 1]:
+        if len(nums) == 3:
+            if sum(nums) == 0:
+                return [nums]
+        
+        i = 0
+        while i < len(nums) - 2:
+            if i > 0 and nums[i - 1] == nums[i]:
+                i += 1
                 continue
-            j = i + 1
-            k = len(nums) - 1
-            while j < k:
-                summ = nums[j] + nums[k] + nums[i]
-                if summ == 0:
-                    ans.append([nums[i],nums[j],nums[k]])
-                    while j < k and nums[j] == nums[j + 1]:
+            else:
+                j = i + 1
+                k = len(nums) - 1
+                
+
+                while j < k:
+                    total = nums[i] + nums[j] + nums[k]
+
+                    if total == 0:
+                        ans.append([nums[i],nums[j],nums[k]])
+
+                        while j < k and nums[j] == nums[j + 1]:
+                            j += 1
+                        
+                        while j < k and nums[k] == nums[k - 1]:
+                            k -= 1
+                    
                         j += 1
-                    
-                    while j < k and nums[k] == nums[k - 1]:
                         k -= 1
-                    
-                    j += 1
-                    k -= 1
+                    elif total < 0:
+                        j += 1
+                    else:
+                        k -= 1
+            i += 1
 
-                elif summ > 0:
-                    k -= 1
-                else:
-                    j += 1
-        
-        
         return ans
-
-
-            
+                
 
 
 
-        
