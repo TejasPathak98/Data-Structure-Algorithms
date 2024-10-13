c0daf1747f030fba983965d0d536490b7603fede
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-10-12 20:16:17 -0700

    Time: 92 ms (38.63%) | Memory: 20.8 MB (96.43%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -3,7 +3,27 @@ class Solution:
         if len(nums) == k:
             return nums
         
-        count = Counter(nums)
+        dic = defaultdict(int)
 
-        return heapq.nlargest(k,count.keys(),count.get)
+        for i in range(len(nums)):
+            dic[nums[i]] += 1
+        
+        dic = dict(sorted(dic.items(), key = lambda x : x[1],reverse=True))
+
+        ans = []
+        var = 0
+
+        for key,val in dic.items():
+            var += 1
+            if var > k:
+                break
+            else:
+                ans.append(key)
+        
+        return ans
+
+
+
+
+
         
