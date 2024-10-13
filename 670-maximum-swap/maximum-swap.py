e667ba3316b0c7b11d831919e9cf59644ce5f5f1
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-10-13 00:10:18 -0700

    Time: 35 ms (52.15%) | Memory: 16.5 MB (78.84%) - LeetSync

--- 670-maximum-swap/maximum-swap.py
+++ 670-maximum-swap/maximum-swap.py
@@ -1,31 +1,31 @@
 class Solution:
     def maximumSwap(self, num: int) -> int:
-        s = list(str(num))
-        s_sorted = sorted(s,reverse = True)
-
-        n = len(s)
-        i = 0
-
-        pos = -1
-
-        while i < n:
-            if s[i] == s_sorted[i]:
-                i += 1
-                continue
-            else:
-                pos = i
-                for j in range(n - 1,-1,-1):
-                    if s[j] == s_sorted[i]:
-                        new_pos = j
-                        break
-                s[i] , s[new_pos] = s[new_pos] , s[i]
-                break
-
-        if pos == -1:
+        if 0 <= num <= 9:
             return num
-        
-        ans = ""
+        s = str(num)
 
-        for x in s:
-            ans += x
-        return int(ans)
+        l = []
+        for i in range(len(s)):
+            l.append(s[i])
+        l_sorted = sorted(l,reverse=True)
+
+        i = 0
+        while i < len(l):
+            if l[i] == l_sorted[i]:
+                i += 1
+            else:
+                x = l_sorted[i]
+                pos = -1
+                for k in range(len(l) - 1,-1,-1):
+                    if l[k] == x:
+                        pos = k
+                        break
+                l[i] , l[pos] = l[pos] , l[i]
+                break
+        
+        return int(''.join(l))
+                
+
+        return 0
+        
+        #7632
