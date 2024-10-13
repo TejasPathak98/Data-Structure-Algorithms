509267c7c35b8063fc22b8e2577af6236dec98c5
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-10-13 02:09:45 -0700

    Time: 220 ms (94.94%) | Memory: 18.9 MB (85.85%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -13,33 +13,44 @@ class Solution:
         visited = [[False for _ in range(m)] for _ in range(n)]
         ans = 0
 
-        def bfs(x,y):
-            nonlocal visited
-            visited[x][y] = True
-            queue = deque()
-            queue.append((x,y))
+        # def bfs(x,y):
+        #     nonlocal visited
+        #     visited[x][y] = True
+        #     queue = deque()
+        #     queue.append((x,y))
 
-            while queue:
-                x1,y1 = queue.popleft()
+        #     while queue:
+        #         x1,y1 = queue.popleft()
 
-                for dx,dy in dir:
-                    rx = dx + x1
-                    ry = dy + y1
+        #         for dx,dy in dir:
+        #             rx = dx + x1
+        #             ry = dy + y1
 
-                    if rx < 0 or rx >= n or ry < 0 or ry >=m or grid[rx][ry] == "0" or visited[rx][ry] == True:
-                        continue
-                    else:
-                        visited[rx][ry] = True
-                        queue.append((rx,ry))
+        #             if rx < 0 or rx >= n or ry < 0 or ry >=m or grid[rx][ry] == "0" or visited[rx][ry] == True:
+        #                 continue
+        #             else:
+        #                 visited[rx][ry] = True
+        #                 queue.append((rx,ry))
+
+        def dfs(x,y):
+            grid[x][y] = "0"
+
+            for dx,dy in dir:
+                rx = dx + x
+                ry = dy + y
+
+                if 0 <= rx < n and 0 <= ry < m and grid[rx][ry] == "1":
+                    dfs(rx,ry)
 
         for i in range(n):
             for j in range(m):
-                if grid[i][j] == "1" and visited[i][j] == False:
-                    bfs(i,j)
+                if grid[i][j] == "1":  #and visited[i][j] == False:
+                    dfs(i,j)
                     ans += 1
 
         return ans
 
+        #O(nm) ; O(nm)
         
             
 
