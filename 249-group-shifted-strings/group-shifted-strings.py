d2f3e83237775aefe6a92817174b682e64923495
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-10-13 12:26:48 -0700

    Time: 39 ms (58.25%) | Memory: 16.6 MB (90.22%) - LeetSync

---
+++ 249-group-shifted-strings/group-shifted-strings.py
@@ -0,0 +1,34 @@
+class Solution:
+    def groupStrings(self, strings: List[str]) -> List[List[str]]:
+        n = len(strings)
+        if n == 1:
+            return [strings[0]]
+        
+        dic = defaultdict(list)
+        ans = []
+        t = []
+
+        for st in strings:
+            if len(st) == 1:
+                t.append(st)
+            else:
+                i = 0
+                temp = []
+                while i <= len(st) - 2:
+                    diff = abs(ord(st[i + 1]) - ord(st[i]) + 26) % 26
+                    temp.append(diff)
+                    i += 1
+                dic[tuple(temp)].append(st)
+        
+        for key,v in dic.items():
+            ans.append(v)
+        
+        if t:
+            ans.append(t)
+        
+        return ans
+        
+        
+
+
+        
