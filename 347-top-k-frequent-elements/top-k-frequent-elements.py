38e59165579ef6dc35aec0f34dc7a46e834fac05
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-10-12 20:20:46 -0700

    Time: 91 ms (44.13%) | Memory: 21.2 MB (70.23%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,29 +1,9 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        if len(nums) == k:
+        if k == len(nums):
             return nums
         
-        dic = defaultdict(int)
+        c = Counter(nums)
 
-        for i in range(len(nums)):
-            dic[nums[i]] += 1
-        
-        dic = dict(sorted(dic.items(), key = lambda x : x[1],reverse=True))
-
-        ans = []
-        var = 0
-
-        for key,val in dic.items():
-            var += 1
-            if var > k:
-                break
-            else:
-                ans.append(key)
-        
-        return ans
-
-
-
-
-
+        return heapq.nlargest(k, c.keys(),key = c.get)
         
