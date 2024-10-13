2b97c788ff8df17cee864c540f1ca969ae1f1eaa
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-10-13 02:04:46 -0700

    Time: 230 ms (82.51%) | Memory: 18.9 MB (85.85%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,40 +1,46 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        m = len(grid)
-        n = len(grid[0])
+        n = len(grid)
+        m = len(grid[0])
+
+        if m == 1 and n == 1:
+            if grid[0][0] == "1":
+                return 1
+            else:
+                return 0
+        
+        dir = [(-1,0),(1,0),(0,1),(0,-1)]
+        visited = [[False for _ in range(m)] for _ in range(n)]
         ans = 0
 
-        if not grid:
-            return 0
-
-        dir = [[-1,0],[0,1],[1,0],[0,-1]]
-
         def bfs(x,y):
-            queue = deque([(x,y)])
+            nonlocal visited
+            visited[x][y] = True
+            queue = deque()
+            queue.append((x,y))
 
             while queue:
-                s,t = queue.popleft()
-                for i in range(4):
-                    dx = s + dir[i][0]
-                    dy = t + dir[i][1]
+                x1,y1 = queue.popleft()
 
-                    if dx < 0 or dx >= m or dy < 0 or dy >= n or grid[dx][dy] != "1":
+                for dx,dy in dir:
+                    rx = dx + x1
+                    ry = dy + y1
+
+                    if rx < 0 or rx >= n or ry < 0 or ry >=m or grid[rx][ry] == "0" or visited[rx][ry] == True:
                         continue
                     else:
-                        grid[dx][dy] = 2
-                        queue.append([dx,dy])
+                        visited[rx][ry] = True
+                        queue.append((rx,ry))
 
-
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == "1":
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == "1" and visited[i][j] == False:
                     bfs(i,j)
                     ans += 1
-                    print("Hi")
-        
+
         return ans
 
-
-
+        
+            
 
         
