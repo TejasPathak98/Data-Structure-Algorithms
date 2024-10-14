911927ac6414873a691b0d5369569681575594cc
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-10-13 17:03:24 -0700

    Time: 44 ms (7.64%) | Memory: 16.7 MB (10.97%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.py
+++ 807-custom-sort-string/custom-sort-string.py
@@ -1,22 +1,28 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
-        ans = ""
-        
-        count = Counter(s)
-        my_set = set()
+        n = len(s)
+        m = len(order)
+        pos = []
+        t = ""
 
-        for ch in order:
-            if ch in s:
-                freq = count.get(ch)
-                ans = ans + ch * freq
-                my_set.add(ch)
+        for i in range(m):
+            indices = [j for j,c in enumerate(s) if s[j] == order[i]]
+            if indices != [-1]:
+                pos.extend(indices)
         
-        for ch in s:
-            if ch not in my_set:
-                ans = ans + ch
-        
-        return ans
+        print(pos)
 
+        for i in range(len(pos)):
+            t = t + s[pos[i]]
+        
+        for i in range(n):
+            if i not in pos:
+                t = t + s[i]
+        
+        return t
+        
+        
+        
 
 
         
