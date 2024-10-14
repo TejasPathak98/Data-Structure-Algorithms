56abae2e81a28558746b97348e3e37c6cd0eda35
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-10-13 17:16:35 -0700

    Time: 28 ms (91.98%) | Memory: 16.5 MB (80.65%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.py
+++ 807-custom-sort-string/custom-sort-string.py
@@ -1,28 +1,17 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
-        n = len(s)
-        m = len(order)
-        pos = []
-        t = ""
-        l_ = list(range(n))
+        ans = []
 
-        for i in range(m):
-            indices = [j for j,c in enumerate(s) if s[j] == order[i]]
-            if indices != [-1]:
-                pos.extend(indices)
+        sMap = defaultdict(int)
 
-        for i in range(len(pos)):
-            t = t + s[pos[i]]
+        for c in s:
+            sMap[c] += 1
+        for c in order:
+            ans.append(c * sMap[c])
+            del sMap[c]
         
-        l = list(set(l_) - set(pos))
-
-        for i in range(len(l)):
-            t = t + s[l[i]]
-
-        return t
+        for c,f in sMap.items():
+            ans.append(f*c)
         
-        
-        
-
-
+        return ''.join(ans)
         
