8a7d765fadc75672f0ce8cfd3b29e4ef1fc65ac5
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-10-16 00:40:46 -0700

    Time: 775 ms (34.33%) | Memory: 20.8 MB (41.81%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,44 +1,45 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums = sorted(nums)
-        ans = []
+        if len(nums) <= 2:
+            return []
 
         if len(nums) == 3:
             if sum(nums) == 0:
                 return [nums]
+            else:
+                return []
         
         i = 0
+        ans = []
         while i < len(nums) - 2:
-            if i > 0 and nums[i - 1] == nums[i]:
+            if i > 0 and nums[i] == nums[i - 1]:
                 i += 1
                 continue
-            else:
-                j = i + 1
-                k = len(nums) - 1
+            j = i + 1
+            k = len(nums) - 1
+
+            while j < k:
+                total = nums[i] + nums[j] + nums[k]
+
+                if total == 0:
+                    ans.append([nums[i],nums[j],nums[k]])
                 
-
-                while j < k:
-                    total = nums[i] + nums[j] + nums[k]
-
-                    if total == 0:
-                        ans.append([nums[i],nums[j],nums[k]])
-
-                        while j < k and nums[j] == nums[j + 1]:
-                            j += 1
-                        
-                        while j < k and nums[k] == nums[k - 1]:
-                            k -= 1
+                    while j < len(nums) - 1  and nums[j] == nums[j + 1]:
+                        j += 1
                     
-                        j += 1
+                    while k > 0 and nums[k] == nums[k - 1]:
                         k -= 1
-                    elif total < 0:
-                        j += 1
-                    else:
-                        k -= 1
+                    
+                    j += 1
+                    k -= 1
+                elif total > 0:
+                    k -= 1
+                else:
+                    j += 1
             i += 1
-
-        return ans
                 
+        return ans
 
 
 
