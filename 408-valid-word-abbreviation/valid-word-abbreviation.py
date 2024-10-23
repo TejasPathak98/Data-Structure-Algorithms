0cb94a2ad7345cf3d1226d94c2df928e00341232
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-10-22 22:45:59 -0700

    Time: 3 ms (91.71%) | Memory: 16.5 MB (87.85%) - LeetSync

--- 408-valid-word-abbreviation/valid-word-abbreviation.py
+++ 408-valid-word-abbreviation/valid-word-abbreviation.py
@@ -3,12 +3,6 @@ class Solution:
         if len(abbr) > len(word):
             return False
         
-        if len(word) == 1:
-            if abbr == '1' or abbr == word:
-                return True
-            else:
-                return False
-
         i = 0
         j = 0
 
@@ -16,25 +10,21 @@ class Solution:
             if word[i] == abbr[j]:
                 i += 1
                 j += 1
-                continue
-            elif abbr[j].isdigit():
-                if int(abbr[j]) == 0:
+            else:
+                if abbr[j].isnumeric():
+                    if int(abbr[j]) == 0:
+                        return False
+                    val = 0
+                    while j < len(abbr) and abbr[j].isnumeric():
+                        val = val*10 + int(abbr[j])
+                        j += 1
+                    i = i + val
+                else:
                     return False
-                x = 0
-                while  j < len(abbr) and abbr[j].isdigit():
-                    x = x*10 + int(abbr[j])
-                    j += 1
-                i = i + x
-                if i > len(word):
-                    return False
-            elif word[i] != abbr[j] and not word[i].isdigit() and not abbr[j].isdigit():
-                return False
         
         if i == len(word) and j == len(abbr):
             return True
         else:
             return False
-            
-
-
+                    
         
