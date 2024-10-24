703a405b911caaa4dadab0ae5a97d309595a1fad
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-10-23 19:40:19 -0700

    Time: 57 ms (93.66%) | Memory: 33.9 MB (77.68%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,18 +1,16 @@
 class Solution:
-    from functools import cache
-    @cache
     def partition(self, s: str) -> List[List[str]]:
         if not s:
-            return [[]]
+            return []
         
-        ans = []
-        
-        for i in range(1,len(s) + 1):
-            if s[:i] == s[:i][::-1]:
-                for suf in self.partition(s[i:]):
-                    ans.append([s[:i]] + suf)
-    
-        return ans
+        def backtracking(start,path):
+            if start == len(s):
+                result.append(path)
+                return
+            for end in range(start + 1,len(s) + 1):
+                if s[start:end] == s[start:end][::-1]:
+                    backtracking(end, path + [s[start:end]])
 
-        
-        
+        result = []
+        backtracking(0,[])
+        return result
