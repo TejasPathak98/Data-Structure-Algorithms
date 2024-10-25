0574ca15ead7602698e4bb39478d7a0b5b24ae31
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-10-25 07:54:50 -0700

    Time: 1 ms (97.87%) | Memory: 16.5 MB (71.31%) - LeetSync

--- 93-restore-ip-addresses/restore-ip-addresses.py
+++ 93-restore-ip-addresses/restore-ip-addresses.py
@@ -3,17 +3,20 @@ class Solution:
         ans = []
         self.helper(s,ans,0,"",0)
         return ans
-
+    
     def helper(self,curr,ans,index,temp,count):
         if count > 4:
             return
         if count == 4 and index == len(curr):
             ans.append(temp)
         for i in range(1,4):
+            s = curr[index:index + i]
             if index + i > len(curr):
                 break
-            s = curr[index:index + i]
             if (s[0] == "0" and len(s) > 1) or (len(s) == 3 and int(s) >= 256):
                 continue
-            self.helper(curr, ans, index + i, temp + s + ("." if count < 3 else ""), count + 1)
+            self.helper(curr,ans,index + i,temp + s + ("." if count < 3 else ""),count + 1)
+
+
+
         
