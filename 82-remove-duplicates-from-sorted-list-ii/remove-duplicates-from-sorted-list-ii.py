87c1c8d7ff91286bb3fb79afff0f58bd7e53b144
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-10-25 20:21:08 -0700

    Time: 0 ms (100.00%) | Memory: 16.8 MB (18.90%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.py
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.py
@@ -8,38 +8,16 @@ class Solution:
         if not head or not head.next:
             return head
         
-        my_set = set()
-        my_set_2 = set()
+        output = ListNode(-1,head)
+        pre = output
 
-        temp = head
-        temp2 = head
-
-        while temp:
-            if temp.val not in my_set:
-                my_set.add(temp.val)
+        while head:
+            if head.next and head.val == head.next.val:
+                while head.next and head.val == head.next.val:
+                    head = head.next
+                pre.next = head.next
             else:
-                my_set_2.add(temp.val)
-            temp = temp.next
+                pre = pre.next
+            head = head.next
         
-        print(my_set_2)
-
-        prev = temp2
-        curr = temp2.next
-        while curr:
-            if curr.val in my_set_2:
-                prev.next = curr.next
-                curr = curr.next
-            else:
-                curr = curr.next
-                prev = prev.next
-        
-        if temp2.val in my_set_2:
-            return temp2.next
-        else:
-            return temp2
-        
-
-                
-
-        
-        
+        return output.next
