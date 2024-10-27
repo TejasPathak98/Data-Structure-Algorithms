ef940a1ef4979bac77b2b2c02464c8c596320221
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-10-26 17:04:13 -0700

    Time: 99 ms (29.30%) | Memory: 20.9 MB (19.36%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -1,27 +1,34 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        s = s.replace(" ","")
-        op = "+"
+        while " " in s:
+            s = s.replace(" ","")
+        
         stack = []
-        num = 0
+        op = "+"
+        i = 0
+        number = 0
+        operators = ["+","-","/","*"]
 
-        for i , char in enumerate(s):
+        while i < len(s):
             if s[i].isdigit():
-                num = num*10 + int(s[i])
-            
-            if s[i] in ["+","-","*","/"] or i == len(s) - 1:
+                number = number*10 + int(s[i])
+            if s[i] in operators or i == len(s) - 1:
                 if op == "+":
-                    stack.append(num)
+                    stack.append(number)
                 elif op == "-":
-                    stack.append(-num)
+                    stack.append(-number)
                 elif op == "*":
-                    stack[-1] = stack[-1] * num
+                    stack[-1] = stack[-1] * number
                 elif op == "/":
-                    stack[-1] = int(stack[-1]/ num)
-                
-                num = 0
+                    stack[-1] = int(stack[-1]/ number)
                 op = s[i]
+                number = 0
+            i += 1
 
+        
         return sum(stack)
+
+
+                
 
         
