502d0648e32080f1ffe0b84a22af829e1c46af32
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-10-26 17:57:14 -0700

    Time: 0 ms (100.00%) | Memory: 16.8 MB (7.74%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,17 +1,15 @@
 class Solution:
     @cache
     def myPow(self, x: float, n: int) -> float:
-        if n == 0:
-            return 1
-        if n == 1:
-            return x
-        if n == -1:
-            return 1 / x
-        
+        if x == 1:return 1
+        if x == 0:return 0
+        if n == 1:return x
+        if n == 0:return 1
+        if n == -1:return 1 / x 
+
         half = self.myPow(x, n // 2)
 
-        if n % 2 == 0:
-            return half * half
-        else:
-            return half * half * self.myPow(x, n % 2)
+        return half * half * self.myPow(x, n % 2)
+
+
         
