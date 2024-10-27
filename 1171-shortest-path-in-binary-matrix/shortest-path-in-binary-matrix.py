7e5eaa8bd44841a33b9954e41258453ad6faf0d3
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-10-26 17:20:56 -0700

    Time: 197 ms (48.94%) | Memory: 18.4 MB (8.39%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -1,37 +1,37 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        
-        
         n = len(grid)
         m = len(grid[0])
-        queue = deque()
-        queue.append((0,0,1))
 
         if grid[0][0] == 1 or grid[n - 1][m - 1] == 1:
             return -1
-
-        directions = [(-1,0),(1,0),(0,1),(0,-1),(1,1),(1,-1),(-1,1),(-1,-1)]
-        visited = [[[False] for _ in range(m)] for _ in range(n)]
-        visited[0][0] = True
-
+        
+        queue = deque()
+        queue.append((0,0))
+        dir = [(0,1),(0,-1),(1,0),(-1,0),(1,1),(1,-1),(-1,1),(-1,-1)]
+        visited = set()
+        visited.add((0,0))
+        path = 0
+        
         while queue:
-            for _ in range(len(queue)):
-                x,y,step = queue.popleft()
-                print(x,y,step)
+            size = len(queue)
+            path += 1
+            for _ in range(size):
+                x,y = queue.popleft()
 
                 if x == n - 1 and y == m - 1:
-                    return step
-                
-                for dx,dy in directions:
-                    rx = dx + x
-                    ry = dy + y
+                    return path
 
-                    if rx < 0 or rx >= n or ry < 0 or ry >= m or grid[rx][ry] == 1 or visited[rx][ry] == True:
-                        continue
-                    else:
-                        visited[rx][ry] = True
-                        queue.append((rx,ry,step + 1))
-        
+                for dx,dy in dir:
+                    new_x = x + dx
+                    new_y = y + dy
+
+                    if 0 <= new_x < n and 0 <= new_y < m and grid[new_x][new_y] == 0 and (new_x,new_y) not in visited:
+                        queue.append((new_x,new_y))
+                        visited.add((new_x,new_y))
+
         return -1
 
 
+
+        
