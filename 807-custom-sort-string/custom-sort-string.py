05217f5ec0dd233c718227cb1f2e878efe3b8511
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-10-26 20:08:52 -0700

    Time: 0 ms (100.00%) | Memory: 16.7 MB (15.09%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.py
+++ 807-custom-sort-string/custom-sort-string.py
@@ -1,17 +1,18 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
         ans = []
-
         sMap = defaultdict(int)
 
         for c in s:
             sMap[c] += 1
+        
         for c in order:
             ans.append(c * sMap[c])
             del sMap[c]
         
-        for c,f in sMap.items():
-            ans.append(f*c)
+        for c,freq in sMap.items():
+            ans.append(c * freq)
         
         return ''.join(ans)
+
         
