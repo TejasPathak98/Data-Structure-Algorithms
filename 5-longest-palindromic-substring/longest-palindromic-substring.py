77a97decc9287b48dc6155a502a872bf455540ea
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-10-27 23:46:42 -0700

    Time: 301 ms (54.06%) | Memory: 16.6 MB (80.92%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,28 +1,27 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        n = len(s)
-        if n == 1:
+        if len(s) <= 1:
             return s
-        max_s = ""
-        max_l = 0
+        
+        max_ans = ""
+        max_len = 0
 
         def helper(i,j):
-            nonlocal max_l,max_s
-            while i >= 0 and j < len(s):
-                if s[i] == s[j]:
-                    if j - i + 1 > max_l:
-                        max_s = s[i:j + 1]
-                        max_l = j - i + 1
-                    i -= 1
-                    j += 1
-                else:
-                    break
-        
-        for i in range(0,len(s) - 1):
-            helper(i,i)
-            helper(i,i+1)
+            nonlocal max_ans,max_len
+            while i>= 0 and j < len(s) and s[i] == s[j]:
+                if j - i + 1 > max_len:
+                    max_len = j - i + 1
+                    max_ans = s[i:j + 1]
+                i -= 1
+                j += 1
 
-        return max_s
+        i = 0
+        while i < len(s) - 1:
+            helper(i,i)
+            helper(i,i + 1)
+            i += 1
+        
+        return max_ans
 
         
 
