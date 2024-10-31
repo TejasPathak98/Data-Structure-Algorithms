cc09d6a90b0678118261071eeae7c49cfc985dbb
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-10-31 16:54:11 -0700

    Time: 3 ms (69.73%) | Memory: 16.8 MB (17.57%) - LeetSync

---
+++ 139-word-break/word-break.py
@@ -0,0 +1,23 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        def construct(current,wordDict,memo = {}):
+            if current in memo:
+                return memo[current]
+            
+            if not current:
+                return True
+            
+            for word in wordDict:
+                if current.startswith(word):
+                    if construct(current[len(word):],wordDict,memo):
+                        memo[current] = True
+                        return True
+            
+            memo[current] = False
+            return False
+        
+        return construct(s,wordDict)
+
+
+
+        
