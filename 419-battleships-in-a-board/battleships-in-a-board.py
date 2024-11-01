f777813e0dbe4ac54aa531af87d95accf93621be
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-10-31 20:03:07 -0700

    Time: 1 ms (66.33%) | Memory: 19.1 MB (14.01%) - LeetSync

--- 419-battleships-in-a-board/battleships-in-a-board.py
+++ 419-battleships-in-a-board/battleships-in-a-board.py
@@ -3,10 +3,9 @@ class Solution:
         count = 0
 
         for i, row in enumerate(board):
-            for j,cell in enumerate(row):
+            for j, cell in enumerate(row):
                 if cell == "X":
-                    if (i == 0 or board[i-1][j] == ".") and (j == 0 or board[i][j - 1] == "."):
+                    if (i == 0 or board[i - 1][j] == ".") and (j == 0 or board[i][j - 1] == "."):
                         count += 1
         
         return count
-        
