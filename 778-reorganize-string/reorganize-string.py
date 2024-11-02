af0999f9c5145a0a1a65998bcb645f42093c699c
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-11-02 15:05:11 -0700

    Time: 4 ms (22.86%) | Memory: 16.9 MB (9.44%) - LeetSync

--- 778-reorganize-string/reorganize-string.py
+++ 778-reorganize-string/reorganize-string.py
@@ -1,27 +1,35 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
         c = Counter(s)
-        c = sorted(c.items(),key = lambda x : x[1],reverse = True)
+        print(c)
+        c = sorted(c.items(),key = lambda x : x[1],reverse= True)
 
         res = []
         for k,val in c:
-            res.extend([k] * val)
+            res.extend([val * k])
         
-        res = deque(res)
+        st = ''.join(res)
+        
         arr = [None] * len(s)
         i = 0
+        j = 0
 
-        while res:
+        while j < len(st):
             if i > len(s) - 1:
                 i = 1
-            arr[i] = res[0]
-            res.popleft()
+            arr[i] = st[j]
             i += 2
+            j += 1
+        
+        print(arr)
         
         for i in range(1,len(arr)):
             if arr[i] == arr[i - 1]:
                 return ""
-        return "".join(arr)
+        return ''.join(arr)
 
 
+
+
+        return ""
         
