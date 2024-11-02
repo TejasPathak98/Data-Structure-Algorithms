d6f0e3f23164e8626f14565341a9bc761d8b826f
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-11-02 14:42:11 -0700

    Time: 83 ms (61.67%) | Memory: 36.1 MB (87.71%) - LeetSync

--- 1539-diagonal-traverse-ii/diagonal-traverse-ii.py
+++ 1539-diagonal-traverse-ii/diagonal-traverse-ii.py
@@ -3,19 +3,22 @@ class Solution:
         n = len(nums)
         m = 0
 
-        for i in range(n):
-            m = max(m,len(nums[i]))
-        ans = [[] for _ in range(n + m - 1)]
+        for i in nums:
+            m = max(m,len(i))
+        
+        arr = [[] for _ in range(n + m - 1)]
 
-        for i in range(n):
+        for i in range(len(nums)):
             for j in range(len(nums[i])):
-                ans[i + j].append(nums[i][j])
+                arr[i + j].append(nums[i][j])
         
-        sol = []
+        ans = []
+        for v in arr:
+            ans.extend(v[::-1])
+        
+        return ans
 
-        for v in ans:
-            sol.extend(v[::-1])
+
         
-        return sol
 
         
