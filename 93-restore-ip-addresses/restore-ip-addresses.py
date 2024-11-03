ce66a84a33a2851e0c0b99befeb17173680bf50d
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-11-02 18:01:08 -0700

    Time: 3 ms (64.72%) | Memory: 16.7 MB (39.95%) - LeetSync

--- 93-restore-ip-addresses/restore-ip-addresses.py
+++ 93-restore-ip-addresses/restore-ip-addresses.py
@@ -1,22 +1,23 @@
 class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
         ans = []
-        self.helper(s,ans,0,"",0)
+        self.helper(ans,s,0,"",0)
         return ans
     
-    def helper(self,curr,ans,index,temp,count):
+    def helper(self,ans,s,index,temp,count):
         if count > 4:
             return
-        if count == 4 and index == len(curr):
+        elif count == 4 and index == len(s):
             ans.append(temp)
-        for i in range(1,4):
-            s = curr[index:index + i]
-            if index + i > len(curr):
-                break
-            if (s[0] == "0" and len(s) > 1) or (len(s) == 3 and int(s) >= 256):
-                continue
-            self.helper(curr,ans,index + i,temp + s + ("." if count < 3 else ""),count + 1)
+            return
+        else:
+            for i in range(1,4):
+                curr = s[index:index + i]
+                if index + i > len(s):
+                    break
+                if (curr[0] == "0" and len(curr) > 1) or (len(curr) == 3 and int(curr) >= 256):
+                    continue
+                self.helper(ans,s,index + i,temp + curr + ("." if count < 3 else ""),count + 1)
 
-
-
+                
         
