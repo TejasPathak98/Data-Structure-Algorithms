81de66816fa9136d487de74d1e391ed1ef490c19
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-11-02 17:32:35 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (81.93%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.py
+++ 658-find-k-closest-elements/find-k-closest-elements.py
@@ -10,11 +10,12 @@ class Solution:
             elif x >= arr[mid + k]:
                 low = mid + 1
             else:
-                d1 = abs(arr[mid] - x)
-                d2 = abs(arr[mid + k] - x)
+                d1 = abs(x - arr[mid])
+                d2 = abs(x - arr[mid + k])
                 if d1 <= d2:
                     high = mid
                 else:
                     low = mid + 1
-        
+            
         return arr[low:low + k]
+        
