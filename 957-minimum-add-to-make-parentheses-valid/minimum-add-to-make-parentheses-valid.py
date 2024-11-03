9de67ef894d95bedc2bc62fb9b4e8ce4dd724623
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-11-02 20:25:44 -0700

    Time: 3 ms (14.90%) | Memory: 16.7 MB (11.79%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.py
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.py
@@ -1,6 +1,20 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
-        while "()" in s:
-            s = s.replace("()","")
-        return len(s)
+        stack = []
+        i = 0
+        ans = 0
+
+        while i < len(s):
+            if s[i] == "(":
+                stack.append(i)
+                i += 1
+            else:
+                if stack:
+                    stack.pop()
+                else:
+                    ans += 1
+                i += 1
         
+        return ans + len(stack)
+
+
