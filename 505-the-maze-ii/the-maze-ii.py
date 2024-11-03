e282aac64693be4119a729ab17b6d8408a2c8d8b
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-11-03 11:41:53 -0800

    Time: 55 ms (37.01%) | Memory: 17.6 MB (20.22%) - LeetSync

--- 505-the-maze-ii/the-maze-ii.py
+++ 505-the-maze-ii/the-maze-ii.py
@@ -3,42 +3,36 @@ class Solution:
         n = len(maze)
         m = len(maze[0])
 
-        visited = {}
+        visited = set()
         start = tuple(start)
         destination = tuple(destination)
+        heap = [(0,start)]
         directions = [(0,1),(0,-1),(1,0),(-1,0)]
 
-        queue = deque([(0,start)])
+        while heap:
+            dist,coordinate = heapq.heappop(heap)
 
-        while queue:
-            size = len(queue)
-            for _ in range(size):
-                dist , coordinate = queue.popleft()
-
-                if coordinate in visited and visited[coordinate] <= dist:
-                    continue
-                
-                visited[coordinate] = dist
-                
-
-                for direction in directions:
-                    i,j = coordinate
-                    new_dist = 0
-                    while (0 <= (direction[0] + i) < n) and (0 <= (direction[1] + j) < m) and maze[i + direction[0]][j + direction[1]] == 0:
-                        i += direction[0]
-                        j += direction[1]
-                        new_dist += 1
-
-                    queue.append((new_dist + dist,(i,j)))
-
-        
-        if destination in visited:
-            return visited[destination]
-        else:
-            return -1
-
-
+            if coordinate == destination:
+                return dist
             
+            if coordinate in visited:
+                continue
+            
+            visited.add(coordinate)
+
+            for direction in directions:
+                new_dist = 0
+                i,j = coordinate
+
+                while (0 <= i + direction[0] < n) and (0 <= j + direction[1] < m) and (maze[i + direction[0]][j + direction[1]] == 0):
+                    i += direction[0]
+                    j += direction[1]
+                    new_dist += 1
+
+                heapq.heappush(heap,(new_dist + dist,(i,j)))
+        
+
+        return -1
 
 
         
