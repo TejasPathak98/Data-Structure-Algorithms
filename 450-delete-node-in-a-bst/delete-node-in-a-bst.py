0934b36788eb2f27ceb4a95d53bed091148455c1
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-11-03 18:39:25 -0800

    Time: 0 ms (100.00%) | Memory: 20.4 MB (17.09%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.py
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.py
@@ -11,24 +11,20 @@ class Solution:
         
         if key < root.val:
             root.left = self.deleteNode(root.left, key)
+            return root
         elif key > root.val:
             root.right = self.deleteNode(root.right, key)
+            return root
         else:
-            if root.left is None and root.right is None:
-                return None
-            elif root.left is None:
+            if root.left is None:
                 return root.right
             elif root.right is None:
                 return root.left
             else:
-                root.val = self.findmin(root.right)
+                x = root.right
+                while x.left:
+                    x = x.left
+                root.val = x.val
                 root.right = self.deleteNode(root.right, root.val)
-        
-        return root
-
-    def findmin(self,root):
-        while root.left:
-            root = root.left
-        return root.val
-
+                return root
         
