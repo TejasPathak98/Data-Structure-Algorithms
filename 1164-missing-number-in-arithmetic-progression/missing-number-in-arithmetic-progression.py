443a8858fc40ac8b6dc144be25b3f960dcca8d1f
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-11-21 13:53:35 -0800

    Time: 4 ms (5.41%) | Memory: 17 MB (12.89%) - LeetSync

---
+++ 1164-missing-number-in-arithmetic-progression/missing-number-in-arithmetic-progression.py
@@ -0,0 +1,53 @@
+class Solution:
+    def missingNumber(self, arr: List[int]) -> int:
+        n = len(arr)
+        diff = []
+
+        if all(x == arr[0] for x in arr):
+            return arr[0]
+
+        for i in range(1,n):
+            diff.append(abs(arr[i] - arr[i - 1]))
+        
+        the_diff = float('inf')
+        min_diff = float('inf')
+        max_diff = float('-inf')
+        f = Counter(diff)
+
+        print("Bye")
+        
+        for x,freq in f.items():
+            if freq >= 2:
+                the_diff = x
+                break
+            max_diff = max(max_diff,x)
+            min_diff = min(min_diff,x)
+
+        print(the_diff)
+        print("Hi")
+        
+        if the_diff != float('inf'):
+            print("Br1")
+            for i in range(1,n):
+                print("Br")
+                if abs(arr[i] - arr[i - 1]) > the_diff:
+                    if arr[i] > arr[i - 1]:
+                        print(the_diff)
+                        return arr[i - 1] + the_diff
+                    else:
+                        return arr[i - 1] - the_diff
+        else:
+            print("Br4")
+            for i in range(1,n):
+                if abs(arr[i] - arr[i - 1]) == max_diff:
+                    if arr[i] > arr[i - 1]:
+                        return arr[i - 1] + min_diff
+                    else:
+                        return arr[i - 1] - min_diff
+
+
+
+        
+
+
+        
