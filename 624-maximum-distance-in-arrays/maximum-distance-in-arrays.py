afc97aa95f52b2cf346fa416b6474d74c8236406
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-12-11 19:01:47 -0800

    Time: 68 ms (45.99%) | Memory: 33.1 MB (56.11%) - LeetSync

--- 624-maximum-distance-in-arrays/maximum-distance-in-arrays.py
+++ 624-maximum-distance-in-arrays/maximum-distance-in-arrays.py
@@ -1,57 +1,14 @@
 class Solution:
     def maxDistance(self, arrays: List[List[int]]) -> int:
-        min_arr = []
-        max_arr = []
+        smallest = arrays[0][0]
+        biggest = arrays[0][-1]
+        max_distance = float(-inf)
+
+        for i in range(1,len(arrays)):
+            arr = arrays[i]
+            max_distance = max(max_distance,abs(smallest - arr[-1]), abs(biggest - arr[0]))
+            smallest = min(smallest,arr[0])
+            biggest = max(biggest,arr[-1])
         
-        # for arr in arrays:
-        #     min_arr.append(arr[0])
-        #     max_arr.append(arr[len(arr) - 1])
-        
-        # ans = float(-inf)
-
-        # for i in range(len(min_arr)):
-        #     for j in range(len(max_arr)):
-        #         if j == i:
-        #             continue
-        #         else:
-        #             ans = max(ans,abs(min_arr[i] - max_arr[j]))
-        
-        # return ans
-
-        for i in range(len(arrays)):
-            min_arr.append((arrays[i][0],i))
-            max_arr.append((arrays[i][len(arrays[i]) - 1],i))
-        
-        min_arr = sorted(min_arr,key = lambda x : x[0])
-        max_arr = sorted(max_arr,key = lambda x : x[0],reverse= True)
-
-        print(min_arr)
-        print(max_arr)
-
-        ans = float(-inf)
-
-        i = 0
-        j = 0
-
-        while j < len(max_arr):
-            if max_arr[j][1] != min_arr[i][1]:
-                ans = max(ans,abs(max_arr[j][0] - min_arr[i][0]))
-                break
-            else:
-                j += 1
-        
-        i = 0
-        j = 0
-
-        while i < len(min_arr):
-            if max_arr[j][1] != min_arr[i][1]:
-                ans = max(ans,abs(max_arr[j][0] - min_arr[i][0]))
-                break
-            else:
-                i += 1
-        
-        return ans
-
-
-
+        return max_distance
         
