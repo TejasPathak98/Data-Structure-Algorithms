42c2613eeec341a0ae35b3728212253ebeb3b227
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2024-12-24 10:46:53 -0800

    Time: 23 ms (98.53%) | Memory: 21.2 MB (48.38%) - LeetSync

--- 286-walls-and-gates/walls-and-gates.py
+++ 286-walls-and-gates/walls-and-gates.py
@@ -3,25 +3,32 @@ class Solution:
         """
         Do not return anything, modify rooms in-place instead.
         """
+
         if not rooms:
             return
-        n = len(rooms)
-        m = len(rooms[0])
+        m = len(rooms)
+        n = len(rooms[0])
+        directions = [(-1,0),(1,0),(0,1),(0,-1)]
         queue = deque()
-        for i in range(n):
-            for j in range(m):
+
+        for i in range(m):
+            for j in range(n):
                 if rooms[i][j] == 0:
                     queue.append((i,j))
         
-        dir = [(-1,0),(0,1),(1,0),(0,-1)]
-
         while queue:
             x,y = queue.popleft()
             dist = rooms[x][y] + 1
-            for dx,dy in dir:
-                new_x = dx + x 
-                new_y = dy + y
-                if 0<= new_x < n and 0 <= new_y < m and rooms[new_x][new_y] == 2147483647:
-                    rooms[new_x][new_y] = dist
-                    queue.append((new_x,new_y))
+            for dx,dy in directions:
+                x_ = x + dx
+                y_ = y + dy
+
+                if 0 <= x_ < m and 0 <= y_ < n and rooms[x_][y_] == 2147483647:
+                    rooms[x_][y_] = dist
+                    queue.append((x_,y_))
+        
+        
+
+
+
         
