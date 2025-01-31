779c32d5521f1680e8584b2b1b33c3b6df5bb679
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-01-30 20:53:41 -0800

    Time: 258 ms (45.09%) | Memory: 25.5 MB (20.82%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -2,32 +2,32 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         n = len(grid)
         m = len(grid[0])
-
-        directions = [(0,1),(0,-1),(1,0),(-1,0)]
-        visited = [[False for _ in range(m)] for _ in range(n)]
-
-        ans = 0
+        directions = [[-1,0],[1,0],[0,1],[0,-1]]
+        visited = set()
 
         def bfs(x,y):
             queue = deque()
             queue.append((x,y))
-            visited[x][y] = True
+            visited.add((x,y))
 
             while queue:
-                x,y = queue.popleft()
+                a,b = queue.popleft()
+                grid[a][b] = "0"
 
                 for dx,dy in directions:
-                    new_x= x + dx
-                    new_y = y + dy
-                    if 0 <= new_x < n and 0 <= new_y < m and not visited[new_x][new_y] and grid[new_x][new_y] == "1":
-                        queue.append((new_x,new_y))
-                        visited[new_x][new_y] = True
+                    x_ = a + dx
+                    y_ = b + dy
+                    if x_ >= 0 and x_ < n and y_ >= 0 and y_ < m and grid[x_][y_] == "1" and (x_,y_) not in visited:
+                        queue.append((x_,y_))
+                        visited.add((x_,y_))
 
+        ans = 0
 
         for i in range(n):
             for j in range(m):
-                if grid[i][j] == "1" and not visited[i][j]:
+                if grid[i][j] == "1" and (i,j) not in visited:
                     bfs(i,j)
                     ans += 1
-
+        
         return ans
+        
