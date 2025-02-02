cce81e8b517c1b0d3fc1f31254a8bac0743e3f35
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-02-02 15:55:36 -0800

    Time: 0 ms (100.00%) | Memory: 18.7 MB (58.30%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -4,6 +4,7 @@ class Solution:
 
         for i in range(1,len(prices)):
             if prices[i] > prices[i - 1]:
-                profit += prices[i] - prices[i - 1]
+                profit += (prices[i] - prices[i - 1])
         
         return profit
+        
