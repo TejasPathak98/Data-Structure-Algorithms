80736130b81d0e3c44cba7d78ca3ac683f8d3b1d
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-02-01 18:19:35 -0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (44.83%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -3,25 +3,36 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        i = len(nums) - 2
 
-        while i >= 0:
-            if nums[i] < nums[i + 1]:
+        if len(nums) == 1:
+            return
+        
+        i = len(nums) - 1
+
+        while i >= 1:
+            if nums[i - 1] >= nums[i]:
+                i -= 1
+            else:
+                pos = i - 1
                 break
-            i -= 1
         
-        j = len(nums) - 1
-        if i >= 0:
-            while j >= 0:
-                if nums[i] >= nums[j]:
-                    j -= 1
-                else:
-                    break
+        if i == 0 and nums[0] >= nums[1]:
+            nums.reverse()
+            return None
+
+        for j in range(len(nums) - 1,i - 1,-1):
+            if nums[j] <= nums[pos]:
+                continue
+            else:
+                nums[j] , nums[pos] = nums[pos] , nums[j]
+                break
+
+        nums[pos + 1:] = sorted(nums[pos + 1:])
+
+
+
+
         
-        nums[i] , nums[j] = nums[j] , nums[i]
 
-        nums[i + 1:] = sorted(nums[i + 1:])
-
-
-
+            
         
