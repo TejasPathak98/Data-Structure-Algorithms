69ce0b53d7d115ec644abe8e38e34f7f5c030b18
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-02-04 01:21:49 -0800

    Time: 51 ms (25.83%) | Memory: 18.9 MB (93.32%) - LeetSync

---
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -0,0 +1,48 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        if not heights or not heights[0]:
+            return []
+        
+        pacific_q = deque()
+        atlantic_q = deque()
+        m = len(heights)
+        n = len(heights[0])
+
+        for i in range(m):
+            pacific_q.append((i,0))
+            atlantic_q.append((i,n - 1))
+        
+        for i in range(n):
+            pacific_q.append((0,i))
+            atlantic_q.append((m - 1,i))
+
+        def bfs(queue):
+            reachable = set()
+
+            while queue:
+                x,y = queue.popleft()
+                reachable.add((x,y))
+
+                for dx,dy in [(-1,0),(1,0),(0,1),(0,-1)]:
+                    x_ = x + dx
+                    y_ = y + dy
+                
+                    if x_ < 0 or x_ >= m or y_ < 0 or y_ >= n:
+                        continue
+                    
+                    if (x_,y_) in reachable:
+                        continue
+                    
+                    if heights[x_][y_] < heights[x][y]:
+                        continue
+                    
+                    queue.append((x_,y_))
+            
+
+            return reachable
+        
+        p = bfs(pacific_q)
+        q = bfs(atlantic_q)
+        return list(p.intersection(q))
+
+        
