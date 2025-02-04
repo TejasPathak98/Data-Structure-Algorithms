5ef7cd780df9de2c2509cbd00be9efbb65fa4fcd
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-02-04 03:03:19 -0800

    Time: 3 ms (78.32%) | Memory: 19 MB (78.50%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,31 +1,27 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        my_dict = defaultdict(list)
-        indegree = [0] * numCourses
+        inDegree = [0] * numCourses
+        graph = defaultdict(list)
+
         for course,pre_req in prerequisites:
-            my_dict[pre_req].append(course)
-            indegree[course] += 1
+            graph[pre_req].append(course)
+            inDegree[course] += 1
         
-        ans = []
         queue = deque()
 
         for i in range(numCourses):
-            if indegree[i] == 0:
+            if inDegree[i] == 0:
                 queue.append(i)
         
+        completed_course = []
+
         while queue:
-            c = queue.popleft()
-            ans.append(c)
-            print(ans)
-            for neighbor in my_dict[c]:
-                indegree[neighbor] -= 1
-                if indegree[neighbor] == 0:
-                    queue.append(neighbor)
+            course = queue.popleft()
+            completed_course.append(course)
+            for c in graph[course]:
+                inDegree[c] -= 1
+                if inDegree[c] == 0:queue.append(c)
         
-        if len(ans) == numCourses:
-            return ans
-        else:
-            return []
+        return completed_course if len(completed_course) == numCourses else []
+
         
-
-
