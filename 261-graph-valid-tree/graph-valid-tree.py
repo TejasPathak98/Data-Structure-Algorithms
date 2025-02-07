73d04b6150be715a54c9d2e0be8484d147ad191e
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-02-07 00:29:51 -0800

    Time: 0 ms (100.00%) | Memory: 19 MB (60.81%) - LeetSync

---
+++ 261-graph-valid-tree/graph-valid-tree.py
@@ -0,0 +1,31 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) != n - 1:
+            return False
+        
+        graph = defaultdict(list)
+        for x,y in edges:
+            graph[x].append(y)
+            graph[y].append(x)
+
+        def BFS():
+            queue = deque()
+            queue.append(0)
+            seen = set()
+            parent = {}
+            #seen.add(0)
+
+            while len(queue):
+                node = queue.popleft()
+                seen.add(node)
+                for child in graph[node]:
+                    if parent.get(node) == child:continue
+                    if child in seen:return False
+                    parent[child] = node
+                    queue.append(child)
+                    
+            return len(seen) == n
+        
+        return BFS()
+
+        
