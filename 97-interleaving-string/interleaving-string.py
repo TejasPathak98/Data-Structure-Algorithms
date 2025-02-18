ca9bb6dac651d33a226c25ef03f88db6ebea4a52
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-02-17 20:33:20 -0800

    Time: 34 ms (96.73%) | Memory: 18 MB (67.15%) - LeetSync

--- 97-interleaving-string/interleaving-string.py
+++ 97-interleaving-string/interleaving-string.py
@@ -4,22 +4,22 @@ class Solution:
         n = len(s2)
         l = len(s3)
 
-        if m + n != l:return False
-
-
-        dp = [[False] * (n + 1) for _ in range(m + 1)]
-        dp[0][0] = True
-
-        for i in range(1,m + 1):
-            dp[i][0] = dp[i - 1][0] and (s1[i - 1] == s3[i - 1])
+        if m + n != l:
+            return False
         
+        if m < n:
+            return self.isInterleave(s2, s1, s3)
+        
+        dp = [False] * (n + 1)
+        dp[0] = True
+
         for j in range(1,n + 1):
-            dp[0][j] = dp[0][j - 1] and (s2[j - 1] == s3[j - 1])
+            dp[j] = dp[j - 1] and s2[j - 1] == s3[j - 1]
         
         for i in range(1,m + 1):
-            for j in range(1,n + 1):
-                dp[i][j] = (dp[i - 1][j] and s1[i - 1] == s3[i + j - 1]) or (dp[i][j - 1] and s2[j - 1] == s3[i + j - 1])
-        
-        return dp[m][n]
-        
+            dp[0] = dp[0] and (s1[i - 1] == s3[i - 1])
 
+            for j in range(1,n + 1):
+                dp[j] = (dp[j] and s1[i - 1] == s3[i + j - 1]) or (dp[j - 1] and s2[j - 1] == s3[i + j - 1])
+        
+        return dp[n]
