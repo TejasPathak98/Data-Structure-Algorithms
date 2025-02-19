5cdea00d7358db77d9b3ae6a1a59c069410806e9
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-02-18 22:56:33 -0800

    Time: 2084 ms (5.29%) | Memory: 457.3 MB (7.16%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -1,27 +1,20 @@
 class Solution:
-    from functools import cache
     def maxProfit(self, prices: List[int]) -> int:
-        
         memo = collections.defaultdict(int)
-        
+
         #@cache
-        def dp(idx,state,trans):
+        def dp(idx,state,transactions):
             if idx >= len(prices):
-                return 0 
-            if trans >= 2:
-                return 0 
+                return 0
+            if transactions >= 2:
+                return 0
             
-            # buying
-            if (idx,state,trans) not in memo:
+            if (idx,state,transactions) not in memo:
                 if not state:
-                    memo[(idx,state,trans)] = max(dp(idx + 1,not state,trans) - prices[idx],dp(idx + 1,state,trans))
+                    memo[(idx,state,transactions)] = max(dp(idx + 1 ,not state,transactions) - prices[idx],dp(idx + 1,state,transactions))
                 else:
-                    memo[(idx,state,trans)] = max(dp(idx + 1,not state,trans + 1) + prices[idx],dp(idx + 1,state,trans))
-                
-            return memo[(idx,state,trans)]
+                    memo[(idx,state,transactions)] = max(dp(idx + 1 ,not state,transactions + 1) + prices[idx],dp(idx + 1,state,transactions))
+            
+            return memo[(idx,state,transactions)]
         
         return dp(0,False,0)
-
-
-
-
