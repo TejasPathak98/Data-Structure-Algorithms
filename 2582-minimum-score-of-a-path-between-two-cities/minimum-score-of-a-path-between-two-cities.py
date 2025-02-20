062f2bbac451b8e88cff9651dce3e2df008406f2
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-02-19 18:07:38 -0800

    Time: 275 ms (50.73%) | Memory: 73.5 MB (72.77%) - LeetSync

--- 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.py
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.py
@@ -1,34 +1,62 @@
 class Solution:
     def minScore(self, n: int, roads: List[List[int]]) -> int:
-        list_adj = {}
+        graph = defaultdict(list)
         
-        for road in roads:
-            a,b,d = road
-            if a not in list_adj:
-                list_adj[a] = []
-            if b not in list_adj:
-                list_adj[b] = []
-            list_adj[a].append((b,d))
-            list_adj[b].append((a,d))
-
-        q = [1]
-        dist = []
-        seen = set()
-
-        while q:
-            a = q.pop()
-            if a in seen:
-                continue
-            seen.add(a)
-            for children in list_adj[a]:
-                c,d = children
-                q.append(c)
-                dist.append(d)
-
-        return min(dist)
-            
-            
-
-
-
+        for u,v,w in roads:
+            graph[u].append((v,w))
+            graph[v].append((u,w))
         
+        min_distance = float("inf")
+
+        # def dfs(node,temp,visited,dist):
+        #     nonlocal min_distance
+
+        #     if node == n:
+        #         print("Break",dist)
+        #         temp.append(dist)
+        #         tp = temp.copy()
+        #         min_distance = min(min_distance,min(tp))
+        #         return
+            
+        #     if node in visited:
+        #         return
+            
+        #     visited.add(node)
+        #     temp.append(dist)
+
+        #     for neighbor,d in graph[node]:
+        #         dfs(neighbor,temp,visited,d)
+        
+        # dfs(1,[],set(),10 ** 4 + 1)
+
+        # return min_distance
+
+        visited_edges = set()
+
+        def dfs(node):
+            nonlocal min_distance
+            
+            for neighbor,d in graph[node]:
+                edge = tuple(sorted([node,neighbor]))
+                
+                if edge in visited_edges:
+                    continue
+
+                visited_edges.add(edge)
+
+                min_distance = min(min_distance,d)
+
+                if min_distance == 1:
+                    return
+                
+                dfs(neighbor)
+        
+        dfs(1)
+        return min_distance
+
+            
+
+
+
+
+        return min_distance
