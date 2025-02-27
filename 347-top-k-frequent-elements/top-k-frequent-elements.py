6c90a488d4d3a8d5b8b8678284d6e5e631d48fc3
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-02-27 00:29:58 -0800

    Time: 0 ms (100.00%) | Memory: 21.4 MB (40.65%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -2,7 +2,7 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         ans = []
         counter = Counter(nums)
-        counter = sorted(counter.items(),key = lambda x : x[1],reverse=True)
+        counter = sorted(counter.items(),key = lambda x:x[1],reverse = True)
         for number,frequency in counter:
             if k == 0:
                 break
@@ -10,4 +10,3 @@ class Solution:
             k -= 1
         
         return ans
-        
