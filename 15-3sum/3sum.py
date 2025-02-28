4dee363a66f9c75f07293b6fbbd720a904ac9805
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-02-27 23:03:37 -0800

    Time: 569 ms (63.66%) | Memory: 20.5 MB (84.51%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,21 +1,57 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # if len(nums) < 3:
+        #     return []
+        # nums = sorted(nums)
+        # ans = set()
+
+        # for i,v in enumerate(nums):
+        #     if i >= 1 and nums[i - 1] == v:
+        #         continue
+        #     triplet_dict = {}
+        #     for x in nums[i + 1:]:
+        #         if x not in triplet_dict:
+        #             triplet_dict[(-x-v)] = 1
+        #         else:
+        #             ans.add((v,x,-x-v))
+        
+        # return list(ans)
+
+        #O(N^2) ; O(N)
+
         if len(nums) < 3:
             return []
-        nums = sorted(nums)
-        ans = set()
+        nums.sort()
+        ans = []
 
         for i,v in enumerate(nums):
             if i >= 1 and nums[i - 1] == v:
                 continue
-            triplet_dict = {}
-            for x in nums[i + 1:]:
-                if x not in triplet_dict:
-                    triplet_dict[(-x-v)] = 1
+            
+            left = i + 1
+            right = len(nums) - 1
+
+            while left < right:
+                total = nums[left] + nums[right] + v
+                if total == 0:
+                    ans.append([v,nums[left],nums[right]])
+                    left += 1
+                    right -= 1
+                
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+                
+                elif total < 0:
+                    left += 1
                 else:
-                    ans.add((v,x,-x-v))
-        
-        return list(ans)
+                    right -= 1
+
+        return ans
+
+
 
 
         
