0cbd9aeb80b0d9b552787e653c60c2dce062db5d
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-02-28 19:26:16 -0800

    Time: 55 ms (34.09%) | Memory: 18 MB (19.10%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -3,15 +3,19 @@ class Solution:
         l= 0
         r = 0
         s1_dict = Counter(s1)
+        sliding_window_dict = Counter(s2[:len(s1)])
 
         l = 0
         r = len(s1) - 1
 
         while r < len(s2):
-            if Counter(s2[l:r + 1]) == s1_dict:
+            if sliding_window_dict == s1_dict:
                 return True
             else:
-                r += 1
+                sliding_window_dict[s2[l]] -= 1
                 l += 1
+                r += 1
+                if r < len(s2):sliding_window_dict[s2[r]] += 1
+        return False
 
-        return False 
+        #O(l1 + l2) ; O() 
