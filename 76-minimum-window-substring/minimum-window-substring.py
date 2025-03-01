40d22379d3049ea0e6fe5aa6916b97f1823b69cf
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-02-28 21:44:24 -0800

    Time: 82 ms (54.02%) | Memory: 18.1 MB (77.81%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,41 +1,39 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        m = len(s)
-        n = len(t)
-        if m < n:
+        if len(s) < len(t):
             return ""
-        if m == n:
+        if len(s) == len(t):
             if Counter(s) == Counter(t):
                 return s
             else:
                 return ""
-
-        if not s or not t:
-            return ""
         
-        dict_t = Counter(t)
-        dict_s = defaultdict(int)
+        t_dict = Counter(t)
+        s_dict = defaultdict(int)
+
         l = 0 
         r = 0
-        ans = float("inf"),l,r
+        ans = float('inf'),l,r
+        required = len(t_dict)
         formed = 0
-        required = len(dict_t)
 
         while r < len(s):
-            dict_s[s[r]] += 1
-            if dict_s[s[r]] == dict_t[s[r]]:
+            s_dict[s[r]] += 1
+            if s_dict[s[r]] == t_dict[s[r]]:
                 formed += 1
             
             while l < len(s) and formed == required:
                 if ans[0] > r - l + 1:
                     ans = r - l + 1,l,r
                 
-                dict_s[s[l]] -= 1
-                if dict_s[s[l]] < dict_t[s[l]]:
+                s_dict[s[l]] -= 1
+                if s_dict[s[l]] < t_dict[s[l]]:
                     formed -= 1
+
                 l += 1
             r += 1
         
+
         return "" if ans[0] == float("inf") else s[ans[1]:ans[2] + 1]
 
-        
+
