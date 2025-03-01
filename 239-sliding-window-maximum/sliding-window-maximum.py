9b4bde897ec6b165bf772254838abe02f57dc6df
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-02-28 21:20:02 -0800

    Time: 170 ms (60.07%) | Memory: 31.9 MB (33.87%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -1,24 +1,46 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        # if len(nums) == 1:
+        #     return [nums[0]]
+        
+        # ans = []
+
+        # max_heap = []
+        # for i in range(k):
+        #     max_heap.append((-1 * nums[i],i))
+
+        # heapq.heapify(max_heap)
+
+        # ans.append(-1 * max_heap[0][0])
+
+        # for i in range(k,len(nums)):
+        #     while max_heap and max_heap[0][1] <= i - k:
+        #         heapq.heappop(max_heap)
+            
+        #     heapq.heappush(max_heap, (-1 * nums[i],i))
+        #     ans.append(-1* max_heap[0][0])
+        
+        # return ans
+
+        # #O(NlogN) ; O(N)
+        
         if len(nums) == 1:
             return [nums[0]]
         
         ans = []
+        dq = deque()
 
-        max_heap = []
-        for i in range(k):
-            max_heap.append((-1 * nums[i],i))
-
-        heapq.heapify(max_heap)
-
-        ans.append(-1 * max_heap[0][0])
-
-        for i in range(k,len(nums)):
-            while max_heap and max_heap[0][1] <= i - k:
-                heapq.heappop(max_heap)
+        for i in range(len(nums)):
+            if dq and dq[0] < i - k + 1:
+                dq.popleft()
             
-            heapq.heappush(max_heap, (-1 * nums[i],i))
-            ans.append(-1* max_heap[0][0])
+            while dq and nums[dq[-1]] < nums[i]:
+                dq.pop()
+            
+            dq.append(i)
+
+            if i >= k - 1:
+                ans.append(nums[dq[0]])
         
         return ans
 
