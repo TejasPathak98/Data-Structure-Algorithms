d9cb2e91d59f4e2d608a80036d534412a60700d8
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-02 22:32:09 -0800

    Time: 47 ms (6.42%) | Memory: 18.4 MB (6.26%) - LeetSync

---
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -0,0 +1,52 @@
+class Solution:
+    def __init__(self):
+        self.minHeap = []
+        self.maxHeap = []
+
+    def helper(self,number,minHeap,maxHeap):
+        if not maxHeap:
+            heapq.heappush(maxHeap,-number)
+        elif not minHeap:
+            heapq.heappush(minHeap, number)
+        else:
+            if len(maxHeap) <= len(minHeap):
+                heapq.heappush(maxHeap,-number)
+            elif len(maxHeap) >= len(minHeap) + 1:
+                heapq.heappush(minHeap, number)
+
+        if maxHeap and minHeap and -maxHeap[0] > minHeap[0]:
+            x = -heapq.heappop(maxHeap)
+            heapq.heappush(minHeap, x)
+
+        while len(maxHeap) < len(minHeap):
+            x = heapq.heappop(minHeap)
+            heapq.heappush(maxHeap, -x)
+
+
+        print(-maxHeap[0]) if maxHeap else print("No Max Heap")
+        print(minHeap[0]) if minHeap else print("No Min Heap")
+
+            
+
+
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        for i in range(len(nums1)):
+            self.helper(nums1[i],self.minHeap,self.maxHeap)
+        
+        for i in range(len(nums2)):
+            self.helper(nums2[i],self.minHeap,self.maxHeap)
+
+        if len(self.maxHeap) == len(self.minHeap) + 1:
+            return float(-self.maxHeap[0])
+        else:
+            print(self.minHeap[0])
+            return float((-self.maxHeap[0] + self.minHeap[0]) / 2)
+
+        
+
+    
+
+
+
+
+        
