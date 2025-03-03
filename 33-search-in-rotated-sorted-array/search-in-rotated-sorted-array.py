fbf2494cbd1f5f29bba0eaef9097f93674a91999
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-02 20:28:36 -0800

    Time: 3 ms (4.49%) | Memory: 18 MB (91.36%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,26 +1,32 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left = 0 
-        right = len(nums) - 1
+        n = len(nums)
+        if n == 1:
+            if nums[0] == target:return 0
+            else:return -1
+        
+        l = 0
+        r = len(nums) - 1
 
-        while left <= right:
-            mid = (left + right) // 2
+        while l <= r:
+            mid = (l + r) // 2
             if nums[mid] == target:
                 return mid
             
-            if nums[left] <= nums[mid]:
-                if nums[left] <= target <= nums[mid]:
-                    right = mid - 1
+            if nums[mid] > target and nums[mid] >= nums[0]:
+                if target >= nums[0]:
+                    r = mid - 1
                 else:
-                    left = mid + 1
-            
-            else:
-                if nums[mid] <= target <= nums[right]:
-                    left = mid + 1
+                    l = mid + 1
+            elif nums[mid] > target and nums[mid] < nums[0]:
+                r = mid - 1
+            elif nums[mid] < target and nums[mid] >= nums[0]:
+                l = mid + 1
+            elif nums[mid] < target and nums[mid] < nums[0]:
+                if target >= nums[0]:
+                    r = mid - 1
                 else:
-                    right = mid - 1
+                    l = mid + 1
         
         return -1
-            
-
         
