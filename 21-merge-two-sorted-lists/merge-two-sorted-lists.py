1a9269c0f1e7bae419329978b8c3918006a67091
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-04 02:28:50 -0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (61.84%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -5,6 +5,40 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        # if not l1 and not l2:
+        #     return None
+        # if not l1:
+        #     return l2
+        # if not l2:
+        #     return l1
+        
+        # new_head = l3 = ListNode(-101)
+
+        # while l1 and l2:
+        #     if l1.val >= l2.val:
+        #         l3.next = ListNode(l2.val)
+        #         l2 = l2.next
+        #     else:
+        #         l3.next = ListNode(l1.val)
+        #         l1 = l1.next
+        #     l3 = l3.next
+        
+        # while l1:
+        #     l3.next = ListNode(l1.val)
+        #     l3 = l3.next
+        #     l1 = l1.next
+        
+        # while l2:
+        #     l3.next = ListNode(l2.val)
+        #     l3 = l3.next
+        #     l2 = l2.next
+
+        # return new_head.next
+
+
+        # #O(N) ; O(N)
+
+
         if not l1 and not l2:
             return None
         if not l1:
@@ -12,29 +46,22 @@ class Solution:
         if not l2:
             return l1
         
-        new_head = l3 = ListNode(-101)
+        new_head = l3 = ListNode(-101) 
 
         while l1 and l2:
             if l1.val >= l2.val:
-                l3.next = ListNode(l2.val)
+                l3.next = l2
                 l2 = l2.next
             else:
-                l3.next = ListNode(l1.val)
+                l3.next = l1
                 l1 = l1.next
             l3 = l3.next
         
-        while l1:
-            l3.next = ListNode(l1.val)
-            l3 = l3.next
-            l1 = l1.next
+        if l1:
+            l3.next = l1
+        elif l2:
+            l3.next = l2
         
-        while l2:
-            l3.next = ListNode(l2.val)
-            l3 = l3.next
-            l2 = l2.next
-
         return new_head.next
-
-
 
         
