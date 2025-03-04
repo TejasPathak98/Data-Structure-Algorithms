365cc068ba3d923aea4bd549a4486b74ff79a95d
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-04 03:24:44 -0800

    Time: 78 ms (99.10%) | Memory: 78.5 MB (46.33%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -8,58 +8,56 @@ class Node:
 class LRUCache:
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.my_dict = defaultdict(Node)
+        self.cache = defaultdict(Node)
         self.head = Node(-1,-1)
         self.tail = Node(-1,-1)
         self.head.next = self.tail
         self.tail.prev = self.head
-        
+
     def get(self, key: int) -> int:
-        if key not in self.my_dict:
+        if key not in self.cache:
             return -1
         else:
-            node = self.my_dict[key]
-            self.removeNode(node)
+            node = self.cache[key]
+            self.RemoveNode(node)
             self.addNode(node)
             return node.value
-
+        
     def put(self, key: int, value: int) -> None:
-        if key in self.my_dict:
-            node = self.my_dict[key]
+        if key in self.cache:
+            node = self.cache[key]
             node.value = value
-            self.removeNode(node)
+            self.RemoveNode(node)
             self.addNode(node)
         else:
-            if len(self.my_dict) < self.capacity:
-                node = Node(key,value)
-                self.addNode(node)
-                self.my_dict[key] = node
-            else:
-                self.removeLastNode()
-                node = Node(key,value)
-                self.addNode(node)
-                self.my_dict[key] = node
-            
-    def addNode(self,node):
-        next_temp = self.head.next
-        self.head.next = node
-        node.next = next_temp
-        node.prev = self.head
-        next_temp.prev = node
+            if len(self.cache) == self.capacity:
+                self.RemovefromLast()
+            node = Node(key,value)
+            self.cache[key] = node
+            self.addNode(node)
 
-    def removeNode(self,node):
-        prev_node = node.prev
-        next_node = node.next
-        prev_node.next = next_node
-        next_node.prev = prev_node
-
-    def removeLastNode(self):
-        node = self.tail.prev
-        if node.key not in self.my_dict:
-            return
-        del self.my_dict[node.key]
-        self.removeNode(node)
+    def addNode(self,new_node):
+        temp_node = self.head.next
+        self.head.next = new_node
+        new_node.next = temp_node
+        new_node.prev = self.head
+        temp_node.prev = new_node
     
+    def RemoveNode(self,node):
+        next_node = node.next
+        prev_node = node.prev
+        next_node.prev = prev_node
+        prev_node.next = next_node
+    
+    def RemovefromLast(self):
+        node = self.tail.prev
+        if node.key not in self.cache:
+            return
+        del self.cache[node.key]
+        self.RemoveNode(node)
+
+
+
         
 
 
