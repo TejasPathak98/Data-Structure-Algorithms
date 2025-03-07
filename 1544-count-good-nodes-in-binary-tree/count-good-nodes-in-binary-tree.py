7a3811d3661cd6143e8239c77fd303a2d58ed079
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-06 23:15:31 -0800

    Time: 146 ms (49.18%) | Memory: 31.9 MB (86.88%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
@@ -9,24 +9,42 @@ class Solution:
         self.max_count = 0 
 
     def goodNodes(self, root: TreeNode) -> int:
-        self.dfs(root,[])
-        return self.max_count
+    #     self.dfs(root,[])
+    #     return self.max_count
 
-    def dfs(self,root,nodes):
-        if root is None:
-            return
+    # def dfs(self,root,nodes):
+    #     if root is None:
+    #         return
 
-        if len(nodes) == 0:
-            self.max_count += 1
-        else:
-            if root.val >= max(nodes):
-                self.max_count += 1
+    #     if len(nodes) == 0:
+    #         self.max_count += 1
+    #     else:
+    #         if root.val >= max(nodes):
+    #             self.max_count += 1
         
-        nodes.append(root.val)
+    #     nodes.append(root.val)
         
-        self.dfs(root.left,nodes)
-        self.dfs(root.right, nodes)
+    #     self.dfs(root.left,nodes)
+    #     self.dfs(root.right, nodes)
 
-        nodes.pop()
+    #     nodes.pop()
+
+    # #O(N); O(N)
+
+        queue = deque([(root,float("-inf"))])
+        max_count = 0
+
+        while queue:
+            node,max_so_far = queue.popleft()
+            if node.val >= max_so_far:
+                max_count += 1
+            
+            if node.left:
+                queue.append((node.left,max(max_so_far,node.val)))
+            if node.right:
+                queue.append((node.right,max(max_so_far,node.val)))
+
+        return max_count
+            
 
         
