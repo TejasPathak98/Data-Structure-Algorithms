ebb8ee88af6feeddb57de2c193ae75d473a19b9b
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-06 19:53:53 -0800

    Time: 0 ms (100.00%) | Memory: 18.1 MB (59.07%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/subtree-of-another-tree.py
@@ -6,24 +6,41 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        return self.dfs(root,subRoot)
+    #     return self.dfs(root,subRoot)
 
-    def dfs(self,root,subRoot):
-        if not root:
-            return False
+    # def dfs(self,root,subRoot):
+    #     if not root:
+    #         return False
         
-        if self.isSameTree(root,subRoot):
-            return True
+    #     if self.isSameTree(root,subRoot):
+    #         return True
         
-        return self.dfs(root.left,subRoot) or self.dfs(root.right,subRoot)
+    #     return self.dfs(root.left,subRoot) or self.dfs(root.right,subRoot)
     
-    def isSameTree(self,root,subRoot):
-        if not root and not subRoot:
-            return True
-        if not root or not subRoot:
-            return False
-        if root.val != subRoot.val:
-            return False
+    # def isSameTree(self,root,subRoot):
+    #     if not root and not subRoot:
+    #         return True
+    #     if not root or not subRoot:
+    #         return False
+    #     if root.val != subRoot.val:
+    #         return False
         
-        return self.isSameTree(root.left, subRoot.left) and self.isSameTree(root.right, subRoot.right)
+    #     return self.isSameTree(root.left, subRoot.left) and self.isSameTree(root.right, subRoot.right)
  
+
+    # #O(N*M) ; O(N)
+
+        root_str_representation = self.preorder(root)
+        subRoot_str_representation = self.preorder(subRoot)
+
+        if subRoot_str_representation in root_str_representation:
+            return True
+        
+        return False
+
+    def preorder(self,root):
+        if root is None:
+            return "X"
+
+        return f" #{root.val} {self.preorder(root.left)} {self.preorder(root.right)}"
+
