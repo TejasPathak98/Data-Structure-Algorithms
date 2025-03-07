993856b23162332cc06f8838d52e134b8b267904
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-06 20:07:57 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (66.21%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -17,19 +17,40 @@ class Solution:
 
         #O(N) ; O(N)
 
-        queue = deque([(p,q)])
+        # queue = deque([(p,q)])
 
-        while queue:
-            n1,n2 = queue.popleft()
+        # while queue:
+        #     n1,n2 = queue.popleft()
+
+        #     if n1 is None and n2 is None:
+        #         continue
+            
+        #     if  n1 is None or n2 is None or n1.val != n2.val:
+        #         return False
+            
+        #     queue.append((n1.left,n2.left))
+        #     queue.append((n1.right,n2.right))
+        
+        # return True
+
+        # #O(N) ; O(N) ....Better than recursion for bigger trees where N > 1000
+
+
+        stack = [(p,q)]
+
+        while stack:
+            n1,n2 = stack.pop()
 
             if n1 is None and n2 is None:
                 continue
             
             if  n1 is None or n2 is None or n1.val != n2.val:
                 return False
-            
-            queue.append((n1.left,n2.left))
-            queue.append((n1.right,n2.right))
+
+            stack.append((n1.left,n2.left))
+            stack.append((n1.right,n2.right))
         
         return True
+
+
     
