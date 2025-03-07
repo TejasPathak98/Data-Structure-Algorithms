f7b53a726ad686fabc0d80e140bfb7ef6ba6e232
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-06 23:25:52 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (17.98%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -6,24 +6,45 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        queue = deque([root])
-        queue.append(None)
-        temp = []
-        final_arr= []
+        # queue = deque([root])
+        # queue.append(None)
+        # temp = []
+        # final_arr= []
 
-        while root:
-            node = queue.popleft()
-            if node == None:
-                final_arr.append(temp.copy())
-                if len(queue) == 0:
-                    break
-                queue.append(None)
-            else:
-                temp.append(node.val)
+        # while root:
+        #     node = queue.popleft()
+        #     if node == None:
+        #         final_arr.append(temp.copy())
+        #         if len(queue) == 0:
+        #             break
+        #         queue.append(None)
+        #     else:
+        #         temp.append(node.val)
+        #         if node.left:
+        #             queue.append(node.left)
+        #         if node.right:
+        #             queue.append(node.right)
+        
+        # return [l[-1] for l in final_arr]
+                
+        # #O(N) ; O(N)
+        if not root:
+            return []
+
+        queue = deque([root])
+        right_view = []
+
+        while queue:
+            level_size = len(queue)
+            for i in range(level_size):
+                node = queue.popleft()
+
+                if i == level_size - 1:
+                    right_view.append(node.val)
+                
                 if node.left:
                     queue.append(node.left)
                 if node.right:
                     queue.append(node.right)
-        
-        return [l[-1] for l in final_arr]
-                
+
+        return right_view
