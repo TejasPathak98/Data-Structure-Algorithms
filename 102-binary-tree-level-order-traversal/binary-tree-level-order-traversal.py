d8a459e3e0fcc73589bdcf62081817fd37a5d5ff
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-07 02:19:32 -0800

    Time: 0 ms (100.00%) | Memory: 18.5 MB (60.08%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -5,29 +5,54 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def __init__(self):
+        self.node_dict = defaultdict(list)
+
+    
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        # if not root:
+        #     return []
+        
+        # ans = []
+        # queue = deque([root])
+        # temp = []
+
+        # while queue:
+        #     size = len(queue)
+        #     ans.append(temp.copy())
+        #     temp.clear()
+        #     for _ in range(size):
+        #         node = queue.popleft()
+        #         temp.append(node.val)
+
+        #         if node.left:
+        #             queue.append(node.left)
+        #         if node.right:
+        #             queue.append(node.right)
+        
+        # ans.append(temp)
+        # return ans[1:]
+
+        # #O(N) ; O(N)
         if not root:
             return []
         
-        ans = []
-        queue = deque([root])
-        temp = []
+        self.dfs(root,0)
+        return list(self.node_dict.values())
 
-        while queue:
-            size = len(queue)
-            ans.append(temp.copy())
-            temp.clear()
-            for _ in range(size):
-                node = queue.popleft()
-                temp.append(node.val)
+    def dfs(self,root,level):
+        if root is None:
+            return
 
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-        
-        ans.append(temp)
-        return ans[1:]
+        self.node_dict[level].append(root.val)
+
+        if root.left:
+            self.dfs(root.left,level + 1)
+        if root.right:
+            self.dfs(root.right,level + 1)
+
+
+
 
 
     
