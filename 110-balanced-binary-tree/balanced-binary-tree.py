944f0cf769e6c30cff5bca47250632fd24508a8a
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-06 20:46:07 -0800

    Time: 3 ms (68.28%) | Memory: 19 MB (25.76%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -6,19 +6,37 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        return self.dfs(root)
+    #     return self.dfs(root)
+
+    # def dfs(self,root):
+    #     if not root:
+    #         return True
+        
+    #     if abs(self.depth(root.left) - self.depth(root.right)) > 1:
+    #         return False
+        
+    #     return self.dfs(root.left) and self.dfs(root.right)
+    
+    # def depth(self,root):
+    #     if root is None:
+    #         return 0
+        
+    #     return 1 + max(self.depth(root.left),self.depth(root.right))
+
+        return self.dfs(root)[0]
+
 
     def dfs(self,root):
         if not root:
-            return True
-        
-        if abs(self.depth(root.left) - self.depth(root.right)) > 1:
-            return False
-        
-        return self.dfs(root.left) and self.dfs(root.right)
+            return (True,0) #isBalanced , depth
+
+        left_balanced,left_depth = self.dfs(root.left)
+        right_balanced,right_depth = self.dfs(root.right)
+
+        is_Balanced =  (left_balanced and right_balanced and abs(left_depth - right_depth) <= 1)
+        max_depth = 1 + max(left_depth,right_depth)
+
+        return is_Balanced,max_depth
+
+
     
-    def depth(self,root):
-        if root is None:
-            return 0
-        
-        return 1 + max(self.depth(root.left),self.depth(root.right))
