5708a1fbf36fb667a95d40b9487acd442e4392b8
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-08 01:19:02 -0800

    Time: 19 ms (74.40%) | Memory: 17.9 MB (97.74%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -1,46 +1,31 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        n = len(grid)
-        m = len(grid[0])
-        directions = [(-1,0),(1,0),(0,1),(0,-1)]
+        m = len(grid)
+        n = len(grid[0])
         max_area = 0
-        visited = set()
 
-        def bfs(x,y):
-            nonlocal max_area
-            queue = deque()
-            queue.append((x,y))
-            area = 0
-            visited.add((x,y))
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    max_area = max(max_area,self.bfs(grid,i,j))
 
-            while queue:
-                x,y = queue.popleft()
-                area += 1
-                max_area = max(max_area,area)
-
-                for dx,dy in directions:
-                    x_new = x + dx
-                    y_new = y + dy
-
-                    if 0 <= x_new < n and 0 <= y_new < m and grid[x_new][y_new] == 1 and (x_new,y_new) not in visited:
-                        visited.add((x_new,y_new))
-                        queue.append((x_new,y_new))
-
-        
-        
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j] == 1 and (i,j) not in visited:
-                    bfs(i,j)
-        
         return max_area
 
-        
-            
+    def bfs(self,grid,i,j):
+        area = 0
+        queue = deque([(i,j)])
+        grid[i][j] = -1
 
+        while queue:
+            x,y = queue.popleft()
+            area += 1
 
+            for dx,dy in [(0,1),(0,-1),(1,0),(-1,0)]:
+                x_ = x + dx
+                y_ = y + dy
 
+                if x_ >= 0 and x_ < len(grid) and y_ >= 0 and y_ < len(grid[0]) and grid[x_][y_] == 1:
+                    grid[x_][y_] = -1
+                    queue.append((x_,y_))
 
-
-
-        
+        return area
