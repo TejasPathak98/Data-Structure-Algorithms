9efe4b8162fc7cd5de9f430e6fb8c65f1d0c91f5
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-08 02:01:12 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (26.92%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -3,56 +3,44 @@ class Solution:
         m = len(grid)
         n = len(grid[0])
 
-        s = sum(sum(row) for row in grid) 
-        if s == 0:
+        if all(cell == 0 for row in grid for cell in row):
             return 0
 
-        if m == 1 and n == 1:
-            if grid[0][0] == 1:return -1
-            elif grid[0][0] == 2:return 0
-            else: return 0
-
-        rotten_oranges = []
+        queue = deque()
 
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 2:
-                    rotten_oranges.append([i,j])
+                    queue.append((i,j))
         
-        visited = [[False] * n for _ in range(m)]
-        queue = deque()
-        for orange in rotten_oranges:
-            queue.append(orange)
-            visited[orange[0]][orange[1]] = True
+        return self.bfs(queue,grid)
 
-        directions = [[-1,0],[1,0],[0,1],[0,-1]]
+
+    def bfs(self,queue,grid):
         time = -1
+        m = len(grid)
+        n = len(grid[0])
 
         while queue:
+            size = len(queue)
             time += 1
-            for _ in range(len(queue)):
+
+            for _ in range(size):
                 x,y = queue.popleft()
-                for dx,dy in directions:
+
+                for dx,dy in [(0,1),(0,-1),(1,0),(-1,0)]:
                     x_ = x + dx
                     y_ = y + dy
 
-                    if x_ >= 0 and x_ < m and y_ >= 0 and y_ < n and not visited[x_][y_]:
-                        if grid[x_][y_] == 1:
-                            grid[x_][y_] = 2
-                            queue.append([x_,y_])
-                            visited[x_][y_] = True
+                    if x_ >= 0 and x_ < m and y_ >=0 and y_ < n and grid[x_][y_] == 1:
+                        grid[x_][y_] = 2
+                        queue.append((x_,y_))
 
         
-
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1:
                     return -1
-        
         return time
-        
 
 
-
-
-        
