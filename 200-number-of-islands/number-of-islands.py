d7083336f41a3cc93f5e8fccf0dc0b3b65ba184e
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-08 01:02:11 -0800

    Time: 257 ms (46.90%) | Memory: 20.1 MB (69.09%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,33 +1,29 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        n = len(grid)
-        m = len(grid[0])
-        directions = [[-1,0],[1,0],[0,1],[0,-1]]
-        visited = set()
+        m = len(grid)
+        n = len(grid[0])
+        count = 0
 
-        def bfs(x,y):
-            queue = deque()
-            queue.append((x,y))
-            visited.add((x,y))
-
-            while queue:
-                a,b = queue.popleft()
-                grid[a][b] = "0"
-
-                for dx,dy in directions:
-                    x_ = a + dx
-                    y_ = b + dy
-                    if x_ >= 0 and x_ < n and y_ >= 0 and y_ < m and grid[x_][y_] == "1" and (x_,y_) not in visited:
-                        queue.append((x_,y_))
-                        visited.add((x_,y_))
-
-        ans = 0
-
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j] == "1" and (i,j) not in visited:
-                    bfs(i,j)
-                    ans += 1
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "1":
+                    self.bfs(i,j,grid)
+                    count +=1
         
-        return ans
-        
+        return count
+
+    def bfs(self,x,y,grid):
+        queue = deque([(x,y)])
+
+        while queue:
+            cx,cy = queue.popleft()
+
+            for dx,dy in [(0,1),(0,-1),(1,0),(-1,0)]:
+                x_ = cx + dx
+                y_ = cy + dy
+
+                if x_ >= 0 and x_ < len(grid) and y_ >=0 and y_ < len(grid[0]) and grid[x_][y_] == "1":
+                    grid[x_][y_] = "-1"
+                    queue.append((x_,y_))
+
+    
