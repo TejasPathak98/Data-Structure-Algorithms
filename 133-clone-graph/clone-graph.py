d537f8a407ae27463ec886a555278bb589159c19
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-08 01:35:54 -0800

    Time: 45 ms (46.88%) | Memory: 18.3 MB (17.08%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -8,21 +8,43 @@ class Node:
 
 from typing import Optional
 class Solution:
-    def __init__(self):
-        self.graph_dict = {}
+    # def __init__(self):
+    #     self.graph_dict = {}
 
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+    #     if not node:
+    #         return None
+        
+    #     if node in self.graph_dict:
+    #         return self.graph_dict[node]
+        
+    #     new_node = Node(node.val)
+
+    #     self.graph_dict[node] = new_node
+
+    #     for neighbor in node.neighbors:
+    #         new_node.neighbors.append(self.cloneGraph(neighbor))
+        
+    #     return new_node
+
+    # #O(V + E) ; O(V)
+
         if not node:
             return None
         
-        if node in self.graph_dict:
-            return self.graph_dict[node]
-        
-        new_node = Node(node.val)
+        old_to_new = {node : Node(node.val)}
+        queue = deque([node])
 
-        self.graph_dict[node] = new_node
+        while queue:
+            curr_node = queue.popleft()
 
-        for neighbor in node.neighbors:
-            new_node.neighbors.append(self.cloneGraph(neighbor))
-        
-        return new_node
+            for neighbor in curr_node.neighbors:
+                if neighbor not in old_to_new:
+                    old_to_new[neighbor] = Node(neighbor.val)
+                    queue.append(neighbor)
+
+                old_to_new[curr_node].neighbors.append(old_to_new[neighbor])
+
+        return old_to_new[node]
+
+
