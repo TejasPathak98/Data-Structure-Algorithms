6fada80282d836a94c9b4a85431ce2739ad3425e
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-09 14:54:14 -0700

    Time: 365 ms (95.32%) | Memory: 19.8 MB (37.37%) - LeetSync

--- 744-network-delay-time/network-delay-time.py
+++ 744-network-delay-time/network-delay-time.py
@@ -1,25 +1,55 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        # graph = defaultdict(list)
+
+        # for u,v,t in times:
+        #     graph[u].append((v,t))
+
+        # queue = deque([k])
+        # time = [float("inf")] * (n + 1)
+        # time[k] = 0
+
+        # while queue:
+        #     x = queue.popleft()
+
+        #     for neighbor,time_taken in graph[x]:
+        #         if time[x] + time_taken < time[neighbor]:
+        #             time[neighbor] = time[x] + time_taken
+        #             queue.append(neighbor)
+
+        
+        # if max(time[1:]) == float("inf"):
+        #     return -1
+        # else:
+        #     return max(time[1:])
+
+        # #O(V + E)  ; O(V + E)
+
         graph = defaultdict(list)
 
         for u,v,t in times:
-            graph[u].append((v,t))
+            graph[u].append((v,t)) #....O(E)
 
-        queue = deque([k])
-        time = [float("inf")] * (n + 1)
+        time = {i : float("inf") for i in range(1, n+ 1)}
+        min_heap = [(0,k)]
+        heapify(min_heap)
         time[k] = 0
 
-        while queue:
-            x = queue.popleft()
+        while min_heap:
+            curr_time,curr_node = heapq.heappop(min_heap) #O(VlogV)
 
-            for neighbor,time_taken in graph[x]:
-                if time[x] + time_taken < time[neighbor]:
-                    time[neighbor] = time[x] + time_taken
-                    queue.append(neighbor)
+            if time[curr_node] < curr_time:
+                continue
+            
+            for neighbor,t in graph[curr_node]:
+                if t + curr_time < time[neighbor]:
+                    time[neighbor] = t + curr_time
+                    heapq.heappush(min_heap, (t + curr_time,neighbor)) #O(ElogV)
 
-        
-        if max(time[1:]) == float("inf"):
-            return -1
-        else:
-            return max(time[1:])
+
+        if max(time.values()) == float("inf"):return -1
+        else: return max(time.values())
+
+
+
 
