2c70cc84f927b986db21ada547833e4b7b0d59ef
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-09 12:47:29 -0700

    Time: 336 ms (36.11%) | Memory: 18.8 MB (84.47%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -1,36 +1,28 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        wordSet = set(wordList)
-
-        queue = deque()
-        queue.append(beginWord)
-
-        if endWord not in wordSet:
+        if endWord not in wordList:
             return 0
-        
-        distance = 1
-        
+
+        queue = deque([beginWord])
+        visited = set()
+        visited.add(beginWord)
+        count = 0
+        wordList = set(wordList)
+
         while queue:
             size = len(queue)
-
-            for _ in range(size):
-
-                word = queue.popleft()
-
-                if word == endWord:
-                    return distance
-
-                for i in range(len(word)):
-                    for c in 'abcdefghijklmnopqrstuvwxyz':
-                        if c == word[i]:
-                            continue
-                        
-                        new_word = word[:i] + c + word[i + 1:]
-
-                        if new_word in wordSet:
-                            queue.append(new_word)
-                            wordSet.remove(new_word)
+            count += 1
             
-            distance += 1
+            for _ in range(size):
+                word = queue.popleft()
+                if word == endWord:
+                    return count
+            
+                for i, ch in enumerate(word):
+                    for c in "abcdefghijklmnopqrstuvwxyz":
+                        new_word = word[:i] + c + word[i + 1:]
+                        if new_word not in visited and new_word in wordList:
+                            queue.append(new_word)
+                            visited.add(new_word)
         
         return 0
