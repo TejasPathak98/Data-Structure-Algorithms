819c3dd7757d19037f4f070dbbca71be332d4b08
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-09 12:15:08 -0700

    Time: 1 ms (66.87%) | Memory: 18.4 MB (20.38%) - LeetSync

--- 684-redundant-connection/redundant-connection.py
+++ 684-redundant-connection/redundant-connection.py
@@ -1,31 +1,37 @@
 class UnionFind:
     def __init__(self,n):
-        self.parent = list(range(n + 1))
-    
-    def find(self,x):
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])
+        self.parent = list(range(n))
+        self.rank  = [1] * n
+
+    def find(self,x): # O(1)
+        if x != self.parent[x]:
+            self.parent[x] = self.find(self.parent[x])  # Path Compression
         return self.parent[x]
-    
-    def UF(self,x,y):
+
+    def Union(self,x,y): #O(1)
         px = self.find(x)
         py = self.find(y)
 
         if px == py:
             return False
+
+        if self.rank[px] > self.rank[py]:
+            self.parent[py] = px
+        elif self.rank[px] < self.rank[py]:
+            self.parent[px] = py
         else:
             self.parent[py] = px
-            return True
-    
+            self.rank[px] += 1
+        
+        return True
+
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        the_edge = []
-        uf = UnionFind(len(edges) + 1)
+        uf = UnionFind(len(edges))
+        last_edge = []
 
         for edge in edges:
-            if not uf.UF(edge[0],edge[1]):
-                the_edge = edge
+            if uf.Union(edge[0] - 1,edge[1] - 1) == False:
+                last_edge = edge
         
-        return the_edge
-
-        
+        return last_edge
