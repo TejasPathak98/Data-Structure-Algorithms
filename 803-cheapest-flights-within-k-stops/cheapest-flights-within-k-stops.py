aeeae194c91003b80b9c90be4650bf0a4e5655aa
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-10 02:26:47 -0700

    Time: 34 ms (38.26%) | Memory: 19.3 MB (38.16%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -1,29 +1,33 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        adj = {i : [] for i in range(n)}
 
-        for u,v,w in flights:
-            adj[u].append((v,w))
+        # The key idea is the inclusion of stops in the min_heap and cost_dict, as the stops is another factor in finding the lowest cost, which is different than a standard Dijkstra problem
+
+        graph = defaultdict(list)
+
+        for s,d,cost in flights:
+            graph[s].append((d,cost))
         
-        heap = [(0,src,0)]
-        best_cost = {}
+        min_heap = [(0,src,0)] #cost,node,stops
+        cost_dict = {} # (node,stops) : (cost)
 
-        while heap:
-            cost,node,stops = heapq.heappop(heap)
+        while min_heap:
+            curr_cost, node , stops = heapq.heappop(min_heap)
 
-
-            if node == dst:
-                return cost
-
-            if stops > k:
+            if node == dst and stops <= k + 1:
+                return curr_cost
+            
+            if stops > k + 1:
                 continue
-            
-            for new_node,weight in adj[node]:
-                new_cost = weight + cost
-                if (new_node,stops + 1) not in best_cost or new_cost < best_cost[(new_node,stops + 1)]:
-                    best_cost[(new_node,stops + 1)] = new_cost
-                    heapq.heappush(heap, (new_cost,new_node,stops+ 1))
+
+            for neighbor,cost in graph[node]:
+                new_cost = cost + curr_cost
+                
+                if (neighbor,stops + 1) not in cost_dict or new_cost < cost_dict.get((neighbor,stops + 1)):
+                    heapq.heappush(min_heap, (new_cost,neighbor,stops + 1))
+                    cost_dict[(neighbor,stops + 1)] = new_cost
+
+        
+        return -1
             
 
-
-        return -1     
