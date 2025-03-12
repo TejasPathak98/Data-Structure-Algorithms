f73a0acb1a377efa3559601421fcef809dee04cc
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-12 16:21:43 -0700

    Time: 75 ms (47.66%) | Memory: 22.8 MB (51.35%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -1,17 +1,35 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        min_heap = []
+        # The below is a min heap operation but can be converted to Max heap for O(klogN) TC...least distance...remember rule of thumb
+        # min_heap = []
+
+        # for point in points:
+        #     x = point[0]
+        #     y = point[1]
+        #     dist = math.sqrt(x ** 2 + y ** 2)
+        #     heapq.heappush(min_heap, (dist,x,y))
+
+        # ans = []
+
+        # for _ in range(k):
+        #     _,x,y = heapq.heappop(min_heap)
+        #     ans.append([x,y])
+        
+        # return ans
+
+        # #O(NlogN) ; O(N)
+
+        max_heap = []
 
         for point in points:
             x = point[0]
             y = point[1]
-            dist = math.sqrt(x ** 2 + y ** 2)
-            heapq.heappush(min_heap, (dist,x,y))
+            dist = -1 * math.sqrt(x ** 2 + y ** 2)
+            heapq.heappush(max_heap, (dist,x,y))
 
-        ans = []
-
-        for _ in range(k):
-            _,x,y = heapq.heappop(min_heap)
-            ans.append([x,y])
+            if len(max_heap) > k:
+                heapq.heappop(max_heap)
         
-        return ans
+        return [[x,y] for _,x,y in max_heap]
+
+
