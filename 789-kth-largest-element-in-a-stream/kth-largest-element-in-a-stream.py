38c8e98cc2258999158af7a6d3f9cc3cbd521efd
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-12 15:58:20 -0700

    Time: 14 ms (72.60%) | Memory: 23.5 MB (83.36%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
@@ -4,15 +4,21 @@ class KthLargest:
         self.min_heap = nums
         self.k = k
 
-        heapq.heapify(self.min_heap)
-
-    def add(self, val: int) -> int:
-        heapq.heappush(self.min_heap, val)
+        heapq.heapify(self.min_heap) # O(N)...Flyods method inplace
 
         while len(self.min_heap) > self.k:
-            heapq.heappop(self.min_heap)
+            heapq.heappop(self.min_heap) #O(logK)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.min_heap, val) #O(logK)
+
+        while len(self.min_heap) > self.k:
+            heapq.heappop(self.min_heap) #O(logK)
         
         return self.min_heap[0]
+
+    
+        #O(N) + O(logK) ; O(N)
 
 
 # Your KthLargest object will be instantiated and called as such:
