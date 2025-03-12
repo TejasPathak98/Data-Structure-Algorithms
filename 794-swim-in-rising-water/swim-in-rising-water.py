c73edff07de64d12abdf5af2c34437b902ad2f0b
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-11 22:02:00 -0700

    Time: 39 ms (41.46%) | Memory: 18.3 MB (86.79%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.py
+++ 794-swim-in-rising-water/swim-in-rising-water.py
@@ -1,27 +1,30 @@
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
-        n = len(grid)
-        heap = [(grid[0][0],0,0)]
+        n = len(grid[0])
+
+        time_taken = 0
+        time = [[float("inf")] * n for _ in range(n)]
         visited = [[False] * n for _ in range(n)]
+        time[0][0] = grid[0][0]
 
-        while heap:
-            elevation,x,y = heapq.heappop(heap)
+        min_heap = [(grid[0][0],0,0)]
 
-            if visited[x][y] == True:
-                continue
-            
-            if x == n - 1 and y == n - 1:
-                return elevation
-            
+        while min_heap:
+            t , x, y = heapq.heappop(min_heap)
+            time_taken = max(time_taken,t)
             visited[x][y] = True
 
-            for dx,dy in [(0,1),(1,0),(-1,0),(0,-1)]:
+            if x == n - 1 and y == n - 1:
+                return time_taken
+
+            for dx,dy in [(0,1),(0,-1),(1,0),(-1,0)]:
                 x_ = x + dx
-                y_ = y + dy
-                if 0 <= x_ < n and 0 <= y_ < n and not visited[x_][y_]:
-                    heapq.heappush(heap,(max(elevation,grid[x_][y_]),x_,y_))
+                y_ = y  + dy
 
-        
+                if 0 <= x_ < n and 0 <= y_ < n and grid[x_][y_] < time[x_][y_] and visited[x_][y_] != True:
+                    heapq.heappush(min_heap, (grid[x_][y_],x_,y_))
+
+
         return -1
-                     
-
+        
+                
