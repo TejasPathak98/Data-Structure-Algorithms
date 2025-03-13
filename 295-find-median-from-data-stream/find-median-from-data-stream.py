e03c228b366944d5d493c68016291b681202c356
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-12 18:43:35 -0700

    Time: 99 ms (96.66%) | Memory: 40.1 MB (21.38%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -12,11 +12,11 @@ class MedianFinder:
         if self.max_heap and self.min_heap and -self.max_heap[0]  > self.min_heap[0]:
             heappush(self.min_heap, -heappop(self.max_heap))
 
-        #Balance the Min_heap
+        #Balance the Min_heap ; this is to ensure that the max_heap is always 1 greater than the length of the minheap
         if len(self.max_heap) > len(self.min_heap) + 1:
             heappush(self.min_heap, -heappop(self.max_heap))
         
-        #Balance the Max_heap
+        #Balance the Max_heap ; this is to ensure that the length of min heap is not greater than the max heap
         if len(self.max_heap) < len(self.min_heap):
             heappush(self.max_heap, -heappop(self.min_heap))
         
@@ -27,7 +27,7 @@ class MedianFinder:
         else:
             return (-self.max_heap[0] + self.min_heap[0]) / 2.0
         
-
+        #O(logN)
 
 # Your MedianFinder object will be instantiated and called as such:
 # obj = MedianFinder()
