74c23cc48346b7eaf8cb21b5c409bd4afae4fdb8
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-12 17:08:55 -0700

    Time: 12 ms (96.64%) | Memory: 19.1 MB (90.28%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -1,27 +1,37 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        freq = Counter(tasks)
-        max_heap = []
-        cooldown = deque()
-        timer = 0
+        # freq = Counter(tasks)
+        # max_heap = []
+        # cooldown = deque()
+        # timer = 0
 
-        for k,v in freq.items():
-            heapq.heappush(max_heap, -v)
+        # for k,v in freq.items():
+        #     heapq.heappush(max_heap, -v)
 
-        while max_heap or cooldown:
+        # while max_heap or cooldown:
 
-            if max_heap:
-                task_count = -heapq.heappop(max_heap)
-                if task_count > 1:
-                    cooldown.append((task_count - 1,timer + n + 1))
+        #     if max_heap:
+        #         task_count = -heapq.heappop(max_heap)
+        #         if task_count > 1:
+        #             cooldown.append((task_count - 1,timer + n + 1))
             
-            timer += 1
+        #     timer += 1
 
-            while cooldown and cooldown[0][1] == timer:
-                tc , t = cooldown[0]
-                cooldown.popleft()
-                heappush(max_heap,-tc)
+        #     while cooldown and cooldown[0][1] == timer:
+        #         tc , t = cooldown[0]
+        #         cooldown.popleft()
+        #         heappush(max_heap,-tc)
         
-        return timer
+        # return timer
 
-        #O(klogk) ; O(k)
+        # #O(klogk) ; O(k)
+
+
+        #The code below is more optimal(mathematical approach)
+
+        freq = Counter(tasks)
+        max_freq_task = max(freq.values())
+        num_max_freq_tasks = sum(1 for v in freq.values() if v == max_freq_task)
+
+        return max(len(tasks),(max_freq_task - 1)*(n + 1) + num_max_freq_tasks)
+
