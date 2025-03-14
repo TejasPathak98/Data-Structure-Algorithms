756ab6604965d22d9ce7052b3b3fba6d608fd8c4
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-14 15:22:48 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (23.44%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -2,30 +2,33 @@ class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         if not digits:
             return []
-        
-        digit_map = {
-            "2" : "abc",
-            "3" : "def",
-            "4" : "ghi",
-            "5" : "jkl",
-            "6" : "mno",
-            "7" : "pqrs",
-            "8" : "tuv",
-            "9" : "wxyz"
+
+        char_dict  = {
+            "2" : ["a","b","c"],
+            "3" : ["d","e","f"],
+            "4" : ["g","h","i"],
+            "5" : ["j","k","l"],
+            "6" : ["m","n","o"],
+            "7" : ["p","q","r","s"],
+            "8" : ["t","u","v"],
+            "9" : ["w","x","y","z"]
         }
 
-        result = []
 
-        def helper(pos,temp_string):
-            if len(temp_string) == len(digits):
-                result.append(temp_string)
+        ans = []
+
+        def helper(temp,next_digit):
+            if len(temp) == len(digits):
+                ans.append("".join(temp[:]))
                 return
-            for i in range(pos,len(digits)):
-                for ch in digit_map[digits[i]]:
-                    helper(i + 1,temp_string + ch)
 
-        helper(0,"")
-        return result
+            if next_digit >= len(digits):
+                return
+            
+            for ch in char_dict[digits[next_digit]]:
+                temp.append(ch)
+                helper(temp,next_digit + 1)
+                temp.pop()
 
-
-        
+        helper([],0)
+        return ans
