aa507279057c0991141a26e408e74919d250d362
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-13 22:34:38 -0700

    Time: 7 ms (83.09%) | Memory: 17.8 MB (87.49%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,25 +1,47 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        n = len(candidates)
-        ans = set() 
+        # n = len(candidates)
+        # ans = set() 
 
-        def helper(temp,i):
-            if sum(temp) > target:
+        # def helper(temp,i):
+        #     if sum(temp) > target:
+        #         return
+            
+        #     if sum(temp) == target:
+        #         ans.add(tuple(temp.copy()))
+        #         return
+            
+        #     for j in range(i,n):
+        #         temp.append(candidates[j])
+                
+        #         helper(temp,j + 1)
+
+        #         helper(temp,j)
+
+        #         temp.pop()
+                
+
+        # helper([],0)
+        # return list(ans)
+
+        #the issue in my code is redundant sum calculation and also the helper call should be combined
+
+        n = len(candidates)
+        ans = [] 
+
+        def helper(temp,current_sum,i):
+            if current_sum > target:
                 return
             
-            if sum(temp) == target:
-                ans.add(tuple(temp.copy()))
+            if current_sum == target:
+                ans.append(temp[:])
                 return
             
             for j in range(i,n):
                 temp.append(candidates[j])
-                
-                helper(temp,j + 1)
-
-                helper(temp,j)
-
+                helper(temp,current_sum + candidates[j],j) #Combining those 2 recursive calls
                 temp.pop()
-                
 
-        helper([],0)
-        return list(ans)
+
+        helper([],0,0)
+        return ans
