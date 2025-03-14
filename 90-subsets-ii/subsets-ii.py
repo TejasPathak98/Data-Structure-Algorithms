418bbe9e5508b2390f64747b3ac5b5294678106b
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-13 23:46:32 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (97.52%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -1,26 +1,52 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        if len(nums) == 0:
-            return [[]]
+        # ans = []
+        # n = len(nums)
+
+        # def helper(temp,i):
+        #     if i > len(nums):
+        #         return
+            
+        #     if i == len(nums):
+        #         ans.append(temp[:])
+        #         return
+
+        #     temp.append(nums[i])
+
+        #     helper(temp,i + 1)
+
+        #     temp.pop()
+
+        #     helper(temp,i + 1)
+
         
+        # helper([],0)
+        # return ans
+
         ans = []
-        my_dict = defaultdict(int)
-        
-        def helper(i,temp):
-            if i == len(nums):
-                if tuple(sorted(temp)) in my_dict:
-                    return
-                else:
-                    my_dict[tuple(sorted(temp))] += 1
-                    ans.append(temp.copy())
-                    return
+        n = len(nums)
+        nums.sort()
+
+        def helper(temp,i):
+            # if i > len(nums):
+            #     return
             
-            temp.append(nums[i])
-            helper(i + 1,temp)
-            temp.pop()
-            helper(i + 1,temp)
+            # if i == len(nums):
+            #     ans.append(temp[:])
+            #     return
+
+            #we do not need explicit conditions because the loop handles it, the function naturally returns after the loop is over
+
+            ans.append(temp[:])
+
+            for j in range(i,len(nums)):
+                if j > i and nums[j] == nums[j - 1]:
+                    continue
+                temp.append(nums[j])
+                helper(temp,j + 1)
+                temp.pop()  #.....again here we do not need another call like helper(temp,j + 1) because the loop handles it implicitly
+
         
-        helper(0,[])
+        helper([],0)
         return ans
-            
 
