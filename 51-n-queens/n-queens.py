795f727e72d490622042cf05a9a89fad6fa709dd
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-14 16:14:57 -0700

    Time: 34 ms (25.24%) | Memory: 18.5 MB (16.34%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,38 +1,63 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        def backtrack(r):
-            if r == n:
-                copy = matrix[:]
+        if n == 1:
+            return [["Q"]]
+
+        result = []
+        board = [["."] * n for _ in range(n)]
+        col_set = []
+        row_set = []
+
+
+        def diag_helper(col1,row1,col2,row2):
+            if ((col1 - col2) / (row1 - row2)) in [-1,1]:
+                return True
+            #print(col1,row1,col2,row2)
+            return False
+
+        def backtrack(col,row_num,total_placed,row_set,col_set):
+            if total_placed == n:
                 temp = []
-                for c in copy:
-                    temp.append("".join(c[:]))
-                ans.append(temp)
+                for r in board:
+                    temp.append("".join(r[:]))
+                result.append(temp)
                 return
-            
-            for c in range(n):
-                if c in cols or r + c in posDiag or r - c in negDiag:
+
+            flag = False
+
+            for i in range(n):
+                if i in col_set:
                     continue
+                for k in range(len(col_set)):
+                    if diag_helper(col_set[k],row_set[k],i,row_num + 1) == True:
+                        flag = True
+                        break
                 
-                matrix[r][c] = "Q"
-                cols.add(c)
-                posDiag.add(r + c)
-                negDiag.add(r - c)
+                if flag:
+                    flag = False
+                    continue
 
-                backtrack(r + 1)
+                col_set.append(i)
+                row_set.append(row_num + 1)
+                board[row_num + 1][i] = "Q"
+                print(row_num + 1,i)
+                print("br")
+                backtrack(i, row_num + 1, total_placed + 1,row_set,col_set)
+                board[row_num + 1][i] = "."
+                col_set.pop()
+                row_set.pop()
 
-                matrix[r][c] = "."
-                cols.remove(c)
-                posDiag.remove(r + c)
-                negDiag.remove(r - c)
 
-        matrix = [["."] * n for _ in range(n)]
+        for i in range(n):
+            board[0][i] = "Q"
+            row_set.append(0)
+            col_set.append(i)
+            backtrack(i,0,1,row_set,col_set)
+            board[0][i] = "."
+            col_set.pop()
+            row_set.pop()
 
-        cols =  set()
-        posDiag = set()
-        negDiag = set()
-        ans = []
+        return result
 
-        backtrack(0)
-        return ans
 
 
