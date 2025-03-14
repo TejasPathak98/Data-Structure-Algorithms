5974f4cc29e7b326a07d90308003c255248ffc8f
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-14 14:23:43 -0700

    Time: 51 ms (56.40%) | Memory: 34.4 MB (78.42%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,19 +1,15 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        if not s:
-            return []
-        
-        result = []
+        ans = []
 
         def helper(start,temp):
             if start == len(s):
-                result.append(temp)
+                ans.append(temp[:])
                 return
+            
             for end in range(start + 1,len(s) + 1):
                 if s[start:end] == s[start:end][::-1]:
                     helper(end,temp + [s[start:end]])
         
         helper(0,[])
-        return result
-
-        
+        return ans
