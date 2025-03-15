d3d7771801a81f235a0d24aa3afa83727968fbb1
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-14 17:34:38 -0700

    Time: 15 ms (89.19%) | Memory: 23.3 MB (83.16%) - LeetSync

--- 134-gas-station/gas-station.py
+++ 134-gas-station/gas-station.py
@@ -1,6 +1,17 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        if sum(gas) < sum(cost):
+        # if sum(cost) > sum(gas):
+        #     return -1
+        
+        # for i in range(len(gas)):
+        #     if cost[i] - gas[i] > 0:
+        #         continue
+        #     else:
+        #         return i
+        
+        # return -1
+
+        if sum(cost) > sum(gas):
             return -1
         
         g = 0
@@ -11,10 +22,5 @@ class Solution:
             if g < 0:
                 idx = i + 1
                 g = 0
-
         
         return idx
-
-
-
-        #O(N) ; O(1)
