4d22ad41e65b53de010ff43ffc9cfe1e4ad0160f
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-15 16:44:20 -0700

    Time: 1611 ms (5.20%) | Memory: 50.4 MB (99.32%) - LeetSync

--- 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.py
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.py
@@ -1,16 +1,31 @@
 class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
-        ans = set()
+        index_ignore = []
+        
+        for idx,List in enumerate(triplets):
+            if List[0] > target[0] or List[1] > target[1] or List[2] > target[2]:
+                index_ignore.append(idx)
+        
+        offset = 0
+        for x in index_ignore:
+            triplets.pop(x - offset)
+            offset += 1
+        
+        counter = [0,0,0]
 
-        for t in triplets:
+        print(triplets)
 
-            if t[0] > target[0] or t[1] > target[1] or t[2] > target[2]:
-                continue
+        for idx,List in enumerate(triplets):
+            if List[0] == target[0]:
+                counter[0] = 1
+            if List[1] == target[1]:
+                counter[1] = 1
+            if List[2] == target[2]:
+                counter[2] = 1
             
-            for i,v in enumerate(t):
-                if v == target[i]:
-                    ans.add(i)
-        
-        return len(ans) == 3
+        print(counter)
 
-        
+        if all(counter) == 1:
+            return True
+        else:
+            return False
