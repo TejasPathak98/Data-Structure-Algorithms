698af488424a008fbc3e5387ec05173deb676df0
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-16 14:36:03 -0700

    Time: 0 ms (100.00%) | Memory: 19.9 MB (34.22%) - LeetSync

--- 252-meeting-rooms/meeting-rooms.py
+++ 252-meeting-rooms/meeting-rooms.py
@@ -2,8 +2,10 @@ class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
         intervals = sorted(intervals,key = lambda x : x[0])
 
-        for i in range(len(intervals) - 1):
-            if intervals[i][1] > intervals[i + 1][0]:
+        for i in range(1,len(intervals)):
+            if intervals[i][0] < intervals[i - 1][1]:
                 return False
         
         return True
+
+        #(ONlogN) ; O(N) -> We can make SC O(1) by using .sort()
