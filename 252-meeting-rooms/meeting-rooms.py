e40659c8f40bc3606428b55bdd96ca2ebd225647
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-16 14:26:22 -0700

    Time: 4 ms (32.49%) | Memory: 19.8 MB (34.22%) - LeetSync

--- 252-meeting-rooms/meeting-rooms.py
+++ 252-meeting-rooms/meeting-rooms.py
@@ -1,13 +1,9 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
-        res = float("-inf")
+        intervals = sorted(intervals,key = lambda x : x[0])
 
-        for interval in sorted(intervals,key = lambda x : x[0]):
-            if interval[0] >= res:
-                res = interval[1]
-            else:
+        for i in range(len(intervals) - 1):
+            if intervals[i][1] > intervals[i + 1][0]:
                 return False
         
         return True
-
-        # O(NlogN) ; O(1)
