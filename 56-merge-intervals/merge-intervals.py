21c37479e9130aaad3e3fb7ea631a876a975b9fa
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-16 15:41:15 -0700

    Time: 3 ms (93.48%) | Memory: 20.9 MB (89.23%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,27 +1,17 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        n = len(intervals)
-        intervals = sorted(intervals,key = lambda x : (x[0],x[1]))
-        result = []
-        result.append(intervals[0])
+        intervals.sort(key = lambda x : x[0])
+        ans = []
+        prev_end_time = float("-inf")
 
-        i = 1
+        for i in range(len(intervals)):
+            if intervals[i][0] > prev_end_time:
+                ans.append(intervals[i])
+            else:
+                ans[-1][0] = min(ans[-1][0],intervals[i][0])
+                ans[-1][1] = max(ans[-1][1],intervals[i][1])
+            prev_end_time = ans[-1][1]
 
-
-        while i < len(intervals):
-            if intervals[i][0] > result[-1][1]:
-                result.append(intervals[i])
-                i += 1
-            elif intervals[i][0] <= result[-1][1]:
-                entry = min(result[-1][0],intervals[i][0])
-                the_exit = max(result[-1][1],intervals[i][1])
-                result[-1][0] = entry
-                result[-1][1] = the_exit
-                i += 1
-
-        return result
-
-        #O(NlogN) ;O(N)
+        return ans
             
 
-        
