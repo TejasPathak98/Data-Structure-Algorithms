fbe75ac87ef2342f62ca5621968dc43bfa78fe0e
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-16 16:51:14 -0700

    Time: 292 ms (51.01%) | Memory: 60.6 MB (30.98%) - LeetSync

--- 1977-minimum-interval-to-include-each-query/minimum-interval-to-include-each-query.py
+++ 1977-minimum-interval-to-include-each-query/minimum-interval-to-include-each-query.py
@@ -1,25 +1,29 @@
 class Solution:
     def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
-        intervals = sorted(intervals, key = lambda x : x[0])
-        query = ((q,i) for i,q in enumerate(queries))
-        query = sorted(query , key = lambda x : x[0])
+        intervals.sort(key = lambda x : x[0]) #Sort with respect to begintime
+        #We sort the queries too as we can move forward "just-intime appraoch" and not backtrack
 
-        min_heap = []
-        i = 0
-        result = [-1] * len(query)
+        q = [(query,idx) for idx,query in enumerate(queries)]
+        q.sort(key = lambda x : x[0])
 
-        for q,idx in query:
-            while i < len(intervals) and q >= intervals[i][0]:
-                heapq.heappush(min_heap, (intervals[i][1] - intervals[i][0] + 1 , intervals[i][1]))
+        i = 0 # this helps us to process the interals list only once
+        min_heap = [] # helps us to get the min-interval length of each query
+        result = [-1] * len(queries)
+
+        for query,idx in q:
+            while i < len(intervals) and query >= intervals[i][0]:
+                heappush(min_heap, ((intervals[i][1] - intervals[i][0] + 1),intervals[i][1]))
                 i += 1
             
-            while min_heap and min_heap[0][1] < q:
-                heapq.heappop(min_heap)
+            while min_heap and min_heap[0][1] < query:
+                heappop(min_heap)
             
-            result[idx] = min_heap[0][0] if min_heap else -1
-
+            if min_heap:
+                result[idx] = min_heap[0][0]
+            else:
+                result[idx] = -1
+        
         return result
 
 
 
-        
