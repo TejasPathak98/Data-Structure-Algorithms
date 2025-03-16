8eac240bf3c92b4d0bc17fefcfeac723c9ab86c3
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-15 17:19:40 -0700

    Time: 3 ms (90.98%) | Memory: 17.9 MB (14.55%) - LeetSync

--- 768-partition-labels/partition-labels.py
+++ 768-partition-labels/partition-labels.py
@@ -1,22 +1,22 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
         L = len(s)
-        last = {s[i] : i for i in range(L)}
+        s_dict = {s[i] : i for i in range(L)}
 
         ans = []
         i = 0
 
         while i < L:
-            j = i + 1
-            end = last[s[i]]
+            j = i
+            end = s_dict[s[j]]
+
             while j < end:
-                if last[s[j]] > end:
-                    end = last[s[j]]
+                if s_dict[s[j]] > end:
+                    end = s_dict[s[j]]
                 j += 1
             
             ans.append(end - i + 1)
             i = end + 1
         
-        return ans
 
-        
+        return ans
