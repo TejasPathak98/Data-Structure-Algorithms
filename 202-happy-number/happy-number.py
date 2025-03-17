d1c0d3401a934d81656d1c5827aae835c85735b6
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-16 17:05:39 -0700

    Time: 3 ms (42.01%) | Memory: 17.9 MB (17.67%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,20 +1,25 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
+        if n == 1:
+            return True
+
+        s = set()
+
         def helper(x):
-            sq_sum = 0
+            if x == 1:
+                return True
+            if x in s:
+                return False
             
-            while x:
-                sq_sum += (x% 10) * (x % 10)
+            s.add(x)
+
+            the_sum = 0
+
+            while x > 0:
+                the_sum += (x % 10) * (x % 10)
                 x = x // 10
             
-            return sq_sum
+            return helper(the_sum)
 
-        seen = set()
-        while n != 1 and n not in seen:
-            seen.add(n)
-            n = helper(n)
-            
-        
-        return n == 1
 
-        
+        return helper(n)
