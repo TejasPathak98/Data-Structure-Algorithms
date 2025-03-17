8438b3b63100fca18f3d90c64be7aeef6a0402ca
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-16 17:30:30 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (81.27%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.py
+++ 191-number-of-1-bits/number-of-1-bits.py
@@ -1,9 +1,20 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        res = 0
-        for i in range(32):
-            if (n >> i) & 1:
-                res += 1
+        # ans = 0
+        # for _ in range(32):
+        #     if (n >> 1) & 1 == 1:
+        #         ans += 1
+        #     n = n >> 1
         
-        return res
+        # return ans
+
+
+        ans = 0
+        for _ in range(32):
+            if (n & 1) == 1: #Check the LSB and then shift
+                ans += 1
+            n = n >> 1
         
+        return ans
+
+        #Earlier I was not checking the LSB at all
