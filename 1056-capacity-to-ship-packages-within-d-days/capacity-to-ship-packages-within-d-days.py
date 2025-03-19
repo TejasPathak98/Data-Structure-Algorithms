c5ccb5740352c33cfc7d9d8ed444e9671077d19d
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-19 01:56:27 -0700

    Time: 383 ms (7.95%) | Memory: 22.2 MB (55.29%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -1,33 +1,46 @@
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
+        #days = total_weight / Ship capacity
+
         l = max(weights)
         r = sum(weights)
+        
+        while l <= r:
+            mid = (l + r) // 2
 
-        while l < r:
-            m = (l + r) // 2
-            if self.helper(m,weights) > days:
-                l = m + 1
+            no_of_days = self.helper(weights,mid)
+
+            if no_of_days > days:
+                l = mid + 1
             else:
-                r = m
-    
+                r = mid - 1
+
+        
         return l
+            
+    def helper(self,weights,ship_weight):
+        the_days = 0
+        i = 0
+        temp_sum = 0
 
-    def helper(self,ship_capacity,weights):
-        days = 1
-        capacity = 0
-
-        for weight in weights:
-            if capacity + weight > ship_capacity:
-                days += 1
-                capacity = weight
+        while i < len(weights):
+            temp_sum += weights[i]
+            if temp_sum == ship_weight:
+                the_days += 1
+                temp_sum = 0
+                i += 1
+            elif temp_sum > ship_weight:
+                the_days += 1
+                temp_sum = 0
             else:
-                capacity += weight
+                i += 1
         
-        return days
-    
+        if temp_sum > 0:
+            the_days += 1
         
+        return the_days
 
 
+            
 
 
-        
