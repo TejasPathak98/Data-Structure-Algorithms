e6682abb060127c339de5e87e3eda961bf4dc045
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-21 00:58:39 -0700

    Time: 44 ms (29.26%) | Memory: 18.4 MB (39.38%) - LeetSync

--- 758-convert-binary-search-tree-to-sorted-doubly-linked-list/convert-binary-search-tree-to-sorted-doubly-linked-list.py
+++ 758-convert-binary-search-tree-to-sorted-doubly-linked-list/convert-binary-search-tree-to-sorted-doubly-linked-list.py
@@ -6,34 +6,38 @@ class Node:
         self.left = left
         self.right = right
 """
+
 class Solution:
     def treeToDoublyList(self, root: 'Optional[Node]') -> 'Optional[Node]':
         if not root:
-            return None
+            return
         
+        def dfs_linker(root):
+            if not root:
+                return
+            
+            dfs_linker(root.left)
+
+            if not self.first:
+                self.first = root
+            else:
+                self.last.right = root
+                root.left = self.last
+
+            self.last = root
+
+            dfs_linker(root.right)
+
+
+
         self.first = None
         self.last = None
 
-        self.dfs_linker(root)
+        dfs_linker(root)
 
         self.first.left = self.last
         self.last.right = self.first
 
         return self.first
-
-    def dfs_linker(self,root):
-        if not root:
-            return
-        
-        self.dfs_linker(root.left)
-        
-        if not self.last:
-            self.first = root
-        else:
-            self.last.right = root
-            root.left = self.last
-        
-        self.last = root
-
-
-        self.dfs_linker(root.right)
+    
+    
