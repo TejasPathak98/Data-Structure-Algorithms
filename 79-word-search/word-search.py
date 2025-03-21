03f494755531e35dc8acc0e0828602ee17d3371b
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-20 22:16:49 -0700

    Time: 529 ms (95.31%) | Memory: 17.9 MB (64.93%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,54 +1,50 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        m = len(board)
-        n = len(board[0])
-        lw = len(word)
+        board_str = ""
+        for row in board:
+            board_str += "".join(row)
+        
+        print(board_str)
 
-        board_freq = Counter(sum(board,[]))
-        word_freq = Counter(word)
+        board_counter = Counter(board_str)
+        word_counter = Counter(word)
 
-        for char in word:
-            if word_freq[char] > board_freq.get(char,0):
+        for ch in word:
+            if word_counter[ch] > board_counter[ch]:
+                print("Here")
                 return False
         
-        def neighbor_rarity(x,y):
-            counter = 0
+        m = len(board)
+        n = len(board[0])
+
+        def backtracking(i,j,pos):
+            if pos == len(word):
+                return True
+            
+            if i < 0 or i >= m or j < 0 or j >= n or word[pos] != board[i][j]:
+                return False
+            
+            temp = board[i][j]
+            board[i][j] = "#"
+
             for dx,dy in [(0,1),(0,-1),(1,0),(-1,0)]:
-                if x + dx >= 0 and x + dx  < m and y +dy >= 0 and y + dy < n and board_freq.get(board[x + dx][y + dy],0) > 0:
-                    counter += board_freq.get(board[x + dx][y + dy])
-            return counter
-        
-        positions = []
+                x_ = i + dx
+                y_ = j + dy
+
+                if backtracking(x_, y_, pos + 1):
+                    return True
+
+
+            board[i][j] = temp
 
         for i in range(m):
             for j in range(n):
                 if board[i][j] == word[0]:
-                    positions.append((i,j))
-
-        positions.sort(key = lambda pos : neighbor_rarity(*pos))
-
-        def backtrack(x,y,ptr):
-            if ptr == lw:
-                return True
-            
-            if x < 0 or x >= m or y < 0 or y >= n or board[x][y] != word[ptr]:
-                return False
-            
-            temp = board[x][y]
-            board[x][y] = "#"
-
-            for dx,dy in [(0,1),(0,-1),(1,0),(-1,0)]:
-                if backtrack(x + dx,y + dy,ptr + 1):
-                    return True
-                
-            board[x][y] = temp
-            return False
-
-        
-        for i,j in positions:
-            if backtrack(i,j,0):
-                return True
+                    if backtracking(i,j,0):
+                        return True
         
         return False
 
 
+
+        
