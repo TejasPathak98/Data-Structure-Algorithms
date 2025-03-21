f8c6fa3b69a474eb149d22bc11ba51f91609c3a3
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-20 22:58:38 -0700

    Time: 3 ms (39.81%) | Memory: 18 MB (37.14%) - LeetSync

--- 269-alien-dictionary/alien-dictionary.py
+++ 269-alien-dictionary/alien-dictionary.py
@@ -1,25 +1,25 @@
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
         graph = defaultdict(set)
-        inDegree = {char : 0 for word in words for char in word}
+        inDegree = {char:0 for word in words for char in word}
         result = []
 
-        # Buidling the graph and inDegree Map
         for i in range(len(words) - 1):
             word1 = words[i]
             word2 = words[i + 1]
-            min_word_len = min(len(word1),len(word2))
 
-            if word1[:min_word_len] == word2[:min_word_len] and len(word1) > len(word2):
+            min_len = min(len(word1),len(word2))
+
+            if word2[:min_len] == word1[:min_len] and len(word1) > len(word2):
                 return ""
             
-            for j in range(min_word_len):
+            for j in range(min_len):
                 if word1[j] != word2[j]:
                     if word2[j] not in graph[word1[j]]:
                         graph[word1[j]].add(word2[j])
                         inDegree[word2[j]] += 1
                     break
-        
+            
         queue = deque([char for char in inDegree if inDegree[char] == 0])
 
         while queue:
@@ -31,5 +31,12 @@ class Solution:
                 if inDegree[neighbor] == 0:
                     queue.append(neighbor)
 
+        if len(result) == len(inDegree):
+            return "".join(result)
+        else:
+            return ""
 
-        return "".join(result) if len(inDegree) == len(result) else ""
+
+            
+
+
