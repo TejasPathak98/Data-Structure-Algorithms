96670eba376e317e87b5f761cbfba0106e7c19f8
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-25 19:48:28 -0700

    Time: 7 ms (66.78%) | Memory: 18.4 MB (12.53%) - LeetSync

--- 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.py
+++ 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.py
@@ -4,19 +4,15 @@ class Solution:
         ans = 0
         i = 0
 
-        print(boxTypes)
-
         while i < len(boxTypes):
             units = boxTypes[i][0] * boxTypes[i][1]
             if boxTypes[i][0] < truckSize:
                 ans += units
                 truckSize -= boxTypes[i][0]
                 i += 1
-                print(ans,units,truckSize)
             else:
                 no_of_boxes = truckSize
                 ans += boxTypes[i][1] * no_of_boxes
-                print(ans)
                 break
         
         return ans
