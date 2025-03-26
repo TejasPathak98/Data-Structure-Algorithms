a0c0dc3577d273357b4aaa162c14d8dc2b212bce
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-25 19:19:44 -0700

    Time: 5 ms (86.96%) | Memory: 17.9 MB (61.93%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,47 +1,23 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        # n = len(candidates)
-        # ans = set() 
+        ans = []
 
-        # def helper(temp,i):
-        #     if sum(temp) > target:
-        #         return
-            
-        #     if sum(temp) == target:
-        #         ans.add(tuple(temp.copy()))
-        #         return
-            
-        #     for j in range(i,n):
-        #         temp.append(candidates[j])
-                
-        #         helper(temp,j + 1)
 
-        #         helper(temp,j)
-
-        #         temp.pop()
-                
-
-        # helper([],0)
-        # return list(ans)
-
-        #the issue in my code is redundant sum calculation and also the helper call should be combined
-
-        n = len(candidates)
-        ans = [] 
-
-        def helper(temp,current_sum,i):
+        def backtracking(current_sum,temp,idx):
             if current_sum > target:
                 return
             
             if current_sum == target:
-                ans.append(temp[:])
+                ans.append(temp.copy())
                 return
             
-            for j in range(i,n):
+
+            for j in range(idx,len(candidates)):
                 temp.append(candidates[j])
-                helper(temp,current_sum + candidates[j],j) #Combining those 2 recursive calls
+                backtracking(current_sum + candidates[j],temp,j)
                 temp.pop()
 
+        backtracking(0, [], 0)
 
-        helper([],0,0)
         return ans
+            
