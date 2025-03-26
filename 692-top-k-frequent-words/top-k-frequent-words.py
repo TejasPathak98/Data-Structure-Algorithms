ae98f455ce13b071ddf15c67df554f2903fb5ad1
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-26 15:46:50 -0700

    Time: 0 ms (100.00%) | Memory: 18.1 MB (21.54%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.py
+++ 692-top-k-frequent-words/top-k-frequent-words.py
@@ -17,14 +17,19 @@ class Solution:
         
         # return result[::-1]
 
-        counter = Counter(words)
-        max_heap = [(-freq,word) for word,freq in counter.items()]
-        heapify(max_heap)
-        result = []
+        # counter = Counter(words)
+        # max_heap = [(-freq,word) for word,freq in counter.items()]
+        # heapify(max_heap)
+        # result = []
 
-        for _ in range(k):
-            result.append(heapq.heappop(max_heap)[1])
+        # for _ in range(k):
+        #     result.append(heapq.heappop(max_heap)[1])
         
-        return result
+        # return result
 
+        #O(N + klogN)
+
+        counter = Counter(words)
+
+        return nsmallest(k, counter.keys() ,key = lambda x : (-counter[x],x))
 
