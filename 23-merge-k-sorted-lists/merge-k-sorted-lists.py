87f76a3e966cadf7fefe5bb861c96d3babccfd07
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-26 15:08:19 -0700

    Time: 11 ms (63.96%) | Memory: 20.5 MB (18.99%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -5,45 +5,79 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+    #     if  len(lists) == 0 or not lists:
+    #         return None
+
+    #     while len(lists) >= 2:
+    #         x = lists.pop()
+    #         if lists:
+    #             y = lists.pop()
+    #         else:
+    #             return x
+    #         lists.append(self.merge_two_lists(x,y))
+
+    #     return lists[0]
+        
+    # def merge_two_lists(self,l1,l2):
+    #     if not l1 and not l2:
+    #         return None
+    #     if not l1:
+    #         return l2
+    #     if not l2:
+    #         return l1
+        
+    #     l3 = dummy = ListNode(-1)
+
+
+    #     while l1 and l2:
+    #         if l1.val <= l2.val:
+    #             l3.next = l1
+    #             l1 = l1.next
+    #         else:
+    #             l3.next = l2
+    #             l2 = l2.next
+    #         l3 = l3.next
+        
+    #     if l1:
+    #         l3.next = l1
+    #     elif l2:
+    #         l3.next = l2
+        
+    #     return dummy.next
+
+
+    #     while len(lists) >= 2:
+    #         x = lists.pop()
+    #         if lists:
+    #             y = lists.pop()
+    #         else:
+    #             return x
+    #         lists.append(self.merge_two_lists(x,y))
+
+    #     return lists[0]
+
+    #O (Nk)
+
         if  len(lists) == 0 or not lists:
             return None
-
-        while len(lists) >= 2:
-            x = lists.pop()
-            if lists:
-                y = lists.pop()
-            else:
-                return x
-            lists.append(self.merge_two_lists(x,y))
-
-        return lists[0]
         
-    def merge_two_lists(self,l1,l2):
-        if not l1 and not l2:
-            return None
-        if not l1:
-            return l2
-        if not l2:
-            return l1
-        
-        l3 = dummy = ListNode(-1)
+        min_heap = []
 
-
-        while l1 and l2:
-            if l1.val <= l2.val:
-                l3.next = l1
-                l1 = l1.next
-            else:
-                l3.next = l2
-                l2 = l2.next
-            l3 = l3.next
+        for idx,node in enumerate(lists):
+            if node:
+                heapq.heappush(min_heap, (node.val,idx,node))
         
-        if l1:
-            l3.next = l1
-        elif l2:
-            l3.next = l2
+        dummy = curr = ListNode(-1)
+
+        while min_heap:
+            val,idx,node = heapq.heappop(min_heap)
+            curr.next = node
+            curr = curr.next
+            if node.next:
+                heapq.heappush(min_heap, (node.next.val,idx,node.next))
         
         return dummy.next
+
     
     
         
