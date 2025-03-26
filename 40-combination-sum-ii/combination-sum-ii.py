c48a04613fc659089b5109d8879bc5c941b91935
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-25 19:34:34 -0700

    Time: 3 ms (91.53%) | Memory: 17.8 MB (57.00%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -1,77 +1,25 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        # #candidates = list(set(candidates))
-        # candidates = sorted(candidates)
-        # n = len(candidates)
-        # ans = set()
-
-        # def helper(temp,i,current_sum):
-        #     if current_sum > target:
-        #         return
-            
-        #     if current_sum == target:
-        #         ans.add(tuple(temp[:]))
-        #         return
-            
-        #     if i >= n:
-        #         return
-            
-        #     temp.append(candidates[i])
-        #     helper(temp,i + 1,current_sum + candidates[i])
-
-        #     temp.pop()
-        #     helper(temp,i + 1,current_sum)
-        
-
-        # helper([],0,0)
-        # return list(ans)
-
-        #this(above) gives TLE 
-
-        # candidates = sorted(candidates)
-        # n = len(candidates)
-        # ans = set()
-
-        # def helper(temp,i,current_sum):
-        #     if current_sum > target:
-        #         return
-            
-        #     if current_sum == target:
-        #         ans.add(tuple(temp[:]))
-        #         return
-            
-        #     for j in range(i,n):
-        #         if current_sum + candidates[j] > target:
-        #             return
-        #         temp.append(candidates[j])
-        #         helper(temp,j + 1,current_sum + candidates[j])
-        #         temp.pop()
-        #         helper(temp,j + 1,current_sum)  # this call is redudant because the  loop handles it implicitly
-
-            
-        # helper([],0,0,)
-        # return list(ans)
-
-
-        candidates = sorted(candidates)
-        n = len(candidates)
-
+        candidates.sort()
         ans = []
 
-        def helper(temp,i,current_sum):            
-            if current_sum == target:
-                ans.append(temp[:])
-                return  
+        def backtracking(temp,current_sum,idx):
+            if current_sum > target:
+                return
             
-            for j in range(i,n):
-                if j > i and candidates[j] == candidates[j - 1]:
+            if current_sum == target:
+                ans.append(temp.copy())
+                return
+            
+            for j in range(idx,len(candidates)):
+                if candidates[j] == candidates[j - 1] and j > idx:
                     continue
                 if current_sum + candidates[j] > target:
                     break
                 temp.append(candidates[j])
-                helper(temp,j + 1,current_sum + candidates[j])
+                backtracking(temp, current_sum + candidates[j], j + 1)
                 temp.pop()
 
-        
-        helper([],0,0)
+        backtracking([], 0, 0)
         return ans
+
