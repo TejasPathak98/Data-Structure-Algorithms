67eb2d0fea5aa9b6e41fc627334d0c8404ec9909
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-26 16:51:42 -0700

    Time: 347 ms (55.08%) | Memory: 25.5 MB (35.22%) - LeetSync

--- 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.py
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -1,34 +1,34 @@
 class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
-        n = len(grid)
-        m = len(grid[0])
+        m = len(grid)
+        n = len(grid[0])
 
-        queue = deque([(0,0,k)])
-        dir = [(-1,0),(1,0),(0,1),(0,-1)]
-        steps = 0
+        #BFS with constraints
+
+        queue = deque([(0,0,0)])
+        steps = -1
         visited = set()
-        visited.add((0,0,k))
-        
-        while queue:
-            size = len(queue)
-            for _ in range(size):
-                r,c,rem = queue.popleft()
+        visited.add((0,0,0))
 
-                if r == n - 1 and c == m - 1:
+        while queue:
+            steps += 1
+            
+            for _ in range(len(queue)):
+
+                x,y,obstacle_count = queue.popleft()
+
+                if x == m - 1 and y == n - 1:
                     return steps
                 
-                for dx,dy in dir:
-                    rx = dx + r
-                    cy = dy + c
+                for dx,dy in [(0,1),(0,-1),(1,0),(-1,0)]:
+                    x_ = x + dx
+                    y_ = y + dy
 
-                    if rx < 0 or cy < 0 or rx >= n or cy >= m:
-                        continue
-                    elif grid[rx][cy] == 1 and rem > 0 and (rx,cy,rem - 1) not in visited:
-                        queue.append((rx,cy,rem - 1))
-                        visited.add((rx,cy,rem - 1))
-                    elif grid[rx][cy] == 0 and (rx,cy,rem) not in visited:
-                        queue.append((rx,cy,rem))
-                        visited.add((rx,cy,rem))
-            steps +=1
-        
+                    if 0 <= x_ < m and 0 <= y_ < n:
+                        new_obstacle_count = obstacle_count + grid[x_][y_]
+                        if new_obstacle_count <= k and (x_,y_,new_obstacle_count) not in visited:
+                            queue.append((x_,y_,new_obstacle_count))
+                            visited.add((x_,y_,new_obstacle_count))
+
         return -1
+
