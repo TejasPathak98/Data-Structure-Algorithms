f237d736b975260a4009f5a284eee3f4af9b1ee1
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-25 22:52:49 -0700

    Time: 93 ms (18.30%) | Memory: 20.9 MB (5.66%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -1,33 +1,32 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
 
-        # The key idea is the inclusion of stops in the min_heap and cost_dict, as the stops is another factor in finding the lowest cost, which is different than a standard Dijkstra problem
-
         graph = defaultdict(list)
 
-        for s,d,cost in flights:
-            graph[s].append((d,cost))
+        for u,v,w in flights:
+            graph[u].append((v,w))
         
-        min_heap = [(0,src,0)] #cost,node,stops
-        cost_dict = {} # (node,stops) : (cost)
+        min_heap = [(0,src,0)]
+
+        best = dict()
 
         while min_heap:
-            curr_cost, node , stops = heapq.heappop(min_heap)
+            cost,node,steps = heapq.heappop(min_heap)
 
-            if node == dst and stops <= k + 1:
-                return curr_cost
-            
-            if stops > k + 1:
+            if steps > k + 1:
                 continue
 
-            for neighbor,cost in graph[node]:
-                new_cost = cost + curr_cost
-                
-                if (neighbor,stops + 1) not in cost_dict or new_cost < cost_dict.get((neighbor,stops + 1)):
-                    heapq.heappush(min_heap, (new_cost,neighbor,stops + 1))
-                    cost_dict[(neighbor,stops + 1)] = new_cost
+            if node == dst:
+                return cost
 
+            if (node,steps) in best and best[(node,steps)] <= cost:
+                continue
+
+            best[(node,steps)] = cost
+
+            for neighbor,cost_weight in graph[node]:
+                heapq.heappush(min_heap, (cost + cost_weight,neighbor,steps + 1))
         
         return -1
-            
 
+            
