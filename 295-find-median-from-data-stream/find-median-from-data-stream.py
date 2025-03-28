a1ba1d3439028b8832371ce9b8c92a31cfa9c363
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-27 21:31:48 -0700

    Time: 862 ms (7.89%) | Memory: 39.3 MB (87.57%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -15,14 +15,16 @@ class MedianFinder:
             else:
                 l = mid + 1
         
-        self.arr.insert(l, num)
-
+        self.arr.insert(l, num)  # TC : O(n ^2)
 
     def findMedian(self) -> float:
         if len(self.arr) % 2 == 1:
             return self.arr[len(self.arr) // 2]
         else:
             return (self.arr[len(self.arr) // 2] + self.arr[(len(self.arr) // 2) -1]) / 2.0
+
+
+    # TC : O(n ^2)
         
 
 
@@ -30,3 +32,10 @@ class MedianFinder:
 # obj = MedianFinder()
 # obj.addNum(num)
 # param_2 = obj.findMedian()
+
+
+# If num is greater than all elements, l ends up at the end, so num is inserted at the end.
+
+# If num is smaller than all elements, l ends up at 0, so num is inserted at the beginning.
+
+# Otherwise, l points to the exact position where num fits.
