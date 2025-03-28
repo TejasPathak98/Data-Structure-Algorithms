bd67b5d0d3da891695d6f02fa8fffb2d32ebde8c
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-27 21:19:53 -0700

    Time: 0 ms (100.00%) | Memory: 18.1 MB (19.36%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -9,11 +9,9 @@ class Solution:
         while l < h: #(not l <=h becauase we want mid + 1 to be valid)
             mid = (l + h) // 2
 
-            if nums[mid] > nums[mid - 1] and nums[mid] > nums[mid + 1]:
-                return mid
-            elif nums[mid] < nums[mid + 1]: #The peak element must exist on the right side
+            if nums[mid] < nums[mid + 1]: #The peak element must exist on the right side
                 l = mid + 1
             else: #the peak element must exist on left side
                 h = mid
         
-        return l #because in case of strictly increasing arr l will point to last element and in opposite case the first element
+        return h #because in case of strictly increasing arr l will point to last element and in opposite case the first element
