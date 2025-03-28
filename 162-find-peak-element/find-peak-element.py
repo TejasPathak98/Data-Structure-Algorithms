7657585873e00cfa5261225efe10fef36d4977d2
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-27 20:16:04 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (62.12%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -1,27 +1,19 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        n = len(nums)
-        if n == 1:
-            return 0
-        if n == 2:
-            if nums[0] > nums[1]:
-                return 0
-            else:
-                return 1
-
         l = 0
-        h = n - 1
+        h = len(nums) - 1
 
-        while l < h:
+        if len(nums) == 1:
+            return 0
+
+        while l < h: #(not l <=h becauase we want mid + 1 to be valid)
             mid = (l + h) // 2
 
             if nums[mid] > nums[mid - 1] and nums[mid] > nums[mid + 1]:
                 return mid
-            
-            elif nums[mid] < nums[mid + 1]:
+            elif nums[mid] < nums[mid + 1]: #The peak element must exist on the right side
                 l = mid + 1
-            else:
-                h = mid - 1
+            else: #the peak element must exist on left side
+                h = mid
         
-        return l
-        
+        return l #because in case of strictly increasing arr l will point to last element and in opposite case the first element
