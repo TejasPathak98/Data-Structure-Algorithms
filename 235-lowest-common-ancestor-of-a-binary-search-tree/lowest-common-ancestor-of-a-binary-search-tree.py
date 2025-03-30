aa167b39dcbfdc8f4d8c7f9448b00d796df8ff83
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-29 19:20:40 -0700

    Time: 56 ms (72.38%) | Memory: 21.2 MB (20.03%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,45 +7,17 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        # if root is None:
-        #     return None
-        # if root == p or root == q:
-        #     return root
-        # if p is None:
-        #     return q
-        # if q is None:
-        #     return p
+        if root == None:
+            return None
+
+        if root == p or root == q:
+            return root
         
-        # l = self.lowestCommonAncestor(root.left, p, q)
-        # r = self.lowestCommonAncestor(root.right, p, q)
+        l = self.lowestCommonAncestor(root.left, p, q)
+        r = self.lowestCommonAncestor(root.right, p, q)
 
-        # if l and r:
-        #     return root
+        if l and r:
+            return root
         
-        # return l if l else r
-
-        # #O(N) ; O(N)
-
-        parent = {} #for buidling the parent map
-        stack = [root] #for iterative dfs
-
-        while stack:
-            node = stack.pop()
-
-            if node.left:
-                parent[node.left] = node
-                stack.append(node.left)
-            
-            if node.right:
-                parent[node.right] = node
-                stack.append(node.right)
-
-        ancestors = set() #parent set of p right up till the root
-        while p:
-            ancestors.add(p)
-            p = parent.get(p)
-
-        while q not in ancestors: #Going up p's ancestors for checking the lca
-            q = parent.get(q)
-
-        return q
+        else:
+            return l if l else r
