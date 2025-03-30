ce723ea535e0957e0dadc0448298d3bc32943c97
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-30 14:47:11 -0700

    Time: 23 ms (85.88%) | Memory: 18.7 MB (24.78%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.py
+++ 794-swim-in-rising-water/swim-in-rising-water.py
@@ -1,32 +1,32 @@
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
-        n = len(grid[0])
-
-        time_taken = 0
-        time = [[float("inf")] * n for _ in range(n)]
-        time[0][0] = grid[0][0]
+        #This is Dijsktra problem because the weights are implicit, we are finding the most optimal at very step
 
         min_heap = [(grid[0][0],0,0)]
+        visited = set([0,0])
+        directions = [(0,1),(0,-1),(1,0),(-1,0)]
+        n = len(grid)
 
         while min_heap:
             t , x, y = heapq.heappop(min_heap)
-            time_taken = max(time_taken,t)
 
             if x == n - 1 and y == n - 1:
-                return time_taken
-
-            for dx,dy in [(0,1),(0,-1),(1,0),(-1,0)]:
+                return t
+            
+            for dx,dy in directions:
                 x_ = x + dx
-                y_ = y  + dy
+                y_= y + dy
 
-                if 0 <= x_ < n and 0 <= y_ < n:
-                    new_time = max(t,grid[x_][y_])
-                    if new_time < time[x_][y_] and time[x_][y_] == float("inf"):
-                        time[x_][y_] = new_time
-                        heapq.heappush(min_heap, (grid[x_][y_],x_,y_))
+                if 0 <= x_ < n and 0 <= y_ < n and (x_,y_) not in visited:
+                    new_t= max(t,grid[x_][y_])
+                    heapq.heappush(min_heap, (new_t,x_,y_))
+                    visited.add((x_,y_))
 
-
-        return -1
         
-        #O(MNlog(MN)) ; O(MN)
-                
+        return -1
+
+
+    
+
+            
+
