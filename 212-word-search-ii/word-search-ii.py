1ced8ceb109dac70b156e013ad278dec33aa270d
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-30 22:58:59 -0700

    Time: 391 ms (97.77%) | Memory: 18.5 MB (95.72%) - LeetSync

--- 212-word-search-ii/word-search-ii.py
+++ 212-word-search-ii/word-search-ii.py
@@ -1,44 +1,43 @@
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-
-        def dfs(x,y,trie):
-            letter = board[x][y]
+        
+        def dfs(i,j,trie):
+            letter = board[i][j]
             curr = trie[letter]
 
             w = curr.pop('#',False)
+
             if w:
-                res.append(w)
+                result.append(w)
             
-            board[x][y] = "*"
-            
-            for dx,dy in [(0,1),(1,0),(0,-1),(-1,0)]:
-                x_ = x + dx
-                y_ = y + dy
-                if 0 <= x_ < m and 0 <= y_ < n and board[x_][y_] in curr:
-                    dfs(x_,y_,curr)
-            
-            board[x][y] = letter
+            board[i][j] = "*"
+
+            for dx,dy in [(0,1),(0,-1),(1,0),(-1,0)]:
+                x = dx + i
+                y = dy + j
+
+                if 0 <= x < m and 0 <= y < n and board[x][y] in curr:
+                    dfs(x,y,curr)
+
+            board[i][j] = letter
 
             if not curr:
                 trie.pop(letter)
 
         trie = {}
-        res = []
+        result = []
+        m = len(board)
+        n = len(board[0])
+
         for word in words:
             curr = trie
             for letter in word:
                 curr = curr.setdefault(letter,{})
             curr['#'] = word
         
-        m = len(board)
-        n = len(board[0])
-
         for i in range(m):
             for j in range(n):
                 if board[i][j] in trie:
                     dfs(i,j,trie)
         
-        return res
-
-
-        
+        return result
