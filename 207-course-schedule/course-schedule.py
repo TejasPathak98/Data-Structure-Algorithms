9006351f1facad8b2722b698092a326f6b12a900
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-30 21:33:59 -0700

    Time: 9 ms (20.17%) | Memory: 19.4 MB (41.04%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,65 +1,33 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-    #     graph = defaultdict(list)
-    #     InDegree = [0] * numCourses
-
-    #     for course,pre_req in prerequisites:
-    #         graph[pre_req].append(course)
-    #         InDegree[course] += 1
-        
-    #     queue = deque()
-    #     for course in range(numCourses):
-    #         if InDegree[course] == 0:
-    #             queue.append(course)
-        
-    #     return self.topological_sort(graph,queue,numCourses,InDegree)
-
-    # def topological_sort(self,graph,queue,numCourses,InDegree):
-    #     courses_done = []
-
-    #     while queue:
-    #         course = queue.popleft()
-    #         courses_done.append(course)
-
-    #         for related_course in graph[course]:
-    #             InDegree[related_course] -= 1
-
-    #             if InDegree[related_course] == 0:
-    #                 queue.append(related_course)
-
-        
-    #     return len(courses_done) == numCourses
-
-    # #o(V + E) ; O(V + E)...for graph + queue
-
-
         graph = defaultdict(list)
+        inDegree = [0] * numCourses
+
         for course,pre_req in prerequisites:
-            graph[course].append(pre_req)
+            graph[pre_req].append(course)
+            inDegree[course] += 1
 
-        visited = [0] * numCourses # 1 is visiting, 2 is visited , 0 is un visited
+        visited = [0] * numCourses # 0 for unvisited , 1 for visiting , 2 and for visited
 
-
-        def hasCycle(course):
-            if visited[course] == 1:
+        def dfs(node):
+            if visited[node] == 1:
+                return False
+            if visited[node] == 2:
                 return True
-            if visited[course] == 2:
-                return False
-
             
-            visited[course] = 1
-            for pre_req in graph[course]:
-                if hasCycle(pre_req):
-                    return True
-            visited[course] = 2
-            return False
+            visited[node] = 1
 
+            for nei in graph[node]:
+                if not dfs(nei):
+                    return False
+            
+            visited[node] = 2
+            return True
         
-        for course in range(numCourses):
-            if hasCycle(course):
+        for i in range(numCourses):
+            if not dfs(i):
                 return False
+        
         return True
 
-
-
-
+            
