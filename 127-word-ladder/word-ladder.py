6c20980bbf51c26106d443b048d9f65ee50e6e65
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-30 20:09:17 -0700

    Time: 7820 ms (6.48%) | Memory: 18.6 MB (92.90%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -1,66 +1,44 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        # if endWord not in wordList:
-        #     return 0
-
-        # queue = deque([beginWord])
-        # visited = set()
-        # visited.add(beginWord)
-        # count = 0
-        # wordList = set(wordList)
-
-        # while queue:
-        #     size = len(queue)
-        #     count += 1
+        def helper(w1,w2):
+            l = len(w1)
+            count = 0
             
-        #     for _ in range(size):
-        #         word = queue.popleft()
-        #         if word == endWord:
-        #             return count
-            
-        #         for i, ch in enumerate(word):
-        #             for c in "abcdefghijklmnopqrstuvwxyz":
-        #                 new_word = word[:i] + c + word[i + 1:]
-        #                 if new_word not in visited and new_word in wordList:
-        #                     queue.append(new_word)
-        #                     visited.add(new_word)
+            for j in range(l):
+                if w1[j] == w2[j]:
+                    count += 1
+            if count == l - 1:
+                return True
+            else:
+                return False
         
-        # return 0
 
-        # #O(V + E) ; O(V)  
+        
+        graph = []
 
-        if endWord not in wordList:
-            return 0
+        queue = deque()
 
-        forward_queue = {beginWord}
-        back_queue = {endWord}
-        visited = set()
-        visited.add(beginWord)
-        visited.add(endWord)
-        wordList = set(wordList)
-        count = 1
+        queue.append(beginWord)
+        steps = 1
+        visited = set([beginWord])
 
+        while queue:
+            for _ in range(len(queue)):
+                node = queue.popleft()
 
-        while forward_queue and back_queue:
-            if len(forward_queue) > len(back_queue):
-                forward_queue , back_queue = back_queue , forward_queue
+                if node == endWord:
+                    return steps
+                
+                for word in wordList:
+                    if word not in visited and helper(word,node):
+                        queue.append(word)
+                        visited.add(word)
+            
+            steps += 1
+        
 
-            count += 1
-            next_level = set()
-
-            for word in forward_queue:
-                for i in range(len(word)):
-                    for ch in "abcdefghijklmnopqrstuvwxyz":
-                        new_word = word[:i] + ch + word[i + 1:]
-
-                        if new_word in back_queue: # Intersection of the 2 queue's
-                            return count
-
-                        if new_word not in visited and new_word in wordList:
-                            visited.add(new_word)
-                            next_level.add(new_word)
+        return 0
 
 
-            forward_queue = next_level
 
-        return 0 
+
