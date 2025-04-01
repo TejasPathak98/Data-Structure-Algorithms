650ff970d8c14a634459c193713e73fb42cc7020
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-31 20:49:14 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (66.16%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -6,51 +6,11 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        # if not p and not q:
-        #     return True
-        # if not p or not q:
-        #     return False
-        # if p.val != q.val:
-        #     return False
+        if p is None and q is None:
+            return True
+        if p is None or q is None:
+            return False
+        if p.val != q.val:
+            return False
         
-        # return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-
-        #O(N) ; O(N)
-
-        # queue = deque([(p,q)])
-
-        # while queue:
-        #     n1,n2 = queue.popleft()
-
-        #     if n1 is None and n2 is None:
-        #         continue
-            
-        #     if  n1 is None or n2 is None or n1.val != n2.val:
-        #         return False
-            
-        #     queue.append((n1.left,n2.left))
-        #     queue.append((n1.right,n2.right))
-        
-        # return True
-
-        # #O(N) ; O(N) ....Better than recursion for bigger trees where N > 1000
-
-
-        stack = [(p,q)]
-
-        while stack:
-            n1,n2 = stack.pop()
-
-            if n1 is None and n2 is None:
-                continue
-            
-            if  n1 is None or n2 is None or n1.val != n2.val:
-                return False
-
-            stack.append((n1.left,n2.left))
-            stack.append((n1.right,n2.right))
-        
-        return True
-
-
-    
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
