510a7a21987ce08db9cf23e3cb67397f0eaabddd
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-31 21:08:29 -0700

    Time: 0 ms (100.00%) | Memory: 18.5 MB (60.58%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -5,57 +5,26 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def __init__(self):
-        self.node_dict = defaultdict(list)
-
-    
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        # if not root:
-        #     return []
-        
-        # ans = []
-        # queue = deque([root])
-        # temp = []
-
-        # while queue:
-        #     size = len(queue)
-        #     ans.append(temp.copy())
-        #     temp.clear()
-        #     for _ in range(size):
-        #         node = queue.popleft()
-        #         temp.append(node.val)
-
-        #         if node.left:
-        #             queue.append(node.left)
-        #         if node.right:
-        #             queue.append(node.right)
-        
-        # ans.append(temp)
-        # return ans[1:]
-
-        # #O(N) ; O(N)
         if not root:
             return []
         
-        self.dfs(root,0)
-        return list(self.node_dict.values())
+        queue = deque([root])
+        ans = []
 
-    def dfs(self,root,level):
-        if root is None:
-            return
+        while queue:
+            temp = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
 
-        self.node_dict[level].append(root.val)
+                temp.append(node.val)
 
-        if root.left:
-            self.dfs(root.left,level + 1)
-        if root.right:
-            self.dfs(root.right,level + 1)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            
+            ans.append(temp)
 
-
-
-
-
-    
-
-
+        return ans
 
