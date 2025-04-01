872064418897374886e72abdde9fd648bb7e3f52
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-03-31 21:12:14 -0700

    Time: 0 ms (100.00%) | Memory: 18.5 MB (39.51%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -6,25 +6,46 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        # if not root:
+        #     return []
+        
+        # queue = deque([root])
+        # ans = []
+
+        # while queue:
+        #     temp = []
+        #     for _ in range(len(queue)):
+        #         node = queue.popleft()
+
+        #         temp.append(node.val)
+
+        #         if node.left:
+        #             queue.append(node.left)
+        #         if node.right:
+        #             queue.append(node.right)
+            
+        #     ans.append(temp)
+
+        # return ans
+
         if not root:
             return []
+
+        ans = defaultdict(list)
         
-        queue = deque([root])
-        ans = []
-
-        while queue:
-            temp = []
-            for _ in range(len(queue)):
-                node = queue.popleft()
-
-                temp.append(node.val)
-
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
+        def dfs(node,level):
+            if not node:
+                return
             
-            ans.append(temp)
+            ans[level].append(node.val)
 
-        return ans
+            if node.left:
+                dfs(node.left,level + 1)
+            if node.right:
+                dfs(node.right,level + 1)
+
+        dfs(root,0)
+        return list(ans.values())
+
+
 
