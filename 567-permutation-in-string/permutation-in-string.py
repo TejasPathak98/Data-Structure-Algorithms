8f6faadbc102be21cb1f8fc58a25d0b6c42d6cbe
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-01 23:26:08 -0700

    Time: 27 ms (47.69%) | Memory: 18 MB (40.49%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,53 +1,44 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        # l= 0
-        # r = 0
-        # s1_dict = Counter(s1)
-        # sliding_window_dict = Counter(s2[:len(s1)])
+        n1 = len(s1)
+        n2 = len(s2)
 
-        # l = 0
-        # r = len(s1) - 1
+        if n1 > n2:
+            return False
+        if n1 == s2:
+            if Counter(n1) == Counter(n2):
+                return True
+            else:
+                return False
 
-        # while r < len(s2):
-        #     if sliding_window_dict == s1_dict:
-        #         return True
-        #     else:
-        #         sliding_window_dict[s2[l]] -= 1
-        #         l += 1
-        #         r += 1
-        #         if r < len(s2):sliding_window_dict[s2[r]] += 1
-        # return False
-
-        # #O(l2* l1) becasue of dict comparison ; O(l1)
-
-        if len(s1) > len(s2):return False
+        counter1 = Counter(s1)
 
         l = 0
-        s1_dict = Counter(s1)
-        sliding_window_dict = Counter(s2[:len(s1)])
-        match_count = sum((s1_dict[c] == sliding_window_dict[c] for c in s1_dict))
+        r = 0
 
-        for r in range(len(s1),len(s2)):
-            if match_count == len(s1_dict):
+        temp_dict = defaultdict(int)
+
+        while r < len(s2):
+            temp_dict[s2[r]] += 1
+
+            if temp_dict == counter1:
                 return True
+
+            if s2[r] not in counter1:
+                r += 1
+                l = r
+                temp_dict.clear()
+                continue
             
-            if s2[l] in s1_dict:
-                if s1_dict[s2[l]] == sliding_window_dict[s2[l]]:
-                    match_count -= 1
-                sliding_window_dict[s2[l]] -= 1
-                if s1_dict[s2[l]] == sliding_window_dict[s2[l]]:
-                    match_count += 1 
+            if temp_dict[s2[r]] == counter1[s2[r]] + 1:
 
-            if s2[r] in s1_dict:
-                if s1_dict[s2[r]] == sliding_window_dict[s2[r]]:
-                    match_count -= 1
-                sliding_window_dict[s2[r]] += 1
-                if s1_dict[s2[r]] == sliding_window_dict[s2[r]]:
-                    match_count += 1
-
-
-            l += 1
-
-        return len(s1_dict) == match_count 
-
+                while l < r and temp_dict[s2[r]] != counter1[s2[r]]:
+                    temp_dict[s2[l]] -= 1
+                    l += 1
+            
+            r += 1
         
+        return False
+
+
+
