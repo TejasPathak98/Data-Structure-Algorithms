cdb413827d50df481967a96e098750909509b62c
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-02 00:01:51 -0700

    Time: 80 ms (55.56%) | Memory: 18.5 MB (17.11%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,34 +1,37 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(s) < len(t):
+        if len(t) > len(s):
             return ""
-        
-        t_dict = Counter(t)
-        s_dict = defaultdict(int)
 
-        l = 0 
+        dict_t = Counter(t)
+        requried = len(dict_t) # how many unique characters do I need to match ?
+        formed =0 # how many unique characters have I matched ? Do not compare the full map
+        l = 0
         r = 0
-        ans = float('inf'),l,r
-        required = len(t_dict)
-        formed = 0
+        ans = float("inf"),r,l
+        s_dict = defaultdict(int)
 
         while r < len(s):
             s_dict[s[r]] += 1
-            if s_dict[s[r]] == t_dict[s[r]]:
+
+            if s_dict[s[r]] == dict_t[s[r]]:# we match one unique char count
                 formed += 1
             
-            while l < len(s) and formed == required:
+            while l < len(s) and formed == requried:
                 if ans[0] > r - l + 1:
-                    ans = r - l + 1,l,r
-                
+                    ans = r-l+1,r,l
+
                 s_dict[s[l]] -= 1
-                if s_dict[s[l]] < t_dict[s[l]]:
+
+                if s_dict[s[l]] < dict_t[s[l]]:
                     formed -= 1
-
+                
                 l += 1
+
             r += 1
-        
 
-        return "" if ans[0] == float("inf") else s[ans[1]:ans[2] + 1]
-
-
+        if ans[0] == float("inf"):
+            print("Br")
+            return ""
+        else:
+            return s[ans[2]:ans[1] + 1]
