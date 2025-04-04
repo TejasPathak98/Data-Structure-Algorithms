9ee88fb7c69a7d18baf626fc393253c6b21ec729
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-04 14:15:27 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (67.67%) - LeetSync

---
+++ 273-integer-to-english-words/integer-to-english-words.py
@@ -0,0 +1,37 @@
+class Solution:
+    def numberToWords(self, num: int) -> str:
+        below_Twenty = ["","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen",
+        "Nineteen"]
+
+        Tens = ["","","Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"]
+
+        Thousands = ["","Thousand","Million","Billion"]
+
+        if num == 0:
+            return "Zero"
+        
+
+        def helper(n):
+            if n == 0:
+                return ""
+            elif n < 20:
+                print("break-3")
+                return below_Twenty[n] + " "
+            elif n < 100:
+                print("break-2")
+                return Tens[n // 10] + " " + helper(n % 10)
+            else:
+                print("break-1")
+                return below_Twenty[n // 100] + " " + "Hundred" + " " + helper(n % 100)
+
+        i = 0
+        res = ""
+
+        while num > 0:
+            if num % 1000 != 0:
+                res = helper(num % 1000) + Thousands[i] + " "  + res
+            num = num // 1000
+            i += 1
+
+        
+        return res.strip()
