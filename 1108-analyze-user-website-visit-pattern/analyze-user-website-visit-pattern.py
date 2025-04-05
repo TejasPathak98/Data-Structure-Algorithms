d827b2dc6aa884f0898160000ac08b8e77bc8da0
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-04 21:12:50 -0700

    Time: 7 ms (80.74%) | Memory: 18.6 MB (31.96%) - LeetSync

--- 1108-analyze-user-website-visit-pattern/analyze-user-website-visit-pattern.py
+++ 1108-analyze-user-website-visit-pattern/analyze-user-website-visit-pattern.py
@@ -16,29 +16,26 @@ class Solution:
         
         patterns_mapping_to_users = defaultdict(list)
 
-        print(visits_per_user)
+
 
         for user,websites in visits_per_user.items():
             if len(websites) < 3:
                 continue
             
             website_patterns = set(combinations(websites, 3))
-            print(website_patterns)
             for pattern in website_patterns:
                 patterns_mapping_to_users[pattern].append(user)
 
-        print(patterns_mapping_to_users)
         
         max_score = 0
         best_pattern = None
 
         for pattern,users in patterns_mapping_to_users.items():
             score = len(users)
-            print(score)
             if score > max_score or (max_score == score and (best_pattern is None or pattern < best_pattern)):
                 best_pattern = pattern
                 max_score = score
 
-        print(best_pattern)
+
 
         return list(best_pattern)
