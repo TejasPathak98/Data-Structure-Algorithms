525ae50209be5e4fb7040fb65bb93151682dab69
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-06 19:59:42 -0700

    Time: 236 ms (63.04%) | Memory: 28.3 MB (64.85%) - LeetSync

--- 3594-identify-the-largest-outlier-in-an-array/identify-the-largest-outlier-in-an-array.py
+++ 3594-identify-the-largest-outlier-in-an-array/identify-the-largest-outlier-in-an-array.py
@@ -1,24 +1,18 @@
 class Solution:
     def getLargestOutlier(self, nums: List[int]) -> int:
+        max_outlier = float("-inf")
         total_sum = sum(nums)
-        final_outlier = float("-inf")
-        
-        num_dict = defaultdict(int)
+
+        counter = Counter(nums)
+
+        #formula : total_sum = 2*num + outlier
 
         for num in nums:
-            num_dict[num] += 1
+            potential_outlier = total_sum - 2*num
+
+            if potential_outlier in counter:
+                if potential_outlier != num or counter[potential_outlier] > 1:
+                    max_outlier = max(max_outlier,potential_outlier)
         
-        for num in num_dict:
 
-            potential_outlier = total_sum - 2 * num
-            #this is because : total_sum = sum of special elements + outlier + (other element, which is the sum of special elements)
-
-            if potential_outlier in num_dict:
-                if potential_outlier != num or num_dict[potential_outlier] > 1:
-                    final_outlier = max(final_outlier,potential_outlier)
-
-
-        
-        return final_outlier
-
-        
+        return max_outlier
