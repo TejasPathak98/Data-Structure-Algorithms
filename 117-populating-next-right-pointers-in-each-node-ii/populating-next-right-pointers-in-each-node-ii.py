3115156dbec771aa035e2ade1ac7402d2dc618b0
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-07 14:16:46 -0700

    Time: 47 ms (53.03%) | Memory: 18.8 MB (81.83%) - LeetSync

--- 117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.py
+++ 117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.py
@@ -10,32 +10,62 @@ class Node:
 
 class Solution:
     def connect(self, root: 'Node') -> 'Node':
+        # if not root:
+        #     return root
+        
+        # queue = deque([root])
+
+        # while queue:
+        #     level = []
+
+        #     for _ in range(len(queue)):
+
+        #         node = queue.popleft()
+
+        #         level.append(node)
+
+        #         if node.left:
+        #             queue.append(node.left)
+        #         if node.right:
+        #             queue.append(node.right)
+                
+        #     if len(level) == 1:
+        #         level[0].next = None
+        #     else:
+        #         for i in range(len(level) - 1):
+        #             level[i].next = level[i + 1]
+                
+        #         level[len(level) - 1].next = None
+        
+        # return root
+
+        # #O(N).; O(N)
+
+        #LNR ; NLR , LRN , RLN 
+
         if not root:
             return root
-        
+
         queue = deque([root])
 
         while queue:
-            level = []
+            prev = None
+            size = len(queue)
 
-            for _ in range(len(queue)):
-
+            for _ in range(size):
                 node = queue.popleft()
 
-                level.append(node)
-
+                if prev:
+                    prev.next = node
+                
+                prev = node
+                
                 if node.left:
                     queue.append(node.left)
                 if node.right:
                     queue.append(node.right)
-                
-            if len(level) == 1:
-                level[0].next = None
-            else:
-                for i in range(len(level) - 1):
-                    level[i].next = level[i + 1]
-                
-                level[len(level) - 1].next = None
+            
         
         return root
+
             
