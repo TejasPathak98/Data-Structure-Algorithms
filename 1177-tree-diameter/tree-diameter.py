24dc212a36d2a3c524f4928d95670666d60ec007
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-07 21:43:46 -0700

    Time: 11 ms (97.00%) | Memory: 22.7 MB (53.66%) - LeetSync

--- 1177-tree-diameter/tree-diameter.py
+++ 1177-tree-diameter/tree-diameter.py
@@ -6,24 +6,41 @@ class Solution:
             graph[a].append(b)
             graph[b].append(a)
 
-        self.diameter = 0
+        # self.diameter = 0
 
-        def dfs(node,parent):
-            max1 = max2 = 0
+        # def dfs(node,parent):
+        #     max1 = max2 = 0
+        #     for neighbor in graph[node]:
+        #         if neighbor != parent:
+        #             depth = dfs(neighbor,node)
+        #             if depth > max1:
+        #                 max2 = max1
+        #                 max1 = depth
+        #             elif depth > max2:
+        #                 max2 = depth
+        #     self.diameter = max(self.diameter,max1 + max2)
+        #     return max1 + 1
+        
+        # dfs(0,-1)
+        # return self.diameter
+
+        #O(N) ; O(N)
+
+        def dfs(node,parent,depth):
+            farthest = (depth,node)
             for neighbor in graph[node]:
                 if neighbor != parent:
-                    depth = dfs(neighbor,node)
-                    if depth > max1:
-                        max2 = max1
-                        max1 = depth
-                    elif depth > max2:
-                        max2 = depth
-            self.diameter = max(self.diameter,max1 + max2)
-            return max1 + 1
-        
+                    candidate = dfs(neighbor,node,depth + 1)
+                    if candidate[0] > farthest[0]:
+                        farthest = candidate
+            
+            return farthest
 
-        dfs(0,-1)
-        return self.diameter
+        _ , intermediate_node = dfs(0,-1,0)
+
+        diameter,_ = dfs(intermediate_node,-1,0)
+
+        return diameter
 
         
 
