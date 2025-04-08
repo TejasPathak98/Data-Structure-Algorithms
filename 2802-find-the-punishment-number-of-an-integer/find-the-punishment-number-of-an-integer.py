94dbdb4976f458145c7ef54bd8dc5c50c5dfa5cd
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-07 19:25:51 -0700

    Time: 855 ms (54.60%) | Memory: 17.8 MB (71.41%) - LeetSync

---
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.py
@@ -0,0 +1,28 @@
+class Solution:
+    def punishmentNumber(self, n: int) -> int:
+        ans = 0
+
+        def helper(s,number_of_interest):
+            n = len(s)
+
+            for i in range(1,n):
+                prefix = s[:i]
+                suffix = s[i:]
+
+                if int(prefix) + int(suffix) == number_of_interest:
+                    return True
+                elif helper(suffix,number_of_interest - int(prefix)):
+                    return True
+            
+            return False
+
+        for i in range(1,n + 1):
+            x = i*i
+            x_s = str(x)
+
+            if helper(x_s,i):
+                ans += x
+        
+
+        return ans + 1       
+
