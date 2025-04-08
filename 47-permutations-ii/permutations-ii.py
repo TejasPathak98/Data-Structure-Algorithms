c8f3cd6458ddf90a8f3f5e39fe62408b1b1a9e2a
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-07 17:11:15 -0700

    Time: 0 ms (100.00%) | Memory: 18.3 MB (18.03%) - LeetSync

--- 47-permutations-ii/permutations-ii.py
+++ 47-permutations-ii/permutations-ii.py
@@ -1,20 +1,30 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        ans = set()
-        n = len(nums)
+        ans = []
+        used = [False] * len(nums)
+        nums.sort()
 
-        def helper(i):
-            if i == n:
-                ans.add(tuple(nums[:]))
+        def backtrack(temp):
+            if len(temp) == len(nums):
+                ans.append(temp.copy())
                 return
             
-            for j in range(i,n):
-                nums[i] , nums[j] = nums[j] , nums[i]
-                helper(i + 1)
-                nums[i] , nums[j] = nums[j] , nums[i]
+            if len(temp) > len(nums):
+                return
+
+            for i in range(len(nums)):
+                if used[i]: #the element nums[i] has already been used in our permutation path(we dont care about its location in the path, only that it exists)
+                    continue
+
+                if i > 0 and nums[i] == nums[i - 1] and not used[i - 1]:
+                    continue #because if we dont skip then that element will become part of the path, where the same path was done before using the nums[i - 1] element , so its a repeat
+
+                used[i] = True
+                temp.append(nums[i])
+                backtrack(temp)
+                used[i] = False
+                temp.pop()
 
         
-        helper(0)
-        return list(ans)
-            
-
+        backtrack([])
+        return ans
