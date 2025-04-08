d3155c102c3b85a7b6e86b01c15c32814893e9f1
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-07 17:03:40 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (32.51%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,42 +1,18 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        # ans = []
-        # n = len(nums)
-
-        # def helper(temp,temp_set):
-        #     if len(temp) == n:
-        #         ans.append(temp[:])
-        #         return
-            
-        #     for j in range(n):
-        #         if nums[j] not in temp_set:
-        #             temp.append(nums[j])
-        #             temp_set.add(nums[j])
-        #             helper(temp,temp_set)
-        #             temp.pop()
-        #             temp_set.remove(nums[j])
-
-        
-        # helper([],set())
-        # return ans
-
-
-        #O(N!) ; O(N) + O(N)
-
-
         ans = []
         n = len(nums)
 
-        def helper(i):
+        def backtrack(i):
             if i == n:
                 ans.append(nums[:])
                 return
-            
+
             for j in range(i,n):
                 nums[i] , nums[j] = nums[j] , nums[i]
-                helper(i + 1)
+                backtrack(i + 1)
                 nums[i] , nums[j] = nums[j] , nums[i]
 
-        
-        helper(0)
+    
+        backtrack(0)
         return ans
