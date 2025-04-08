9de2a1d4364d57e25f342ff03b4c13e769957490
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-07 21:36:13 -0700

    Time: 84 ms (84.19%) | Memory: 22.1 MB (92.06%) - LeetSync

---
+++ 1816-lowest-common-ancestor-of-a-binary-tree-iv/lowest-common-ancestor-of-a-binary-tree-iv.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', nodes: 'List[TreeNode]') -> 'TreeNode':
+        if root is None:
+            return root
+        
+        if len(nodes) == 1:
+            return nodes[0]
+        
+        if root in nodes:
+            return root
+        
+        l = self.lowestCommonAncestor(root.left, nodes)
+        r = self.lowestCommonAncestor(root.right, nodes)
+
+        if l and r:
+            return root
+        
+        return l if l else r
