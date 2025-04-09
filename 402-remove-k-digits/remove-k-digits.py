17325df9eea695e362ad3a3eaeb5b87d23163349
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-08 22:14:00 -0700

    Time: 21 ms (54.66%) | Memory: 18.6 MB (77.24%) - LeetSync

---
+++ 402-remove-k-digits/remove-k-digits.py
@@ -0,0 +1,19 @@
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        #Monotonic increasing stack ; we want lease number
+
+        stack = []
+
+        for i in num:
+            while stack and k > 0 and stack[-1] > i:
+                stack.pop()
+                k -= 1
+            stack.append(i)
+
+        while stack and k:
+            stack.pop()
+            k -= 1
+
+        
+        result =  "".join(stack).lstrip("0")
+        return result if result else "0"
