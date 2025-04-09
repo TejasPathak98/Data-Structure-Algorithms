1fd800eb07de495191cca3a851d944f9bf751d95
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-09 16:31:13 -0700

    Time: 2851 ms (90.98%) | Memory: 18 MB (39.72%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,50 +1,31 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        board_str = ""
-        for row in board:
-            board_str += "".join(row)
-        
-        print(board_str)
-
-        board_counter = Counter(board_str)
-        word_counter = Counter(word)
-
-        for ch in word:
-            if word_counter[ch] > board_counter[ch]:
-                print("Here")
-                return False
-        
         m = len(board)
         n = len(board[0])
 
-        def backtracking(i,j,pos):
-            if pos == len(word):
+        def dfs(i,j,idx):
+            if idx == len(word) - 1:
                 return True
             
-            if i < 0 or i >= m or j < 0 or j >= n or word[pos] != board[i][j]:
-                return False
-            
             temp = board[i][j]
+
             board[i][j] = "#"
 
             for dx,dy in [(0,1),(0,-1),(1,0),(-1,0)]:
-                x_ = i + dx
-                y_ = j + dy
+                x = i + dx
+                y = j + dy
 
-                if backtracking(x_, y_, pos + 1):
-                    return True
-
-
+                if 0 <= x < m and 0 <= y < n and board[x][y] == word[idx + 1]:
+                    if dfs(x,y,idx + 1):
+                        return True
             board[i][j] = temp
+            return False 
 
         for i in range(m):
             for j in range(n):
                 if board[i][j] == word[0]:
-                    if backtracking(i,j,0):
+                    #visited = [[False] * n for _ in range(m)]
+                    if dfs(i,j,0):
                         return True
         
         return False
-
-
-
-        
