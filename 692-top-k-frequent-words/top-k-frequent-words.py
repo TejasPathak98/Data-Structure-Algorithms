09b9e3b75f17ffecf00e865c3672253cc7be05ef
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-09 14:59:30 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (20.89%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.py
+++ 692-top-k-frequent-words/top-k-frequent-words.py
@@ -1,35 +1,23 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        # counter = Counter(words)
-        # min_heap = []
-
-        # for word,freq in counter.items():
-        #     heapq.heappush(min_heap, (freq,word))
-
-        #     if len(min_heap) > k:
-        #         heapq.heappop(min_heap)
-
-        # result = []
-
-        # while min_heap:
-        #     ferq,word = heapq.heappop(min_heap)
-        #     result.append(word)
+        n = len(words)
         
-        # return result[::-1]
-
-        # counter = Counter(words)
-        # max_heap = [(-freq,word) for word,freq in counter.items()]
-        # heapify(max_heap)
-        # result = []
-
-        # for _ in range(k):
-        #     result.append(heapq.heappop(max_heap)[1])
-        
-        # return result
-
-        #O(N + klogN)
-
         counter = Counter(words)
 
-        return nsmallest(k, counter.keys() ,key = lambda x : (-counter[x],x))
+        max_heap = []
+        heapify(max_heap)
+        
+        for word,freq in counter.items():
+            heapq.heappush(max_heap, (-freq,word))
+
+        res = []
+
+        for _ in range(k):
+            _,word = heapq.heappop(max_heap)
+            res.append(word)
+
+        
+        return res
+
+        
 
