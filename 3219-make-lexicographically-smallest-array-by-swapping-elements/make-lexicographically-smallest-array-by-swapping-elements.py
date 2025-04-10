7502cc85a7c507a93a35f4e972802ea5326a9328
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-10 00:27:05 -0700

    Time: 562 ms (14.77%) | Memory: 59.3 MB (53.93%) - LeetSync

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
@@ -6,49 +6,46 @@ class UnionFind:
         if x != self.parents[x]:
             self.parents[x] = self.find(self.parents[x])
         return self.parents[x]
-    
+
     def union(self,x,y):
         self.parents[self.find(x)] = self.find(y)
+
 
 class Solution:
     def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
         n = len(nums)
-        uf = UnionFind(n) 
+        uf = UnionFind(n)
+        values_indices = [(val,idx) for idx,val in enumerate(nums)]
 
-        index_values = []
-
-        for idx,val in enumerate(nums):
-            index_values.append((val,idx))
-        
-        index_values = sorted(index_values)
+        values_indices.sort(key = lambda x : x[0])
 
         for i in range(n - 1):
-            val1,idx1 = index_values[i]
-            val2,idx2 = index_values[i + 1]
+            val1,idx1 = values_indices[i]
+            val2,idx2 = values_indices[i + 1]
+
             if abs(val1 - val2) <= limit:
                 uf.union(idx1, idx2)
 
-        #Getting a map for connected_components(idx)
 
         connected_components = defaultdict(list)
+        
         for idx,val in enumerate(nums):
-            root = uf.find(idx) # getting parent
+            root = uf.find(idx)
             connected_components[root].append(idx)
 
         
         res = nums[:]
+
         for indices in connected_components.values():
             values = [nums[i] for i in indices]
             idx_range = indices
             idx_range.sort()
             values.sort()
 
-            for idx,v in zip(idx_range,values):
-                res[idx] = v
+            for i,v in zip(idx_range,values):
+                res[i] = v
+
+
         
         return res
 
-
-        
-
-        
