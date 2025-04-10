3b0c20a0c505cab8452b8bf960afcd7f9c1d4dd5
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-10 00:56:10 -0700

    Time: 7 ms (58.18%) | Memory: 18 MB (77.69%) - LeetSync

--- 47-permutations-ii/permutations-ii.py
+++ 47-permutations-ii/permutations-ii.py
@@ -1,30 +1,33 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        ans = []
-        used = [False] * len(nums)
+        res = []
+        used = [False] *  len(nums)
+        
         nums.sort()
 
         def backtrack(temp):
             if len(temp) == len(nums):
-                ans.append(temp.copy())
-                return
+                res.append(temp.copy())
+                return 
             
             if len(temp) > len(nums):
                 return
+            
 
             for i in range(len(nums)):
-                if used[i]: #the element nums[i] has already been used in our permutation path(we dont care about its location in the path, only that it exists)
+                if used[i]:
                     continue
-
+                
                 if i > 0 and nums[i] == nums[i - 1] and not used[i - 1]:
-                    continue #because if we dont skip then that element will become part of the path, where the same path was done before using the nums[i - 1] element , so its a repeat
+                    continue
+                
 
-                used[i] = True
                 temp.append(nums[i])
+                used[i] = True
                 backtrack(temp)
                 used[i] = False
                 temp.pop()
-
         
+
         backtrack([])
-        return ans
+        return res
