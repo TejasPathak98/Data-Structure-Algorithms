b7c2a44e62e98c84cd617b5e7bf3e940810abf6b
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-09 20:53:26 -0700

    Time: 411 ms (94.89%) | Memory: 18.7 MB (83.47%) - LeetSync

--- 212-word-search-ii/word-search-ii.py
+++ 212-word-search-ii/word-search-ii.py
@@ -1,8 +1,21 @@
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        def build_trie(words):
+            root = {}
+            
+            for word in words:
+                curr = root
+                for ch in word:
+                    if ch not in curr:
+                        curr[ch] = {}
+                    curr = curr[ch]
+                curr['#'] = word
+
+            return root
+
         
-        def dfs(i,j,trie):
-            letter = board[i][j]
+        def dfs(x,y,trie):
+            letter = board[x][y]
             curr = trie[letter]
 
             w = curr.pop('#',False)
@@ -10,34 +23,31 @@ class Solution:
             if w:
                 result.append(w)
             
-            board[i][j] = "*"
+            temp = board[x][y]
+
+            board[x][y] = "*"
 
             for dx,dy in [(0,1),(0,-1),(1,0),(-1,0)]:
-                x = dx + i
-                y = dy + j
+                x_ = x + dx
+                y_ = y + dy
 
-                if 0 <= x < m and 0 <= y < n and board[x][y] in curr:
-                    dfs(x,y,curr)
+                if 0 <= x_ < m and 0 <= y_ < n and board[x_][y_] in curr:
+                    dfs(x_,y_,curr)
 
-            board[i][j] = letter
-
+            board[x][y] = temp
+            
             if not curr:
                 trie.pop(letter)
 
-        trie = {}
         result = []
+        trie = build_trie(words)
         m = len(board)
         n = len(board[0])
 
-        for word in words:
-            curr = trie
-            for letter in word:
-                curr = curr.setdefault(letter,{})
-            curr['#'] = word
-        
         for i in range(m):
             for j in range(n):
-                if board[i][j] in trie:
+                if board[i][j] in trie: 
                     dfs(i,j,trie)
-        
+
+
         return result
