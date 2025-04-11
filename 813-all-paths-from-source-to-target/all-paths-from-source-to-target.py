97276c6b342b72a76ef999c7d2cf675b87c35971
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-10 19:44:38 -0700

    Time: 7 ms (44.57%) | Memory: 19.3 MB (58.22%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
@@ -3,25 +3,25 @@ class Solution:
         paths = []
 
         n = len(graph)
-        visited = [False] * n
+
 
         def dfs(node,path):
             if node == n - 1:
                 paths.append(path.copy())
                 return
 
-            visited[node] = True
+
 
             for nei in graph[node]:
-                if not visited[nei]:
-                    path.append(nei)
-                    dfs(nei,path)
-                    path.pop()
+                path.append(nei)
+                dfs(nei,path)
+                path.pop()
 
-            visited[node] = False
 
         
         dfs(0,[0])
         return paths
+
+        #
 
              
