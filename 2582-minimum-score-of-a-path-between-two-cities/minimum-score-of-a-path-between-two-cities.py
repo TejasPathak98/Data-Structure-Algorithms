9970e59c07d32f361305edb7bfe2d656526dabe1
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-10 19:26:04 -0700

    Time: 238 ms (68.01%) | Memory: 76.3 MB (41.48%) - LeetSync

--- 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.py
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.py
@@ -17,9 +17,14 @@ class Solution:
             visited.add(node)
 
             for nei,distance in graph[node]:
-                min_cost = min(min_cost,distance)
+                min_cost = min(min_cost,distance)  
                 if nei not in visited:
                     dfs(nei)
 
         dfs(1)
         return min_cost
+
+
+
+    # but that causes you to miss edges that connect two already visited cities, even though those edges are valid parts of the connected component.(in the case 
+   # when  I put the min_cost = min(min_cost,distance) inside the if block)
