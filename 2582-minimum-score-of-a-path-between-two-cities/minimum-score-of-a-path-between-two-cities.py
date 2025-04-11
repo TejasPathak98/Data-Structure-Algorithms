f159d316381b76e3507c31a173c55783aa46a7e0
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-10 19:22:53 -0700

    Time: 247 ms (64.30%) | Memory: 76.3 MB (41.48%) - LeetSync

--- 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.py
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.py
@@ -1,55 +1,25 @@
 class Solution:
     def minScore(self, n: int, roads: List[List[int]]) -> int:
         graph = defaultdict(list)
+
+        for u,v,distance in roads:
+            graph[u].append((v,distance))
+            graph[v].append((u,distance))
+
         
-        for u,v,w in roads:
-            graph[u].append((v,w))
-            graph[v].append((u,w))
-        
-        min_distance = float("inf")
+        #we just need to get the least road cost amongst all the connected componenets of 1
 
-        # def dfs(node,temp,visited,dist):
-        #     nonlocal min_distance
-
-        #     if node == n:
-        #         print("Break",dist)
-        #         temp.append(dist)
-        #         tp = temp.copy()
-        #         min_distance = min(min_distance,min(tp))
-        #         return
-            
-        #     if node in visited:
-        #         return
-            
-        #     visited.add(node)
-        #     temp.append(dist)
-
-        #     for neighbor,d in graph[node]:
-        #         dfs(neighbor,temp,visited,d)
-        
-        # dfs(1,[],set(),10 ** 4 + 1)
-
-        # return min_distance
-
-        visited_edges = set()
+        min_cost = float("inf")
+        visited = set()
 
         def dfs(node):
-            nonlocal min_distance
-            
-            for neighbor,d in graph[node]:
-                edge = tuple(sorted([node,neighbor]))
-                
-                if edge in visited_edges:
-                    continue
+            nonlocal min_cost
+            visited.add(node)
 
-                visited_edges.add(edge)
+            for nei,distance in graph[node]:
+                min_cost = min(min_cost,distance)
+                if nei not in visited:
+                    dfs(nei)
 
-                min_distance = min(min_distance,d)
-
-                if min_distance == 1:
-                    return
-                
-                dfs(neighbor)
-        
         dfs(1)
-        return min_distance
+        return min_cost
