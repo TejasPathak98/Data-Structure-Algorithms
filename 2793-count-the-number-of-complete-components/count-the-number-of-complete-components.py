0311b94db0446ce17b0663e522fb8d9aa63df302
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-11 02:36:11 -0700

    Time: 51 ms (62.93%) | Memory: 18.4 MB (55.63%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.py
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.py
@@ -1,36 +1,38 @@
 class Solution:
     def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
+        #the number of nodes(nodes - 1) // 2 should be equal to the number of edges 
+        
         graph = defaultdict(list)
 
         for a,b in edges:
             graph[a].append(b)
             graph[b].append(a)
-        
-        self.visited = set()
 
-        def dfs(node,nodes_edges):
-            self.visited.add(node)
 
-            nodes_edges[0] += 1
-            nodes_edges[1] += len(graph[node])
+        visited = set()
+
+        def dfs(node,node_and_edge):
+            visited.add(node)
+
+            node_and_edge[0] += 1
+            node_and_edge[1] += len(graph[node])
 
             for nei in graph[node]:
-                if nei not in self.visited:
-                    dfs(nei,nodes_edges)
-        
+                if nei not in visited:
+                    dfs(nei,node_and_edge)
+
         count = 0
 
         for i in range(n):
-            if i not in self.visited:
-                nodes_edges = [0,0]
-                dfs(i,nodes_edges)
-                nodes, edges = nodes_edges
+            if i not in visited:
+                node_and_edge = [0,0]
+                dfs(i,node_and_edge)
 
-                if edges // 2 == (nodes * (nodes - 1)) // 2:
+                nodes = node_and_edge[0]
+                edges = node_and_edge[1]
+
+                if edges // 2 == nodes*(nodes - 1) // 2:
                     count += 1
 
-        
-        return count
 
-
-        
+        return count 
