247ccb20bf1b4813e4723c7cae08ea1c32bb664f
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-11 21:14:15 -0700

    Time: 19 ms (74.35%) | Memory: 18 MB (88.60%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -2,30 +2,32 @@ class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         m = len(grid)
         n = len(grid[0])
+
         max_area = 0
 
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1:
-                    max_area = max(max_area,self.bfs(grid,i,j))
+                    area = self.dfs(i,j,grid)
+                    max_area = max(max_area,area)
+
 
         return max_area
 
-    def bfs(self,grid,i,j):
-        area = 0
-        queue = deque([(i,j)])
-        grid[i][j] = -1
 
-        while queue:
-            x,y = queue.popleft()
-            area += 1
+    def dfs(self,x,y,grid):
 
-            for dx,dy in [(0,1),(0,-1),(1,0),(-1,0)]:
-                x_ = x + dx
-                y_ = y + dy
+        grid[x][y] = 0
+        count = 1
 
-                if x_ >= 0 and x_ < len(grid) and y_ >= 0 and y_ < len(grid[0]) and grid[x_][y_] == 1:
-                    grid[x_][y_] = -1
-                    queue.append((x_,y_))
+        for dx,dy in [(0,1),(0,-1),(1,0),(-1,0)]:
+            x_ = x + dx
+            y_ = y + dy
 
-        return area
+            if 0 <= x_ < len(grid) and 0 <= y_ < len(grid[0]) and grid[x_][y_] == 1:
+                count += self.dfs(x_,y_,grid)
+
+
+        return count
+
+
