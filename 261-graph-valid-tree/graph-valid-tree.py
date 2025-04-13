29c0469f3c13ae45e2c857c19784b7bfd836c1de
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-12 23:19:32 -0700

    Time: 2 ms (51.83%) | Memory: 19.1 MB (52.21%) - LeetSync

--- 261-graph-valid-tree/graph-valid-tree.py
+++ 261-graph-valid-tree/graph-valid-tree.py
@@ -1,25 +1,34 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        #Always remember parent trap in undirected graphs
-
+        if len(edges) != n - 1:
+            return False
+        
         graph = defaultdict(list)
 
         for u,v in edges:
             graph[u].append(v)
             graph[v].append(u)
+
         
         visited = set()
 
-        def HasCycle(node,parent):
+        def dfs(node,parent):
+
+            if node in visited:
+                return False
+            
             visited.add(node)
+
             for nei in graph[node]:
-                if nei == parent:
-                    continue
-                if nei in visited or HasCycle(nei, node):
-                    return True
-            return False
-    
-        if HasCycle(0,-1) == True:
-            return False
+                if nei != parent and nei not in visited:
+                    if not dfs(nei,node):
+                        return False
+            
+            return True
+
         
-        return len(visited) == n
+        return dfs(0,-1) and len(visited) == n
+
+
+
+
