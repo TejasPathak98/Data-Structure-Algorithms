c87a5ad01861b78b3b77ba41fb673252793800a8
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-14 15:42:22 -0700

    Time: 243 ms (67.85%) | Memory: 20.4 MB (39.40%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -15,12 +15,30 @@ class Solution:
                 if 0 <= x_ < m and 0 <=  y_ < n and grid[x_][y_] == "1":
                     dfs(x_,y_)
 
+        def bfs(x,y):
+
+            queue = deque([(x,y)])
+
+            while queue:
+
+                r,c = queue.popleft()
+
+                grid[r][c] = 0
+
+                for dx,dy in [(0,1),(1,0),(0,-1),(-1,0)]:
+                    x_ = r + dx
+                    y_ = c + dy
+
+                    if 0 <= x_ < m and 0 <=  y_ < n and grid[x_][y_] == "1":
+                        queue.append((x_,y_))
+                        grid[x_][y_] = 0
+
 
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == "1":
                     no_of_islands += 1
-                    dfs(i,j)
+                    bfs(i,j)
 
 
         return no_of_islands
