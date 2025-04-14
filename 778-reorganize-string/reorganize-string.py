b91fb9a723553e01a41b1b1a0715e4894a44c9a1
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-13 20:21:32 -0700

    Time: 3 ms (48.39%) | Memory: 18 MB (29.19%) - LeetSync

--- 778-reorganize-string/reorganize-string.py
+++ 778-reorganize-string/reorganize-string.py
@@ -1,14 +1,16 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        counter = Counter(s)
+        
+        result = []
+        counter= Counter(s)
         max_heap = [(-freq,char) for char,freq in counter.items()]
         heapify(max_heap)
 
-        prev_freq = 0
-        prev_char = 0
-        result = []
+        prev_char = None
+        prev_freq = 0 
 
         while max_heap:
+
             freq,char = heapq.heappop(max_heap)
 
             result.append(char)
@@ -16,18 +18,18 @@ class Solution:
             if prev_freq < 0:
                 heapq.heappush(max_heap, (prev_freq,prev_char))
             
-            prev_freq = freq + 1 #decreasing the freq by 1 and adding in next iteration(because we do not want consectuive same characters)
+            prev_freq = freq + 1
             prev_char = char
 
+        
         result = "".join(result)
-
-        print(result)
 
         if len(result) < len(s):
             return ""
 
-        for i in range(1,len(s)):
-            if result[i] == result[i - 1]:
-                return ""
-
         return result
+
+
+            
+
+
