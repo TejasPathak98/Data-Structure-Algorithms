8f790b3117d7a6bea2583c37f2023d6e89cffb30
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-14 16:51:41 -0700

    Time: 23 ms (32.13%) | Memory: 17.9 MB (53.29%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -3,8 +3,8 @@ class Solution:
         l = 0
         r = 0
 
+        char_dict = defaultdict(int)
         max_len = 0
-        char_dict = {}
 
         while r < len(s):
             if s[r] not in char_dict:
@@ -14,13 +14,15 @@ class Solution:
             else:
                 if char_dict[s[r]] >= l:
                     prev_pos = char_dict[s[r]]
-                    l = prev_pos + 1
                     char_dict[s[r]] = r
+                    l = prev_pos + 1
+                    max_len = max(max_len,r - l + 1)
                     r += 1
                 else:
                     char_dict[s[r]] = r
                     max_len = max(max_len,r - l + 1)
                     r += 1
-
         
         return max_len
+
+                    
