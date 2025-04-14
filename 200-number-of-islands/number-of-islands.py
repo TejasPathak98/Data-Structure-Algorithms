5be71e399087c72dcfad6adcfe0b185ae38c624b
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-14 15:40:18 -0700

    Time: 234 ms (85.38%) | Memory: 20 MB (82.91%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -2,30 +2,25 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         m = len(grid)
         n = len(grid[0])
-        count = 0
+
+        no_of_islands = 0
+
+        def dfs(x,y):
+            grid[x][y] = 0
+
+            for dx,dy in [(0,1),(1,0),(0,-1),(-1,0)]:
+                x_ = x + dx
+                y_ = y + dy
+
+                if 0 <= x_ < m and 0 <=  y_ < n and grid[x_][y_] == "1":
+                    dfs(x_,y_)
+
 
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == "1":
-                    self.bfs(i,j,grid)
-                    count +=1
-        
-        return count
+                    no_of_islands += 1
+                    dfs(i,j)
 
-    def bfs(self,x,y,grid):
-        queue = deque([(x,y)])
-        grid[x][y] = "-1"
 
-        while queue:
-            cx,cy = queue.popleft()
-
-            for dx,dy in [(0,1),(0,-1),(1,0),(-1,0)]:
-                x_ = cx + dx
-                y_ = cy + dy
-
-                if x_ >= 0 and x_ < len(grid) and y_ >=0 and y_ < len(grid[0]) and grid[x_][y_] == "1":
-                    grid[x_][y_] = "-1"
-                    queue.append((x_,y_))
-
-    
-    #O(MN) ; O(min(M,N))
+        return no_of_islands
