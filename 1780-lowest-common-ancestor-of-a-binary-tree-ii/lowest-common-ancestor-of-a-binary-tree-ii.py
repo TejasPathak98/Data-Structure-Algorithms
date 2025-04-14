a4bd61c65f016c721fdbda74bccc8800c8cdbd39
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-13 21:06:19 -0700

    Time: 149 ms (65.88%) | Memory: 22.7 MB (6.35%) - LeetSync

--- 1780-lowest-common-ancestor-of-a-binary-tree-ii/lowest-common-ancestor-of-a-binary-tree-ii.py
+++ 1780-lowest-common-ancestor-of-a-binary-tree-ii/lowest-common-ancestor-of-a-binary-tree-ii.py
@@ -7,32 +7,33 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        def Existence(node,X):
-            if node is None:
-                return False
+        def lca_node(node):
 
-            if node.val == X.val:
-                return True
+            if not node:
+                return None,False,False
+            
+            left_node , left_has_p , left_has_q = lca_node(node.left)
 
-            return Existence(node.left,X) or Existence(node.right,X)
+            if left_node:
+                return left_node,True,True
+            
 
-        if Existence(root, p) == False or Existence(root, q) == False:
-            return None
-        else:
-            return self.LCA(root,p,q)
+            right_node , right_has_p , right_has_q = lca_node(node.right)
+
+            if right_node:
+                return right_node,True,True
+
+            
+            node_has_p = (node == p) or left_has_p or right_has_p
+            node_has_q = (node == q) or left_has_q or right_has_q
+
+            if node_has_p and node_has_q:
+
+                return node,node_has_p,node_has_q
+            
+            return None,node_has_p,node_has_q
 
 
-    def LCA(self,root,p,q):
-        if root is None:
-            return root
-
-        if root == p or root == q:
-            return root
-        
-        l = self.LCA(root.left, p, q)
-        r = self.LCA(root.right, p, q)
-
-        if l and r:
-            return root
-        
-        return l if l else r
+        lca , has_p , has_q = lca_node(root)
+        return lca if has_p and has_q else None
+                
