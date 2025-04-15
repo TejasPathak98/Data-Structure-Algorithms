87f0f321fe3b85c9c5c965b56ba82312743f550e
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-14 18:44:09 -0700

    Time: 0 ms (100.00%) | Memory: 18.9 MB (82.80%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,33 +1,28 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        graph = defaultdict(list)
         inDegree = [0] * numCourses
+        graph = defaultdict(list)
 
         for course,pre_req in prerequisites:
             graph[pre_req].append(course)
             inDegree[course] += 1
 
-        visited = [0] * numCourses # 0 for unvisited , 1 for visiting , 2 and for visited
-
-        def dfs(node):
-            if visited[node] == 1:
-                return False
-            if visited[node] == 2:
-                return True
-            
-            visited[node] = 1
-
-            for nei in graph[node]:
-                if not dfs(nei):
-                    return False
-            
-            visited[node] = 2
-            return True
-        
+        queue = deque()
         for i in range(numCourses):
-            if not dfs(i):
-                return False
-        
-        return True
+            if inDegree[i] == 0:
+                queue.append(i)
 
-            
+        courses = []
+
+        while queue:
+            course = queue.popleft()
+            courses.append(course)
+
+            for nei in graph[course]:
+                inDegree[nei] -= 1
+                if inDegree[nei] == 0:
+                    queue.append(nei)
+
+        
+        return len(courses) == numCourses
+
