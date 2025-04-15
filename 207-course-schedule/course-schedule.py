4f0c854157dc214475d33e5b058601b27c876d0a
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-14 19:02:31 -0700

    Time: 0 ms (100.00%) | Memory: 19.1 MB (61.72%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,5 +1,33 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # inDegree = [0] * numCourses
+        # graph = defaultdict(list)
+
+        # for course,pre_req in prerequisites:
+        #     graph[pre_req].append(course)
+        #     inDegree[course] += 1
+
+        # queue = deque()
+        # for i in range(numCourses):
+        #     if inDegree[i] == 0:
+        #         queue.append(i)
+
+        # courses = []
+
+        # while queue:
+        #     course = queue.popleft()
+        #     courses.append(course)
+
+        #     for nei in graph[course]:
+        #         inDegree[nei] -= 1
+        #         if inDegree[nei] == 0:
+        #             queue.append(nei)
+
+        
+        # return len(courses) == numCourses
+
+        #O(V + E) ; O(V + E)
+
         inDegree = [0] * numCourses
         graph = defaultdict(list)
 
@@ -7,22 +35,33 @@ class Solution:
             graph[pre_req].append(course)
             inDegree[course] += 1
 
-        queue = deque()
+        visited = [0] * numCourses
+        #0 -> unvisited
+        #1 ->visiting
+        #2 -> visited
+        
+        def dfs(node):
+            
+            if visited[node] == 2:
+                return True
+            
+            if visited[node] == 1:
+                return False
+            
+            visited[node] = 1
+
+            for nei in graph[node]:
+                if not dfs(nei):
+                    return False
+            
+            visited[node] = 2
+            return True
+
         for i in range(numCourses):
-            if inDegree[i] == 0:
-                queue.append(i)
-
-        courses = []
-
-        while queue:
-            course = queue.popleft()
-            courses.append(course)
-
-            for nei in graph[course]:
-                inDegree[nei] -= 1
-                if inDegree[nei] == 0:
-                    queue.append(nei)
+            if not dfs(i):
+                return False
 
         
-        return len(courses) == numCourses
+        return True
+
 
