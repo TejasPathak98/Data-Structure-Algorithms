73aa7bd6df5a1d560bf646298f1138d5c5f86768
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-14 17:01:47 -0700

    Time: 603 ms (43.46%) | Memory: 20.6 MB (84.43%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,57 +1,40 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # if len(nums) < 3:
-        #     return []
-        # nums = sorted(nums)
-        # ans = set()
-
-        # for i,v in enumerate(nums):
-        #     if i >= 1 and nums[i - 1] == v:
-        #         continue
-        #     triplet_dict = {}
-        #     for x in nums[i + 1:]:
-        #         if x not in triplet_dict:
-        #             triplet_dict[(-x-v)] = 1
-        #         else:
-        #             ans.add((v,x,-x-v))
-        
-        # return list(ans)
-
-        #O(N^2) ; O(N)
-
-        if len(nums) < 3:
-            return []
         nums.sort()
-        ans = []
 
-        for i,v in enumerate(nums):
-            if i >= 1 and nums[i - 1] == v:
+        result = []
+
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            
-            left = i + 1
-            right = len(nums) - 1
 
-            while left < right:
-                total = nums[left] + nums[right] + v
-                if total == 0:
-                    ans.append([v,nums[left],nums[right]])
-                    left += 1
-                    right -= 1
-                
-                    while left < right and nums[left] == nums[left - 1]:
-                        left += 1
-                    
-                    while left < right and nums[right] == nums[right + 1]:
-                        right -= 1
-                
-                elif total < 0:
-                    left += 1
+            j = i + 1
+            k = len(nums) - 1
+
+            while j < k:
+
+                Sum = nums[i] + nums[j] + nums[k]
+
+                if Sum == 0:
+                    result.append([nums[i],nums[j],nums[k]])
+
+                    while j < k and nums[j] == nums[j + 1]:
+                        j += 1
+
+                    while j < k and nums[k] == nums[k - 1]:
+                        k -= 1
+
+                    j += 1
+                    k -= 1
+
+                elif Sum > 0:
+                    k -= 1
                 else:
-                    right -= 1
+                    j += 1
 
-        return ans
+    
+        return result
 
 
+                
 
-
-        
