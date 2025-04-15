466b066b7a0115567122f77dba7c6541c70759ba
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-15 02:32:52 -0700

    Time: 47 ms (28.76%) | Memory: 18.8 MB (19.84%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -2,20 +2,37 @@ class Solution:
     def jump(self, nums: List[int]) -> int:
         currFar = 0
         currEnd = 0
-        jumps = 0
 
         if len(nums) == 1:
             return 0
 
-        for i in range(len(nums)):
-            currFar = max(currFar,i + nums[i])
+        prev = list(range(len(nums)))
 
+        jumps = 0
+
+        i = 0
+
+        for i in range(len(nums)):
+            if i + nums[i] > currFar:
+                currFar = i + nums[i]
+
+                for j in range(i + 1,min(currFar + 1,len(nums))):
+                    prev[j] = i
+ 
             if i == currEnd:
                 currEnd = currFar
                 jumps += 1
 
-                if currEnd >= len(nums)  - 1:
+                if currEnd >= len(nums) - 1:
+                    temp = []
+                    curr = len(nums) - 1
+
+                    temp.append(len(nums) - 1)
+
+                    while curr != 0:
+                        curr = prev[curr]
+                        temp.append(curr)
+                    
+                    print(temp)
                     return jumps
 
-
-        return -1
