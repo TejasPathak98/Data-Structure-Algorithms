4e1b3081cf1dfaaecac56dacdae4a90bbf5ff040
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-14 21:13:47 -0700

    Time: 11 ms (83.13%) | Memory: 21.3 MB (43.68%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,32 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # ans = []
-        # string_dict = defaultdict(list)
-
-        # for string in strs:
-        #     string_dict[tuple(sorted(string))].append(string)
-        
-        # for key,listofstrings in string_dict.items():
-        #     ans.append(listofstrings)
-        
-        # return ans
-
-        #O(N * llogl) ; O(Nk)
-
         ans = []
-        string_dict = defaultdict(list)
 
-        for string in strs:
-            char_count = [0] * 26
+        str_dict = defaultdict(list)
 
-            for ch in string:
-                char_count[ord(ch) - ord('a')] += 1
-            string_dict[tuple(char_count)].append(string)
+        for st in strs:
+            str_dict[tuple(sorted(st))].append(st)
 
-        return list(string_dict.values())
-
-
-
-
-
-        
+        for v in str_dict.values():
+            ans.append(v)
+        return ans
