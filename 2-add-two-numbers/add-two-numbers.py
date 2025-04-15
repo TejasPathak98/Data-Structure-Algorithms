7a55e26a2d1af057faca8e15caeee08498b83edb
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-14 20:48:22 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (85.21%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -5,42 +5,37 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        if not l1 and not l2:
-            return None
-        if not l1:
-            return l2
-        if not l2:
-            return l1
-        
-        dummy = l3 = ListNode(-1)
+
+        dummy = ListNode(-1)
+        l3 = dummy
+
         Sum = 0
         carry = 0
 
         while l1 and l2:
-            Sum = (l1.val + l2.val + carry) % 10
-            carry = (l1.val + l2.val + carry) // 10
-
-            l3.next = ListNode(Sum)
+            Sum = l1.val + l2.val + carry
+            l3.next = ListNode(Sum  % 10)
+            carry = Sum // 10
+            l3 = l3.next
             l1 = l1.next
             l2 = l2.next
-            l3 = l3.next
         
         while l1:
-            Sum = (l1.val + carry) % 10
-            carry = (l1.val + carry) // 10
-
-            l3.next = ListNode(Sum)
+            Sum = l1.val + carry
+            l3.next = ListNode(Sum  % 10)
+            carry = Sum // 10
+            l3 = l3.next
             l1 = l1.next
-            l3 = l3.next
-
-        while l2:
-            Sum = (l2.val + carry) % 10
-            carry = (l2.val + carry) // 10
-            l3.next = ListNode(Sum)
-            l2 = l2.next
-            l3 = l3.next
-
-        if carry > 0:
-            l3.next = ListNode(carry)
         
+        while l2:
+            Sum = l2.val + carry
+            l3.next = ListNode(Sum  % 10)
+            carry = Sum // 10
+            l3 = l3.next
+            l2 = l2.next
+        
+        if carry:
+            l3.next = ListNode(carry)
+
         return dummy.next
+        
