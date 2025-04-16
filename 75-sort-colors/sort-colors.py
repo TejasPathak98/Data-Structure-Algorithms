7295b7f013162a744953bfcf640ba84089bcb5b6
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-15 22:28:41 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (12.29%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,10 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-
-        p1 = 0
-        p2 = len(nums) - 1
-        idx = 0
+        p1 = 0 # to track zero
+        p2 = len(nums) - 1 # to track 2
+        idx = 0 # to iterate through the nums
 
         while idx <= p2:
             if nums[idx] == 0:
@@ -17,6 +16,11 @@ class Solution:
                 nums[idx] = nums[p2]
                 nums[p2] = 2
                 p2 -= 1
-                idx -=1
+                idx -= 1
             idx += 1
+
         
+        return 
+
+
+
