63369d9086e1e084e05e4378a242af20e2d7bfae
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-15 23:16:57 -0700

    Time: 7 ms (99.73%) | Memory: 19.2 MB (64.53%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -1,40 +1,15 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        # freq_counter = Counter(tasks)
-        # max_heap = []
-        # cooldown = deque()
-        # timer = 0
-
-        # for k,v in freq_counter.items():
-        #     heapq.heappush(max_heap, -v)
-
-        # while max_heap or cooldown:
-
-        #     if max_heap:
-        #         f = -heapq.heappop(max_heap)
-
-        #         if f > 1:
-        #             cooldown.append((f - 1,timer + n + 1))
-
-        #     timer += 1
-
-        #     while cooldown and cooldown[0][1] == timer:
-        #         task_c, time = cooldown.popleft()
-        #         heapq.heappush(max_heap, -task_c)
-
-        
-        # return timer
-
-        #O(klogk) ; o(k)
-
         counter = Counter(tasks)
         max_freq = max(counter.values())
-        max_freq_items = sum(1 for item in counter.values() if item == max_freq)
-        return max(len(tasks),(max_freq - 1)*(n + 1) + max_freq_items)
 
+        max_freq_count = 0
 
+        for key,value in counter.items():
+            if value == max_freq:
+                max_freq_count += 1
 
-
+        return max(len(tasks),(max_freq - 1)*(n + 1) + max_freq_count)
 
 
 
