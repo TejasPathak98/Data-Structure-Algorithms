dde71488221eb7505803e285c36348cdc0b320f8
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-15 23:28:22 -0700

    Time: 294 ms (95.12%) | Memory: 57.9 MB (65.45%) - LeetSync

--- 1962-single-threaded-cpu/single-threaded-cpu.py
+++ 1962-single-threaded-cpu/single-threaded-cpu.py
@@ -1,31 +1,27 @@
 class Solution:
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
         n = len(tasks)
-
-        tasks_with_index = [(et,pt,idx) for idx,(et,pt) in enumerate(tasks)]
-
-        tasks_with_index.sort()
-
+        tasks_with_idx = [(et,pt,idx) for idx,(et,pt) in enumerate(tasks)]
+        tasks_with_idx.sort()
         result = []
         task_idx = 0
         task_heap = []
         timer = 0
 
+
         while len(result) < n:
 
-            while task_idx < n and tasks_with_index[task_idx][0] <= timer:
-                et,pt,idx = tasks_with_index[task_idx]
-                heapq.heappush(task_heap, (pt,idx))
+            while task_idx < n and tasks_with_idx[task_idx][0] <= timer:
+                et,pt,ind = tasks_with_idx[task_idx]
+                heapq.heappush(task_heap, (pt,ind))
                 task_idx += 1
 
             if task_heap:
-                pt,idx = heapq.heappop(task_heap)
-                result.append(idx)
+                pt,ind = heapq.heappop(task_heap)
+                result.append(ind)
                 timer += pt
             else:
-                timer = max(timer,tasks_with_index[task_idx][0])
-        
+                timer = max(timer,tasks_with_idx[task_idx][0])
 
         return result
-
 
