5acb367bf9186b6a4330aac016b89812d994f695
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-16 02:28:52 -0700

    Time: 4 ms (47.19%) | Memory: 18 MB (85.36%) - LeetSync

--- 332-reconstruct-itinerary/reconstruct-itinerary.py
+++ 332-reconstruct-itinerary/reconstruct-itinerary.py
@@ -1,20 +1,21 @@
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        # Euler Path Problem
-        #The nodes have to be traversed in a post order fashion, explore all the paths and then add the current node ; there is reversing at the end
-
+        
         graph = defaultdict(list)
 
         for u,v in tickets:
-            heapq.heappush(graph[u], v)
-
-        ans = []
+            heapq.heappush(graph[u],v)
+        
+        path = []
 
         def dfs(node):
-            while graph[node]:
-                neighbor = heapq.heappop(graph[node])
-                dfs(neighbor)
-            ans.append(node)
 
-        dfs("JFK")
-        return ans[::-1]
+            while graph[node]:
+                nei = heapq.heappop(graph[node])
+                dfs(nei)
+            
+            path.append(node)
+
+        dfs('JFK')
+
+        return path[::-1]
