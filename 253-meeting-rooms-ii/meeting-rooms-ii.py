c2aa41da56a39fd809365bcc71f67b16fa77bffe
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-15 21:38:11 -0700

    Time: 3 ms (86.18%) | Memory: 19.9 MB (75.27%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -1,41 +1,17 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        # intervals.sort(key = lambda x : x[0])
+        intervals.sort()
+        meeting_heap = []
+        heapify(meeting_heap)
 
-        # min_heap = []
-        # heapify(min_heap)
-        # meeting_rooms = 0
-
-        # for i in range(len(intervals)):
-        #     if not min_heap:
-        #         heappush(min_heap, intervals[i][1])
-        #         meeting_rooms = max(meeting_rooms,len(min_heap))
-        #     else:
-        #         if intervals[i][0] >= min_heap[0]: #No such functionality like min_heap.peek() (only in Java)
-        #             heappushpop(min_heap, intervals[i][1])
-        #             meeting_rooms = max(meeting_rooms,len(min_heap))
-        #         else:
-        #             heappush(min_heap, intervals[i][1])
-        #             meeting_rooms = max(meeting_rooms,len(min_heap))
-
-
-        # return meeting_rooms # we could also eliminate the variable and directly return the len(min_heap) but for clarity probably its better to keep it
-
-        # #O(NlogN) ; O(N)
-
-        start = sorted([i[0] for i in intervals]) #The start of a new meeting
-        end = sorted([i[1] for i in intervals]) # The end of exisiting meetings
-
-        start_ptr = end_ptr = meeting_rooms = 0
-
-        for start_ptr in range(len(start)):
-            if start[start_ptr] >= end[end_ptr]:
-                end_ptr += 1
+        for interval in intervals:
+            if not meeting_heap:
+                heapq.heappush(meeting_heap,interval[1])
             else:
-                meeting_rooms += 1
+                if meeting_heap[0] > interval[0]:
+                    heapq.heappush(meeting_heap, interval[1])
+                else:
+                    heapq.heapreplace(meeting_heap,interval[1])
         
-        return meeting_rooms
-
-
-
+        return len(meeting_heap)
 
