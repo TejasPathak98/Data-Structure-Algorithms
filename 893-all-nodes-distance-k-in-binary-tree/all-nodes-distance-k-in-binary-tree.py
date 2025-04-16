a913f41e4f7fb135620e3b3f307d8f99faa85dd3
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-15 20:44:46 -0700

    Time: 44 ms (46.16%) | Memory: 18.3 MB (12.67%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.py
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.py
@@ -7,44 +7,46 @@
 
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
-        parent = {}
         queue = deque([root])
+        parent = {}
 
         while queue:
             node = queue.popleft()
+
             if node.left:
-                parent[node.left.val] = node
                 queue.append(node.left)
+                parent[node.left.val] = node
             if node.right:
-                parent[node.right.val] = node
                 queue.append(node.right)
+                parent[node.right.val] = node
         
-        ans = []
+
         queue = deque([target])
-        visited = {}
+        visited = set()
+        visited.add(target)
+        ans = []
 
         while queue and k > 0:
             size = len(queue)
+
             for _ in range(size):
+
                 node = queue.popleft()
-                visited[node] = True
 
                 if node.left and node.left not in visited:
                     queue.append(node.left)
+                    visited.add(node.left)
                 if node.right and node.right not in visited:
                     queue.append(node.right)
+                    visited.add(node.right)
                 if node.val in parent and parent[node.val] not in visited:
                     queue.append(parent[node.val])
-
+                    visited.add(parent[node.val])
+            
             k -= 1
 
         while queue:
             ans.append(queue.pop().val)
         
         return ans
-
-
-
-
-
-        
+            
