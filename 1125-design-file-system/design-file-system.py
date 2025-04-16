832c2cc71e695a729bdda9808055d78de657fbc2
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-15 17:22:57 -0700

    Time: 59 ms (29.35%) | Memory: 25.2 MB (97.77%) - LeetSync

--- 1125-design-file-system/design-file-system.py
+++ 1125-design-file-system/design-file-system.py
@@ -2,34 +2,31 @@ class FileSystem:
 
     def __init__(self):
         self.file_dict = {}
-        self.paths = set()
 
     def createPath(self, path: str, value: int) -> bool:
-        if path in self.paths:
+        if path in self.file_dict:
             return False
+        
         slash_count = 0
         for ch in path:
-            if ch == "/":
+            if ch == '/':
                 slash_count += 1
         if slash_count == 1:
-            self.paths.add(path)
             self.file_dict[path] = value
             return True
-        elif slash_count > 1:
-            file_split_parent = path.split("/")[:-1]
-            if "/".join(file_split_parent) not in self.paths:
+        else:
+            parent = path.split('/')[:-1]
+            if '/'.join(parent) not in self.file_dict:
                 return False
             else:
-                self.paths.add(path)
                 self.file_dict[path] = value
                 return True
-        
+
     def get(self, path: str) -> int:
         if path not in self.file_dict:
             return -1
         else:
             return self.file_dict[path]
-        
 
 
 # Your FileSystem object will be instantiated and called as such:
