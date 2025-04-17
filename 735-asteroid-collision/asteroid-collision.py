76c05e782d3a9a7b2173441d62bd33f29a848d3d
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-16 22:06:49 -0700

    Time: 4 ms (78.34%) | Memory: 19 MB (20.07%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -1,39 +1,18 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        temp = []
-        i = 0
+        
+        stack = []
 
-        while i < len(asteroids) - 1:
-            x = asteroids[i]
-            y = asteroids[i + 1]
-
-            if x*y > 0 or (x < 0 and y > 0):
-                i += 1
-                continue
+        for asteroid in asteroids:
+            while stack and asteroid < 0 < stack[-1]: #negative asteroid found and top of stack in positive
+                if stack[-1] < -asteroid:
+                    stack.pop() #The +ve value is popped out
+                    continue # we continue to match with remaining asteroids
+                elif stack[-1] == -asteroid:
+                    stack.pop() #Both are same size , both expplode
+                break
             else:
-                if abs(x) == abs(y):
-                    asteroids[i] = 2000
-                    asteroids[i + 1] = 2000
-                    i += 2
-                    continue
-                else:
-                    if abs(x) > abs(y):
-                        asteroids[i + 1] = 2000
-                    else:
-                        asteroids[i] = 2000
-                    i += 2
-                    continue
+                stack.append(asteroid) # for positive asteroid and for negative asteroid who have beaten all the +Ve ones and gotten here from continue(basically those which have skipped the break)
 
         
-        for i in range(len(asteroids)):
-            if asteroids[i] == 2000:
-                continue
-            else:
-                temp.append(asteroids[i])
-
-    
-        if all(x > 0 for x in temp) > 0 or all(x < 0 for x in temp) < 0 or len(temp) == len(asteroids):
-            return temp
-        else:
-            return self.asteroidCollision(temp)
-
+        return stack
