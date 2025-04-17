685aeea9a57bd6fa3765eb30702c02babde6fd51
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-17 13:28:52 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (25.22%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,33 +1,31 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = [set() for _ in range(9)] 
-        cols = [set() for _ in range(9)] 
-        boxes =  [set() for _ in range(9)] 
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
 
         for i in range(9):
             for j in range(9):
-                if board[i][j] == ".":
+                digit = board[i][j]
+
+                if digit == ".":
                     continue
-                number = int(board[i][j])
 
-                if number in rows[i]:
+                if digit in rows[i]:
                     return False
                 else:
-                    rows[i].add(number)
-
-                if number in cols[j]:
-                    
-                    return False
-                else:
-                    cols[j].add(number)
+                    rows[i].add(digit)
                 
-                box_idx = (i // 3)*3 + (j // 3)
-
-                if number in boxes[box_idx]:
-                    
+                if digit in cols[j]:
                     return False
                 else:
-                    boxes[box_idx].add(number)
-        
+                    cols[j].add(digit)
+                
+                box_idx = (i // 3) * 3 + (j // 3)
+
+                if digit in boxes[box_idx]:
+                    return False
+                else:
+                    boxes[box_idx].add(digit)
+
         return True
-        
