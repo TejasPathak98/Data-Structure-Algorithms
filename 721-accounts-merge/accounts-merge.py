2302ef8e8d4bbf60448993630c68453a404f1f61
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-17 00:11:07 -0700

    Time: 15 ms (97.42%) | Memory: 20.7 MB (83.28%) - LeetSync

--- 721-accounts-merge/accounts-merge.py
+++ 721-accounts-merge/accounts-merge.py
@@ -1,28 +1,32 @@
-class UF:
+class UnionFind:
     def __init__(self,N):
         self.parents = list(range(N))
-    def union(self,child,parent):
-        self.parents[self.find(child)] = self.find(parent)
     def find(self,x):
         if x != self.parents[x]:
             self.parents[x] = self.find(self.parents[x])
         return self.parents[x]
+    def union(self,child,parent):
+        self.parents[self.find(child)]  =self.find(parent)
 
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
-        ownership = {}
-        uf = UF(len(accounts))
+        uf = UnionFind(len(accounts))
+
+        email_to_owner = {}
 
         for i,(_,*emails) in enumerate(accounts):
             for email in emails:
-                if email in ownership:
-                    uf.union(i, ownership[email])
-                ownership[email] = i
-        
-        my_dict = defaultdict(list)
-
-        for email,owner in ownership.items():
-            my_dict[uf.find(owner)].append(email)
+                if email in email_to_owner:
+                    uf.union(i, email_to_owner[email])
+                email_to_owner[email] = i
         
 
-        return [[accounts[i][0]] + sorted(emails) for i,emails in my_dict.items()]
+        owner_to_email = defaultdict(list)
+
+        for email,owner in email_to_owner.items():
+            owner_to_email[uf.find(owner)].append(email)
+
+        
+        return [[accounts[i][0]] + sorted(emails) for i,emails in owner_to_email.items()]
+
+        
