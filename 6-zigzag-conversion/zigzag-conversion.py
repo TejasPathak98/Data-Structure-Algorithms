9cd024997a8641e531960e6ec3dd36b3538c0bca
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-18 14:57:01 -0700

    Time: 39 ms (11.65%) | Memory: 18.3 MB (8.66%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.py
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -1,26 +1,36 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        if numRows == 1 or numRows >= len(s):
+        if numRows == 1:
             return s
         
-        rows = [[]for _ in range(numRows)]
-        d = 1
-        idx = 0
+        rows = [[] for _ in range(numRows)]
+        print(len(rows))
 
-        for ch in s:
-            rows[idx].append(ch)
-            if idx == 0:
-                d = 1
-            elif idx == numRows - 1:
-                d = -1
-            idx += d
-        
-        for i in range(numRows):
-            rows[i] = ''.join(rows[i])
-        
-        return ''.join(rows)
-        
+        i = 0
+        while i < len(s):
+            
+            j = 0
+            while j < numRows and i < len(s):
+                rows[j].append(s[i])
+                i += 1
+                j += 1
 
-        
+            j = numRows - 2
 
+            while j > 0 and i < len(s):
+                rows[j].append(s[i])
+                j -= 1
+                i += 1
+
+
+
+
+        final_row = []
+        for r in rows:
+            print(r)
+            final_row.extend(r)
         
+        
+        return "".join(final_row)
+
+
