12b3754d5a5ae54b6d1aad0f718216a2b90902aa
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-19 13:33:42 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (23.33%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,31 +1,37 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+
         if not digits:
             return []
-
-        char_dict  = {
-            "2" : ["a","b","c"],
-            "3" : ["d","e","f"],
-            "4" : ["g","h","i"],
-            "5" : ["j","k","l"],
-            "6" : ["m","n","o"],
-            "7" : ["p","q","r","s"],
-            "8" : ["t","u","v"],
-            "9" : ["w","x","y","z"]
+        
+        phone_map = {
+            "2" : "abc",
+            "3" : "def",
+            "4" : "ghi",
+            "5" : "jkl",
+            "6" : "mno",
+            "7" : "pqrs",
+            "8" : "tuv",
+            "9" : "wxyz"
         }
 
+        result = []
 
-        ans = []
-
-        def helper(temp,next_digit):
-            if len(temp) == len(digits):
-                ans.append("".join(temp[:]))
+        def backtracking(index,temp):
+            if index == len(digits):
+                result.append("".join(temp[:]))
                 return
             
-            for ch in char_dict[digits[next_digit]]:
+            if index > len(temp):
+                return
+            
+            for ch in phone_map[digits[index]]:
                 temp.append(ch)
-                helper(temp,next_digit + 1)
+                backtracking(index + 1, temp)
                 temp.pop()
 
-        helper([],0)
-        return ans
+        
+        backtracking(0,[])
+        return result
+
+
