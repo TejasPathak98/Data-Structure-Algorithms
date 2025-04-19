4fb132fdcd5fa5d3d2dd18d360af8666ea54e41b
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-19 16:04:05 -0700

    Time: 22 ms (91.32%) | Memory: 18.1 MB (18.22%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -1,53 +1,60 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
-        results = []
-        self.helper(nums,results,4,target,[])
-        return results
+        result = []
+        self.helper(nums,target,4,result,[])
+        return result
 
-    def helper(self,nums,results,N,target,intermediate_res):
+    def helper(self,nums,target,N,result,intermediate_results):
         if N < 2 or len(nums) < N:
             return
         
         elif N == 2:
-            output_res = self.TwoSum(nums,target)
-            if output_res != []:
-                for idx in output_res:
-                    results.append(intermediate_res + idx)
+            res = self.TwoSum(nums,target)
+            if res != []:
+                for arr in res:
+                    result.append(intermediate_results + arr)
         
         else:
-            for i in range(len(nums) - N + 1):
+
+            for i in range(len(nums) - N  + 1):
                 if nums[i] * N > target or nums[-1] * N < target:
                     break
                 elif i == 0 or nums[i] != nums[i - 1]:
-                    self.helper(nums[i + 1:], results, N - 1, target - nums[i], [nums[i]] +  intermediate_res)
+                    self.helper(nums[i + 1:], target - nums[i], N - 1, result, [nums[i]] + intermediate_results)
+        
 
+    
     def TwoSum(self,nums,target):
-        res = []
-        left = 0
-        right = len(nums) - 1
+        l = 0
+        r = len(nums) - 1
+        result = []
 
-        while left < right:
-            if nums[left] + nums[right] == target:
-                res.append([nums[left],nums[right]])
+        while l < r:
 
-                while left < right and nums[left] == nums[left + 1]:
-                    left += 1
+            if nums[l] + nums[r] == target:
+                result.append([nums[l],nums[r]])
+            
+                while l < r and nums[l] == nums[l + 1]:
+                    l += 1
                 
-                while left < right and nums[right] == nums[right - 1]:
-                    right -= 1
-                
-                left += 1
-                right -= 1
+                while l < r and nums[r] == nums[r - 1]:
+                    r -= 1
 
-            elif nums[left] + nums[right] > target:
-                right -= 1
+                l += 1
+                r -= 1
+            
+            elif nums[l] + nums[r] < target:
+
+                l += 1
             else:
-                left += 1
-        
-        return res
 
+                r -= 1
         
+        return result
 
 
-        
+
+
+
+
