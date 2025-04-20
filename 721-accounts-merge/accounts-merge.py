6b2ed092b9a0727aa3d266e97b39daa25347e111
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-20 15:38:48 -0700

    Time: 15 ms (97.44%) | Memory: 20.6 MB (91.40%) - LeetSync

--- 721-accounts-merge/accounts-merge.py
+++ 721-accounts-merge/accounts-merge.py
@@ -6,27 +6,27 @@ class UnionFind:
             self.parents[x] = self.find(self.parents[x])
         return self.parents[x]
     def union(self,child,parent):
-        self.parents[self.find(child)]  =self.find(parent)
+        self.parents[self.find(child)] = self.find(parent)
 
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
         uf = UnionFind(len(accounts))
 
-        email_to_owner = {}
+        email_to_user_mapping = {}
 
-        for i,(_,*emails) in enumerate(accounts):
+        for idx,(_,*emails) in enumerate(accounts):
             for email in emails:
-                if email in email_to_owner:
-                    uf.union(i, email_to_owner[email])
-                email_to_owner[email] = i
+                if email in email_to_user_mapping:
+                    uf.union(idx, email_to_user_mapping[email])
+                email_to_user_mapping[email] = idx
+            
         
+        user_to_email_mapping = defaultdict(list)
 
-        owner_to_email = defaultdict(list)
-
-        for email,owner in email_to_owner.items():
-            owner_to_email[uf.find(owner)].append(email)
-
+        for email,user in email_to_user_mapping.items():
+            user_to_email_mapping[uf.find(user)].append(email)
         
-        return [[accounts[i][0]] + sorted(emails) for i,emails in owner_to_email.items()]
+        return [[accounts[i][0]] + sorted(emails) for i,emails in user_to_email_mapping.items()]
+
 
         
