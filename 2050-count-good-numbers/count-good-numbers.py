89310efe2dc7d9121da072404ec298122b8a3553
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-20 15:23:16 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (5.18%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.py
+++ 2050-count-good-numbers/count-good-numbers.py
@@ -1,40 +1,22 @@
 class Solution:
-    MOD = (10 ** 9 + 7)
+
+    MOD = 10**9 + 7
 
     def countGoodNumbers(self, n: int) -> int:
-        even_digits = 0
-        odd_digits = 0
+        even_digits = (n + 1) // 2
+        odd_digits = n // 2
 
-        if n == 1:
-            even_digits = 1
-        else:
-            if n % 2 == 0:
-                even_digits = odd_digits = n // 2
-            else:
-                even_digits = n // 2 + 1
-                odd_digits = n // 2
-
-        
         return (self.helper(5,even_digits) * self.helper(4,odd_digits)) % self.MOD
-
-   
+    
     def helper(self,x,n):
-        
+
+        base = x % self.MOD
         result = 1
-        base = x % self.MOD 
-        
+
         while n > 0:
             if n % 2 == 1:
-                result = (result * base) % self.MOD
+                result = (result * base) % self. MOD
             base = (base * base) % self.MOD
-            n = n //2
-
-        return result 
-
-
-
-
-            
-
-
-
+            n = n // 2
+        
+        return result
