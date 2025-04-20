9a599a14e2a61b2ca96eb9313f6dbd9be692a39e
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-19 18:47:56 -0700

    Time: 3 ms (46.55%) | Memory: 23.3 MB (80.83%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -8,37 +8,55 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        if not head or not head.next or not head.next.next:
-            return
-        
-        curr = head
-        slow = fast = curr
+        if not head or not head.next:
+            return head
+
+        slow = head
+        fast = head
+        prev = head
 
         while fast and fast.next:
-            fast = fast.next.next
+            prev = slow
             slow = slow.next
+            fast = fast.next.next
         
-        prev = None
-        curr = slow
+        prev.next = None
 
-        while curr:
-            temp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = temp
-        
-        l1 = head
-        l2 = prev
+        def reverse_helper(head):
+            prev = None
+            Next = None
+            curr = head
 
-        while l2.next:
-            t1= l1.next
-            t2 = l2.next
+            while curr:
+                Next = curr.next
+                curr.next = prev
+                prev = curr
+                curr = Next
 
-            l1.next = l2
-            l2.next = t1
+            return prev
 
-            l1 = t1
-            l2 = t2
+        slow = reverse_helper(slow)
+        dummy = ListNode(-1)
+        dummy.next = head
+        curr2 = slow
+        curr1 = head
+        prev = curr1
+
+        while curr1.next:
+            temp1 = curr1.next
+            temp2 = curr2.next
+
+            curr1.next = curr2
+            curr2.next = temp1
+            curr1 = curr1.next.next
+            curr2 = temp2
         
+        if curr2:
+            curr1.next = curr2
         
-        
+        return dummy.next
+
+
+
+
+
