ec8573dcfdf462b5239ff0d82a22aa8755910b28
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-20 14:40:40 -0700

    Time: 7 ms (58.76%) | Memory: 18.9 MB (62.54%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -1,18 +1,17 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        
         stack = []
 
         for asteroid in asteroids:
-            while stack and asteroid < 0 < stack[-1]: #negative asteroid found and top of stack in positive
+            while stack and asteroid < 0 < stack[-1]:
                 if stack[-1] < -asteroid:
-                    stack.pop() #The +ve value is popped out
-                    continue # we continue to match with remaining asteroids
+                    stack.pop()
+                    continue
                 elif stack[-1] == -asteroid:
-                    stack.pop() #Both are same size , both expplode
+                    stack.pop()
                 break
             else:
-                stack.append(asteroid) # for positive asteroid and for negative asteroid who have beaten all the +Ve ones and gotten here from continue(basically those which have skipped the break)
+                stack.append(asteroid)
 
         
         return stack
