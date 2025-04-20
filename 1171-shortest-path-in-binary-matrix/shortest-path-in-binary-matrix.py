b2e3eb2768638aecb487b9a046a6b973b415b250
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-19 18:32:11 -0700

    Time: 199 ms (59.36%) | Memory: 19.6 MB (19.12%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -1,37 +1,37 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         n = len(grid)
-        m = len(grid[0])
-
-        if grid[0][0] == 1 or grid[n - 1][m - 1] == 1:
-            return -1
-        
-        queue = deque()
-        queue.append((0,0))
-        dir = [(0,1),(0,-1),(1,0),(-1,0),(1,1),(1,-1),(-1,1),(-1,-1)]
+        queue = deque([(0,0)])
         visited = set()
         visited.add((0,0))
-        path = 0
-        
+
+        if grid[0][0] == 1 or grid[n - 1][n- 1] == 1:
+            return -1
+
+        directions = [(0,1),(0,-1),(1,0),(-1,0),(1,1),(1,-1),(-1,1),(-1,-1)]
+
+        path = 1
         while queue:
-            size = len(queue)
-            path += 1
-            for _ in range(size):
+
+            for _ in range(len(queue)):
+
                 x,y = queue.popleft()
 
-                if x == n - 1 and y == m - 1:
+                if x == n - 1 and y == n - 1:
                     return path
+                
+                for dx,dy in directions:
+                    nx = x + dx
+                    ny = y + dy
 
-                for dx,dy in dir:
-                    new_x = x + dx
-                    new_y = y + dy
-
-                    if 0 <= new_x < n and 0 <= new_y < m and grid[new_x][new_y] == 0 and (new_x,new_y) not in visited:
-                        queue.append((new_x,new_y))
-                        visited.add((new_x,new_y))
-
+                    if 0 <= nx < n and 0 <= ny < n and (nx,ny) not in visited and grid[nx][ny] == 0:
+                        queue.append((nx,ny))
+                        visited.add((nx,ny))
+            
+            path += 1
+        
         return -1
 
 
 
-        
+
