2348ebb3f269a7eed0a4f2e2d1306fe846400984
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-21 12:05:01 -0700

    Time: 163 ms (67.21%) | Memory: 44.7 MB (38.49%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.py
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.py
@@ -1,31 +1,31 @@
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
         n = len(startTime)
-        jobs = [[startTime[i] , endTime[i] , profit[i]] for i in range(n)]
-        jobs.sort(key = lambda x : x[0])
+        jobs = [(startTime[i],endTime[i],profit[i]) for i in range(n)]
+        jobs.sort()
         startTime.sort()
-        dp = [-1] * len(startTime)
-       
+        dp = [-1] * n
 
-        return self.helper(0,jobs,startTime,dp,n)
+        return self.helper(0,jobs,dp,startTime,n)
+    
+    def helper(self,i,jobs,dp,startTime,n):
+        #dp[i] = max(dp1,dp2) 
+        #where dp1 = profit[i] + helper(at the next start time) and dp2 = helper(for the next job in Sequence)
 
-    def helper(self,i,jobs,startTime,dp,n):
         if i >= n:
             return 0
         if dp[i] != -1:
             return dp[i]
-
-        index_of_next_job = bisect.bisect_left(startTime,jobs[i][1])
-
-        dp1 = jobs[i][2] + self.helper(index_of_next_job,jobs,startTime,dp,n)
-        dp2 = self.helper(i + 1,jobs,startTime,dp,n)
-        dp[i] = max(dp1,dp2)
-        return dp[i]
-
-
-
         
 
+        next_job_start_time = jobs[i][1]
+        idx = bisect.bisect_left(startTime, next_job_start_time)
+
+        dp1 = jobs[i][2] + self.helper(idx,jobs,dp,startTime,n)
+        dp2 = self.helper(i + 1,jobs,dp,startTime,n)
+
+        dp[i] = max(dp1,dp2)
+        return dp[i]
 
 
 
