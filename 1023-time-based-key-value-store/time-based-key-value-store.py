42bb4566c29e050cba89b2dc9219bf2df4d6b5ae
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-20 22:27:10 -0700

    Time: 92 ms (82.28%) | Memory: 73.9 MB (90.49%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.py
+++ 1023-time-based-key-value-store/time-based-key-value-store.py
@@ -1,35 +1,23 @@
 class TimeMap:
 
     def __init__(self):
-        self.time_dict = defaultdict(list)
-        
+        self.my_dict = defaultdict(list)
+
     def set(self, key: str, value: str, timestamp: int) -> None:
-        self.time_dict[key].append([timestamp,value])
-        
+        self.my_dict[key].append((timestamp,value))
+
     def get(self, key: str, timestamp: int) -> str:
-        if key not in self.time_dict or timestamp < self.time_dict[key][0][0]:
+        if key not in self.my_dict:
             return ""
         else:
-            list_of_timestamps_and_values = self.time_dict[key]
-            l = 0
-            r = len(list_of_timestamps_and_values) - 1
+            values = self.my_dict[key]
+            idx = bisect.bisect_right(values,(timestamp,chr(137)))
+            if idx - 1 >= 0:
+                _ , val = values[idx - 1]
 
-            while l <= r:
-                mid = (l + r) // 2
-                
-                if list_of_timestamps_and_values[mid][0] == timestamp:
-                    return list_of_timestamps_and_values[mid][1]
-                
-                elif list_of_timestamps_and_values[mid][0] > timestamp:
-                    r = mid - 1
-                else:
-                    l = mid + 1
-            
+                return val
+            return ""
 
-            return list_of_timestamps_and_values[r][1]
-
-
-        
 
 
 # Your TimeMap object will be instantiated and called as such:
