ed33ad336ceaa66906f4130ac91c3e35f147a5de
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-21 14:51:50 -0700

    Time: 11 ms (63.52%) | Memory: 20.4 MB (35.93%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -5,60 +5,11 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-    #     if  len(lists) == 0 or not lists:
-    #         return None
+        #we take the heads of all linked lists and put them into a heap based on their vals and get the first one out
+        #then we use that to start the final list and we add its next pointer also to the heap
+        # in the same way, we continue
 
-    #     while len(lists) >= 2:
-    #         x = lists.pop()
-    #         if lists:
-    #             y = lists.pop()
-    #         else:
-    #             return x
-    #         lists.append(self.merge_two_lists(x,y))
-
-    #     return lists[0]
-        
-    # def merge_two_lists(self,l1,l2):
-    #     if not l1 and not l2:
-    #         return None
-    #     if not l1:
-    #         return l2
-    #     if not l2:
-    #         return l1
-        
-    #     l3 = dummy = ListNode(-1)
-
-
-    #     while l1 and l2:
-    #         if l1.val <= l2.val:
-    #             l3.next = l1
-    #             l1 = l1.next
-    #         else:
-    #             l3.next = l2
-    #             l2 = l2.next
-    #         l3 = l3.next
-        
-    #     if l1:
-    #         l3.next = l1
-    #     elif l2:
-    #         l3.next = l2
-        
-    #     return dummy.next
-
-
-    #     while len(lists) >= 2:
-    #         x = lists.pop()
-    #         if lists:
-    #             y = lists.pop()
-    #         else:
-    #             return x
-    #         lists.append(self.merge_two_lists(x,y))
-
-    #     return lists[0]
-
-    #O (Nk)
-
-        if  len(lists) == 0 or not lists:
+        if not lists or len(lists) == 0:
             return None
         
         min_heap = []
@@ -70,17 +21,16 @@ class Solution:
         dummy = curr = ListNode(-1)
 
         while min_heap:
-            val,idx,node = heapq.heappop(min_heap)
-            curr.next = node
-            curr = curr.next
-            if node.next:
-                heapq.heappush(min_heap, (node.next.val,idx,node.next))
+            _,idx,node = heapq.heappop(min_heap)
+            if node:
+                curr.next = node
+                curr = curr.next
+                if node.next:
+                    heapq.heappush(min_heap, (node.next.val,idx,node.next))
         
+
         return dummy.next
 
-    
-    
-        
 
 
 
