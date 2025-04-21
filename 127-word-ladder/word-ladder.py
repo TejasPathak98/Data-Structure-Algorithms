3e90abb894c1beef18d38421b5e1e1bee39d0391
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-21 14:18:17 -0700

    Time: 316 ms (41.89%) | Memory: 18.7 MB (84.05%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -1,78 +1,37 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        def helper(w1,w2):
-            l = len(w1)
-            count = 0
-            
-            for j in range(l):
-                if w1[j] == w2[j]:
-                    count += 1
-            if count == l - 1:
-                return True
-            else:
-                return False
-
-        queue = deque()
-
-        queue.append(beginWord)
-        steps = 1
-        visited = set([beginWord])
-
-        while queue:
-            for _ in range(len(queue)):
-                node = queue.popleft()
-
-                if node == endWord:
-                    return steps
-                
-                for word in wordList:
-                    if word not in visited and helper(word,node):
-                        queue.append(word)
-                        visited.add(word)
-            
-            steps += 1
+        if len(endWord) > len(beginWord):
+            return 0
         
+        wordList = set(wordList)
+        wordList.discard(beginWord)
 
-        return 0
+        queue = deque([beginWord])
+        count = 1
 
-        #O(M×L×N) ; O(M +N)
-
-        #Bidirectional BFS
-
-        forward_queue = {beginWord}
-        back_queue = {endWord}
         visited = set()
         visited.add(beginWord)
-        visited.add(endWord)
-        count = 0
-        wordList = set(wordList)
 
-        while forward_queue and back_queue:
+        while queue:
 
-            if len(forward_queue) > len(back_queue):
-                forward_queue , back_queue = back_queue , forward_queue
+            for _ in range(len(queue)):
 
-            count += 1
-            next_level = set()
+                word = queue.popleft()
 
-            for word in forward_queue:
-                for i in len(word):
-                    for ch in 'abcedfghijklmnopqrstuvwxyz':
+                if word == endWord:
+                    return count
+                
+                for i in range(len(word)):
+                    for ch in 'abcdefghijklmnopqrstuvwxyz':
                         new_word = word[:i] + ch + word[i + 1:]
 
-                        if new_word in back_queue:
-                            return count
-                        
-                        if new_word not in visited and new_word in wordList:
+                        if new_word in wordList and new_word not in visited:
+                            queue.append(new_word)
                             visited.add(new_word)
-                            next_level.add(new_word)
+                
+            count += 1
+        
 
-            forward_queue = next_level
 
         return 0
-
-
-
-
-
 
