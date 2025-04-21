010dadabfd1d1fd9b438b3a8d867bb70b2f4ba4d
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-21 12:18:38 -0700

    Time: 18 ms (44.59%) | Memory: 19.1 MB (99.02%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -7,18 +7,16 @@ class Solution:
         max_area = 0
 
         while left < right:
-            if height[left] > leftMax:
-                leftMax = height[left]
-            if height[right] > rightMax:
-                rightMax = height[right]
-            
+
+            leftMax = max(height[left],leftMax)
+            rightMax = max(height[right],rightMax)
+
             if leftMax <= rightMax:
                 max_area += (leftMax - height[left])
                 left += 1
             else:
                 max_area += (rightMax - height[right])
                 right -= 1
+        
 
         return max_area
-
-
