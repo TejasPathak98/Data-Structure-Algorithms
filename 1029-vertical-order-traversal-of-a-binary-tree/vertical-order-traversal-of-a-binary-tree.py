8edd333190a90655c7de2090df415bd97bb54a1e
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-22 14:49:40 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (98.11%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
@@ -6,21 +6,27 @@
 #         self.right = right
 class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
-        col_dict = defaultdict(list)
-        result = []
+        if not root:
+            return []
+        
+        nodes_dict = defaultdict(list)
 
         def dfs(node,row,col):
             if not node:
                 return None
-            col_dict[col].append((row,node.val))
+
+            nodes_dict[col].append((row,node.val))
+            
             dfs(node.left,row + 1,col - 1)
-            dfs(node.right,row + 1,col + 1)
+            dfs(node.right,row +1,col + 1)
         
         dfs(root,0,0)
-        
-        for col in sorted(col_dict.keys()):
-            #Sort by row, then value
-            col_dict[col].sort(key = lambda x : (x[0],x[1]))
-            result.append([val for row,val in col_dict[col]])
+
+        result = []
+
+        for col in sorted(nodes_dict.keys()):
+            nodes_dict[col].sort(key = lambda  x : (x[0],x[1]))
+            result.append([val for key,val in nodes_dict[col]])
         
         return result
+            
