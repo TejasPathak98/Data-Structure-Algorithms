7f667607b5f47225aaf3b07b72ceb02bd422f6a2
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-22 10:21:17 -0700

    Time: 408 ms (95.18%) | Memory: 18.7 MB (89.36%) - LeetSync

--- 212-word-search-ii/word-search-ii.py
+++ 212-word-search-ii/word-search-ii.py
@@ -1,61 +1,54 @@
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
         def build_trie(words):
-            root = {}
+            Trie = {}
 
             for word in words:
-                curr = root
+                curr = Trie
                 for ch in word:
                     if ch not in curr:
                         curr[ch] = {}
                     curr = curr[ch]
                 curr['#'] = word
+        
+            return Trie
 
-            return root
-
-
-        def dfs(i,j,Trie):
-            letter = board[i][j]
+        
+        def dfs(x,y,Trie):
+            letter = board[x][y]
             curr = Trie[letter]
 
             word = curr.pop('#',False)
-
             if word:
                 result.append(word)
-
             
-            temp = board[i][j]
+            temp = board[x][y]
 
-            board[i][j] = "*"
+            board[x][y] = "*"
 
             for dx,dy in [(0,1),(0,-1),(1,0),(-1,0)]:
-                ni = i + dx
-                nj = j + dy
+                nx = x + dx
+                ny = y + dy
 
-                if 0 <= ni < m and 0 <= nj < n and board[ni][nj] in curr:
-                    dfs(ni,nj,curr)
-
-            board[i][j] = temp
+                if 0 <= nx < m and 0 <= ny < n and board[nx][ny] in curr:
+                    dfs(nx,ny,curr)
 
             
+            board[x][y] = temp
+
             if not curr:
                 Trie.pop(letter)
 
-        Trie = build_trie(words)
-        result = []
-
         m = len(board)
         n = len(board[0])
+        Trie = build_trie(words)
+        result = []
 
         for i in range(m):
             for j in range(n):
                 if board[i][j] in Trie:
                     dfs(i,j,Trie)
-
         
+
         return result
-
-
-                    
-
 
