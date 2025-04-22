62ac423a0f5f596d0d8425b61aa9ef9bd610c977
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-22 09:48:00 -0700

    Time: 139 ms (60.79%) | Memory: 40.8 MB (5.09%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -8,13 +8,11 @@ class MedianFinder:
         
         heapq.heappush(self.max_heap,-num)
 
-        if self.max_heap and self.min_heap and -self.max_heap[0] > self.min_heap[0]:
-            heapq.heappush(self.min_heap,-heapq.heappop(self.max_heap))
-
         if len(self.max_heap) > len(self.min_heap) + 1:
             heapq.heappush(self.min_heap,-heapq.heappop(self.max_heap))
         
-       
+        if self.max_heap and self.min_heap and -self.max_heap[0] > self.min_heap[0]:
+            heapq.heappush(self.min_heap,-heapq.heappop(self.max_heap))
 
         if len(self.min_heap) > len(self.max_heap):
             heapq.heappush(self.max_heap,-heapq.heappop(self.min_heap))
