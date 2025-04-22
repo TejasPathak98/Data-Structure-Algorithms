2dd4eba510ae8d01c370cd98b3544e7c7151d5b1
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-22 09:47:10 -0700

    Time: 159 ms (39.19%) | Memory: 40.8 MB (5.09%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -3,28 +3,26 @@ class MedianFinder:
     def __init__(self):
         self.min_heap = []
         self.max_heap = []
-        heapq.heapify(self.min_heap)
-        heapq.heapify(self.max_heap)
 
     def addNum(self, num: int) -> None:
-        heapq.heappush(self.max_heap, -num) #Add to Max_heap
+        
+        heapq.heappush(self.max_heap,-num)
 
-        if len(self.max_heap) > len(self.min_heap) + 1:  #Balance according to size
-            heapq.heappush(self.min_heap, -heapq.heappop(self.max_heap))
+        if len(self.max_heap) > len(self.min_heap) + 1:
+            heapq.heappush(self.min_heap,-heapq.heappop(self.max_heap))
+        
+        if self.max_heap and self.min_heap and -self.max_heap[0] > self.min_heap[0]:
+            heapq.heappush(self.min_heap,-heapq.heappop(self.max_heap))
 
-        if self.max_heap and self.min_heap and -self.max_heap[0] > self.min_heap[0]: #Balance according to root element of heap
-            heapq.heappush(self.min_heap, -heapq.heappop(self.max_heap))
-
-        if len(self.max_heap) < len(self.min_heap): #Rebalance if necessary
-            heapq.heappush(self.max_heap, -heapq.heappop(self.min_heap))
+        if len(self.min_heap) > len(self.max_heap):
+            heapq.heappush(self.max_heap,-heapq.heappop(self.min_heap))
 
     def findMedian(self) -> float:
-        total_length = len(self.min_heap) + len(self.max_heap)
 
-        if total_length % 2 == 0:
-            return (-self.max_heap[0] + self.min_heap[0]) / 2.0
-        else:
+        if (len(self.max_heap) + len(self.min_heap)) % 2 == 1:
             return -self.max_heap[0]
+        else:
+            return (-self.max_heap[0] + self.min_heap[0]) / 2.0
 
 
 # Your MedianFinder object will be instantiated and called as such:
