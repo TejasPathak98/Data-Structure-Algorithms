071ac2e4da331fbb7fb24a7bd195ae18abb941cf
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-23 01:28:09 -0700

    Time: 206 ms (52.35%) | Memory: 26.2 MB (64.39%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -2,36 +2,33 @@ class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
         max_deque = deque() #monotonically decreasing
         min_deque = deque() #monotonically increasing
-        i = 0
-        max_len = 0
 
-        for j in range(len(nums)):
+        j = 0
+        max_len = float('-inf')
 
-            while max_deque and nums[j] > max_deque[-1]:
+        
+        for i in range(len(nums)):
+
+            while max_deque and nums[i] > max_deque[-1]:
                 max_deque.pop()
-            max_deque.append(nums[j])
+            max_deque.append(nums[i])
 
-            while min_deque and nums[j] < min_deque[-1]:
+            while min_deque and nums[i] < min_deque[-1]:
                 min_deque.pop()
-            min_deque.append(nums[j])
-
+            min_deque.append(nums[i])
 
             while max_deque and min_deque and max_deque[0] - min_deque[0] > limit:
 
-                if max_deque and max_deque[0] == nums[i]:
+                if nums[j] == max_deque[0]:
                     max_deque.popleft()
                 
-                if min_deque and min_deque[0] == nums[i]:
+                if nums[j] == min_deque[0]:
                     min_deque.popleft()
-
-                i += 1
-
-            max_len = max(max_len,j - i + 1)
+                
+                j += 1
+            
+            max_len = max(max_len,i - j + 1)
         
 
         return max_len
-                
-
-
-
 
