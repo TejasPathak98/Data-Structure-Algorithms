b9920af20be12bae7b5e9dc0449b6dca82f0ce9d
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-23 02:35:38 -0700

    Time: 87 ms (31.05%) | Memory: 20.8 MB (6.32%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -1,33 +1,32 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        #Dijsktra with Contraints
-        #Push Liberally and Prune later
-
         graph = defaultdict(list)
 
         for u,v,w in flights:
             graph[u].append((v,w))
         
+        min_heap = []
+        heappush(min_heap,(0,src,0))
+
         best = dict()
 
-        min_heap = [(0,src,0)]
-
         while min_heap:
-            current_cost , node , stops = heapq.heappop(min_heap)
+
+            cost , node , stops = heappop(min_heap)
 
             if stops > k + 1:
                 continue
             
             if node == dst:
-                return current_cost
+                return cost
             
-            if (node,stops) in best and best[(node,stops)] <= current_cost: # this is pruning step
+            if (node,stops) in best and best[(node,stops)] <= cost:
                 continue
             
-            best[(node,stops)] = current_cost 
+            best[(node,stops)] = cost
 
-            for neighbor,weight in graph[node]:
-                heapq.heappush(min_heap, (weight + current_cost,neighbor,stops + 1))
-            
+            for nei,w in graph[node]:
+                heappush(min_heap,(cost + w,nei,stops + 1))
+
         
         return -1
