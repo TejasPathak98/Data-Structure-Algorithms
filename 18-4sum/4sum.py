e436f9ddcda1235648237eeded02932444058a8c
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-23 01:11:04 -0700

    Time: 15 ms (98.58%) | Memory: 18 MB (39.77%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -1,60 +1,54 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        nums.sort()
         result = []
-        self.helper(nums,target,4,result,[])
+        nums.sort()
+        self.helper(nums,4,target,result,[])
         return result
 
-    def helper(self,nums,target,N,result,intermediate_results):
-        if N < 2 or len(nums) < N:
+    def helper(self,nums,N,target,result,intermediate_results):
+        if N <= 1 or len(nums) < N:
             return
-        
         elif N == 2:
             res = self.TwoSum(nums,target)
             if res != []:
-                for arr in res:
-                    result.append(intermediate_results + arr)
-        
+                for r in res:
+                    result.append(intermediate_results + r)
         else:
-
-            for i in range(len(nums) - N  + 1):
+            end = len(nums) - N  + 1
+            for i in range(end):
+                if i > 0 and nums[i] == nums[i - 1]:
+                    continue
                 if nums[i] * N > target or nums[-1] * N < target:
                     break
-                elif i == 0 or nums[i] != nums[i - 1]:
-                    self.helper(nums[i + 1:], target - nums[i], N - 1, result, [nums[i]] + intermediate_results)
-        
+                self.helper(nums[i + 1:],N - 1,target - nums[i],result,[nums[i]] + intermediate_results)
 
-    
     def TwoSum(self,nums,target):
+        result = []
+
         l = 0
         r = len(nums) - 1
-        result = []
 
         while l < r:
 
-            if nums[l] + nums[r] == target:
+            Sum = nums[l] + nums[r]
+
+            if Sum == target:
                 result.append([nums[l],nums[r]])
-            
+
                 while l < r and nums[l] == nums[l + 1]:
                     l += 1
                 
                 while l < r and nums[r] == nums[r - 1]:
                     r -= 1
-
+                
                 l += 1
                 r -= 1
-            
-            elif nums[l] + nums[r] < target:
-
+            elif Sum < target:
                 l += 1
             else:
-
                 r -= 1
         
         return result
-
-
-
-
+        
 
 
