88fa44bffd1f64b4f15076ec8a1327f37a5f7b6b
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-24 15:05:15 -0700

    Time: 163 ms (50.16%) | Memory: 31.8 MB (24.63%) - LeetSync

--- 472-concatenated-words/concatenated-words.py
+++ 472-concatenated-words/concatenated-words.py
@@ -9,23 +9,21 @@ class Solution:
                 x = word[:i + 1]
                 y = word[i + 1:]
 
-                if x in Wordset:
-                    if y in Wordset or DP_helper(y):
+                if x in words:
+                    if y in words or DP_helper(y):
                         return True
-
-                
             return False
 
-        Wordset = set(words)
+        words = set(words)
         result = []
 
-        for word in Wordset:
-            Wordset.discard(word)
+        for word in words:
+            words.discard(word)
 
             if DP_helper(word):
                 result.append(word)
             
-            Wordset.add(word)
+            words.add(word)
 
         
         return result
