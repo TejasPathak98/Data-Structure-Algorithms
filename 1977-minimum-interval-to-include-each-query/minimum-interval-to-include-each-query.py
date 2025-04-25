09eb291b3f672caea654ffd37386acfdefe61250
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-25 02:08:23 -0700

    Time: 253 ms (81.94%) | Memory: 60.7 MB (28.71%) - LeetSync

--- 1977-minimum-interval-to-include-each-query/minimum-interval-to-include-each-query.py
+++ 1977-minimum-interval-to-include-each-query/minimum-interval-to-include-each-query.py
@@ -1,18 +1,16 @@
 class Solution:
     def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
-        intervals.sort(key = lambda x : x[0]) #Sort with respect to begintime
-        #We sort the queries too as we can move forward "just-intime appraoch" and not backtrack
-
+        intervals.sort(key = lambda x : x[0])
         q = [(query,idx) for idx,query in enumerate(queries)]
-        q.sort(key = lambda x : x[0])
+        q.sort(key = lambda x: x[0])
 
-        i = 0 # this helps us to process the interals list only once
-        min_heap = [] # helps us to get the min-interval length of each query
+        i = 0
         result = [-1] * len(queries)
+        min_heap = []
 
         for query,idx in q:
             while i < len(intervals) and query >= intervals[i][0]:
-                heappush(min_heap, ((intervals[i][1] - intervals[i][0] + 1),intervals[i][1]))
+                heappush(min_heap,(intervals[i][1] - intervals[i][0] + 1,intervals[i][1]))
                 i += 1
             
             while min_heap and min_heap[0][1] < query:
@@ -23,11 +21,5 @@ class Solution:
             else:
                 result[idx] = -1
         
+
         return result
-
-        #O(NlogN) + O(QlogQ) ; O(N)
-
-        #Think of this a timeline problem + singleSweep + Heap
-
-
-
