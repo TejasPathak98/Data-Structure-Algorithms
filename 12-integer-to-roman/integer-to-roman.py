1b53b14914de67f113a33bca3b3ef6f6861a3fc6
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-25 13:59:54 -0700

    Time: 3 ms (79.22%) | Memory: 17.8 MB (70.91%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -1,58 +1,38 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        temp = num
-        var = []
+        
+        roman_values = [
+            (1000,"M"),
+            (900,"CM"),
+            (500,"D"),
+            (400,"CD"),
+            (100,"C"),
+            (90,"XC"),
+            (50,"L"),
+            (40,"XL"),
+            (10,"X"),
+            (9,"IX"),
+            (5,"V"),
+            (4,"IV"),
+            (3,"III"),
+            (2,"II"),
+            (1,"I")
+        ]
 
-        while temp:
-            x = temp % 10
-            temp = temp // 10
-            var.append(x)
-        
-        for i in range(len(var)):
-            var[i] = var[i] * (10 ** i)
-        
-        var = var[::-1]
         result = ""
 
-        for i in range(len(var)):
-            x = var[i]
-            if x >= 1000:
-                result = result + "M" * (x // 1000)
-            elif 100 <= x < 1000:
-                if x == 500:
-                    result = result + "D"
-                elif x == 400:
-                    result += "CD"
-                elif x == 900:
-                    result += "CM"
-                elif x < 400:
-                    result += (x // 100) * "C"
-                elif x > 500:
-                    result += "D" + ((x - 500) // 100) * "C"
-            elif 10 <= x < 100:
-                if x == 50:
-                    result = result + "L"
-                elif x == 40:
-                    result += "XL"
-                elif x == 90:
-                    result += "XC"
-                elif x < 40:
-                    result += (x // 10) * "X"
-                elif x > 50:
-                    result += "L" + ((x - 50) // 10) * "X"
-            elif 1 <= x < 10:
-                if x == 5:
-                    result = result + "V"
-                elif x == 4:
-                    result += "IV"
-                elif x == 9:
-                    result += "IX"
-                elif x < 4:
-                    result += (x) * "I"
-                elif x > 5:
-                    result += "V" + (x - 5) * "I"
+        for value,symbol in roman_values:
+            while num > 0:
+                if num >= value:
+                    result += symbol
+                    num -= value
+                else:
+                    break
+            else:
+                break
             
 
         return result
 
-        
+
+         
