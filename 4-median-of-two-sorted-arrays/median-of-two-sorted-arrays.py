407e3f89a1e47ad72974f27788cd63b1497e4bb2
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-26 19:41:22 -0700

    Time: 3 ms (53.00%) | Memory: 18.1 MB (40.84%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,33 +1,38 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        if len(nums1) > len(nums2):
-            nums1 , nums2 = nums2 , nums1
+        n = len(nums1)
+        m = len(nums2)
 
-        m = len(nums1)
-        n = len(nums2)
+        if n > m:
+            nums1 , nums2 = nums2 ,nums1
 
-        left = 0
-        right = m
-
-        while left <= right:
-            partitionX = (left + right) //2
-            partitionY = (m + n + 1) // 2 - partitionX
-
-            leftMaxX = float('-inf') if partitionX == 0 else nums1[partitionX - 1]
-            rightMinX = float('inf') if partitionX == m else nums1[partitionX]
-            leftMaxY = float('-inf') if partitionY == 0 else nums2[partitionY - 1]
-            rightMinY = float('inf') if partitionY == n else nums2[partitionY]
-
-            if leftMaxX <= rightMinY and leftMaxY <= rightMinX:
-                if (m + n) % 2 == 1:
-                    return max(leftMaxX,leftMaxY)
-                else:
-                    return (max(leftMaxX,leftMaxY) + min(rightMinX,rightMinY))/2.0
-            
-            elif leftMaxX > rightMinY:
-                right = partitionX - 1
-            else:
-                left = partitionX + 1
+        n = len(nums1)
+        m = len(nums2)
         
+        l = 0
+        r = len(nums1)
 
-        return -1.0
+        while l <= r:
+            partitionX = (l + r) // 2
+            partitionY = (n + m) // 2 - partitionX
+
+            maxLeftX = nums1[partitionX - 1] if partitionX >= 1 else float('-inf')
+            minRightX = nums1[partitionX] if partitionX < n else float('inf')
+            maxLeftY = nums2[partitionY - 1] if partitionY >= 1 else float('-inf')
+            minRightY = nums2[partitionY] if partitionY < m else float('inf')
+
+            if maxLeftX <= minRightY and maxLeftY <= minRightX:
+
+                if (n + m) % 2 == 1:
+                    return min(minRightX,minRightY)
+                else:
+                    return  (max(maxLeftX,maxLeftY) + min(minRightX,minRightY)) / 2.0
+            
+            elif maxLeftX > minRightY:
+                r = partitionX - 1
+            else:
+                l = partitionX + 1
+
+        
+        return -1
+        
