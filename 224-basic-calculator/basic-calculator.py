cc648e6893a8c03c49b7e6496e08766f6f324c6d
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-26 20:46:58 -0700

    Time: 47 ms (28.12%) | Memory: 18.3 MB (79.19%) - LeetSync

--- 224-basic-calculator/basic-calculator.py
+++ 224-basic-calculator/basic-calculator.py
@@ -1,44 +1,47 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        def evluate(stack,sign,num):
-            stack.append(sign* num)
+        def evaluate(stack,number,sign):
+            stack.append(number * sign)
         
-        #global trackers
-
-        i = 0
-        num = 0
-        sign = 1
         stack = []
         state = []
+        number = 0
+        sign = 1
+        i = 0
 
         while i < len(s):
             if s[i].isdigit():
-                num = num*10 + int(s[i])
-            elif s[i] == "(":
-                outer_sum = sum(stack)
-                stack = []
-                state.append((outer_sum,sign))
-                sign = 1
-                num = 0
-            elif s[i] == ")":
-                evluate(stack, sign, num)
-                inner_sum = sum(stack)
-                stack = []
-                outer_sum,outer_sign = state.pop()
-                total_sum = outer_sum + (outer_sign * inner_sum)
-                stack.append(total_sum)
-                sign = 1
-                num = 0
-            elif s[i] == "+":
-                evluate(stack, sign, num)
-                num = 0
-                sign = 1
-            elif s[i] == "-":
-                evluate(stack, sign, num)
-                num = 0
-                sign = -1
-
+                number = number * 10 + int(s[i])
+            else:
+                if s[i] == "(":
+                    evaluate(stack, number, sign)
+                    outer_sum = sum(stack)
+                    stack = []
+                    state.append((outer_sum,sign))
+                    number = 0
+                    sign = 1
+                elif s[i] == ")":
+                    evaluate(stack, number, sign)
+                    inner_sum = sum(stack)
+                    stack = []
+                    outer_sum,outer_sign = state.pop()
+                    stack.append(inner_sum*outer_sign + outer_sum)
+                    number = 0
+                    sign = 1
+                elif s[i] == "+":
+                    evaluate(stack, number, sign)
+                    sign = 1
+                    number = 0
+                elif s[i] == "-":
+                    evaluate(stack, number, sign)
+                    sign = -1
+                    number = 0
             i += 1
-
-        evluate(stack, sign, num)
+        
+        evaluate(stack, number, sign)
         return sum(stack)
+
+                    
+
+
+        
