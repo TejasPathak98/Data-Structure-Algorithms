602d97d2165efcdc2a1e166e3485d963c715e47b
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-27 10:50:15 -0700

    Time: 20 ms (26.38%) | Memory: 22.7 MB (12.92%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,26 +1,17 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # ans = []
+        ans = []
+        my_dict = defaultdict(list)
 
-        # str_dict = defaultdict(list)
-
-        # for st in strs:
-        #     str_dict[tuple(sorted(st))].append(st)
-
-        # for v in str_dict.values():
-        #     ans.append(v)
-        # return ans
-
-        str_dict = defaultdict(list)
-
-        for st in strs:
-
-            ch_count = [0] * 26
-
-            for ch in st:
-                ch_count[ord(ch) - ord('a')] += 1
-
-            str_dict[tuple(ch_count)].append(st)
+        def process(s):
+            arr = [0] * 26
+            for ch in s:
+                arr[ord(ch) - 97] += 1
+            return arr
+        
+        for s in strs:
+            my_dict[tuple(process(s))].append(s)
+        
+        return list(my_dict.values())
 
 
-        return list(str_dict.values())
