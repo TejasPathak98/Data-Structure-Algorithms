8d2fdcf8a0cab1cb16b356781322946f7dad31fd
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-26 19:55:38 -0700

    Time: 11 ms (63.30%) | Memory: 20.3 MB (36.16%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -5,32 +5,25 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        #we take the heads of all linked lists and put them into a heap based on their vals and get the first one out
-        #then we use that to start the final list and we add its next pointer also to the heap
-        # in the same way, we continue
-
         if not lists or len(lists) == 0:
             return None
         
+        dummy = ListNode(-1)
+        curr = dummy
+
         min_heap = []
+        var = 1
 
-        for idx,node in enumerate(lists):
-            if node:
-                heapq.heappush(min_heap, (node.val,idx,node))
+        for idx,l in enumerate(lists):
+            if l:
+                heapq.heappush(min_heap, (l.val,idx,l))
         
-        dummy = curr = ListNode(-1)
-
         while min_heap:
-            _,idx,node = heapq.heappop(min_heap)
+            _,idx,node = heappop(min_heap)
             if node:
                 curr.next = node
                 curr = curr.next
                 if node.next:
-                    heapq.heappush(min_heap, (node.next.val,idx,node.next))
-        
+                    heappush(min_heap, (node.next.val,idx,node.next))
 
         return dummy.next
-
-
-
-
