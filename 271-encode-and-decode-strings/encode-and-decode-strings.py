34bae879ac9a4009b6128ddae60f73526250a8e2
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-27 11:07:33 -0700

    Time: 240 ms (5.20%) | Memory: 18.4 MB (24.75%) - LeetSync

--- 271-encode-and-decode-strings/encode-and-decode-strings.py
+++ 271-encode-and-decode-strings/encode-and-decode-strings.py
@@ -2,30 +2,34 @@ class Codec:
     def encode(self, strs: List[str]) -> str:
         """Encodes a list of strings to a single string.
         """
-        string_to_send = ""
-        for string in strs:
-            temp_string =""
-            for ch in string:
-                c = str(ord(ch))
-                temp_string += c + "*"
-            string_to_send += temp_string + "-"        
-        return string_to_send
+        final_str = ""
+
+        for s in strs:
+            temp = ""
+            for ch in s:
+                temp += str(ord(ch)) + "#"
+            final_str += temp + "*"
+        return final_str
 
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
         """
-        ans = []
-        strings = s.split("-")[:-1]
-        for string in strings:
-            temp_strings = string.split("*")[:-1]
-            ts = ""
-            for st in temp_strings:
-                ch = chr(int(st))
-                ts += ch
-            ans.append(ts)
-        
-        return ans
 
+        final_list = []
+        string_split = s.split("*")
+        string_split = string_split[:-1]
+        print(string_split)
+
+        for st in string_split:
+            s_split = st.split("#")
+            s_split = s_split[:-1]
+            temp = ""
+            for ch in s_split:
+                temp += chr(int(ch))
+            final_list.append(temp)
+        
+        return final_list
+            
 
 
         
