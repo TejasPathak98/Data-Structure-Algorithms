6ea1095ac842483028de03b2fc5ad1aee00f0eb2
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-28 16:51:32 -0700

    Time: 82 ms (14.75%) | Memory: 20.9 MB (5.02%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.py
+++ 876-hand-of-straights/hand-of-straights.py
@@ -1,49 +1,12 @@
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
-        # if len(hand) % groupSize != 0:
-        #     return False
-        
-        # n = len(hand)
-        
-        # heapify(hand)
-        # count = 0
-        # temp = []
-        # temp_store = []
-        # ans = []
-
-        # while hand:
-        #     x = heapq.heappop(hand)
-        #     if len(temp) >= 1:
-        #         if temp[-1] == x:
-        #             temp_store.append(x)
-        #             continue
-        #         elif temp[-1] != x - 1:
-        #             return False
-        #         elif temp[-1] == x - 1:
-        #             temp.append(x)
-        #             count += 1
-        #     else:
-        #         temp.append(x)
-        #         count += 1
-            
-        #     if count == groupSize:
-        #         ans.extend(temp[:])
-        #         temp.clear()
-        #         count = 0
-        #         for y in temp_store:
-        #             heapq.heappush(hand, y)
-        #         temp_store.clear()
-
-        # return len(ans) == n
-
-        # #O(NlogN) ; O(N)
-
-
-        if len(hand) % groupSize != 0:
+        n = len(hand)
+        if n % groupSize != 0:
             return False
         
         count = Counter(hand)
-        keys = sorted(count)
+        keys = sorted(count) #when we sort count(a counter) we just get the keys
+        count_ = sorted(count.items(),key = lambda x : (x[1],x[0]))
 
         for key in keys:
             while count[key] > 0:
@@ -51,5 +14,5 @@ class Solution:
                     if count[i + key] <= 0:
                         return False
                     count[i + key] -= 1
-            
+
         return True
