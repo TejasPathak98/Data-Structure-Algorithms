013a1596f4bfd2acda1ebae57b1f1285b0c2db49
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-28 14:04:50 -0700

    Time: 15 ms (95.55%) | Memory: 19.2 MB (56.98%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -2,14 +2,12 @@ class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         counter = Counter(tasks)
         max_freq = max(counter.values())
+        max_freq_count = 0 
 
-        max_freq_count = 0
-
-        for key,value in counter.items():
-            if value == max_freq:
+        for _,freq in counter.items():
+            if freq == max_freq:
                 max_freq_count += 1
+        
 
-        return max(len(tasks),(max_freq - 1)*(n + 1) + max_freq_count)
-
-
+        return max(len(tasks) ,(max_freq  - 1) * (n + 1) + max_freq_count)
 
