cf643e1faf1d451f7746adcb28f732d197195bed
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-28 16:18:23 -0700

    Time: 3046 ms (88.60%) | Memory: 17.7 MB (87.09%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -3,29 +3,31 @@ class Solution:
         m = len(board)
         n = len(board[0])
 
-        def dfs(i,j,idx):
-            if idx == len(word) - 1:
+        def dfs(x,y,pos):
+            if pos == len(word) - 1:
                 return True
-            
-            temp = board[i][j]
 
-            board[i][j] = "#"
+            letter = board[x][y]            
+            board[x][y] = "#"
 
             for dx,dy in [(0,1),(0,-1),(1,0),(-1,0)]:
-                x = i + dx
-                y = j + dy
+                nx = x + dx
+                ny = y + dy
 
-                if 0 <= x < m and 0 <= y < n and board[x][y] == word[idx + 1]:
-                    if dfs(x,y,idx + 1):
+                if 0 <= nx < m and 0 <= ny < n and board[nx][ny] == word[pos + 1]:
+                    if dfs(nx,ny,pos + 1):
                         return True
-            board[i][j] = temp
-            return False 
+
+
+            board[x][y] = letter
+            return False
+
 
         for i in range(m):
             for j in range(n):
                 if board[i][j] == word[0]:
-                    #visited = [[False] * n for _ in range(m)]
                     if dfs(i,j,0):
                         return True
+
         
         return False
