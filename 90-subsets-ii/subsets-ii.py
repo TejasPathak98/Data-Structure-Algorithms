351d82a79aafe3f0b69d1b0cee47a4073e64e122
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-28 16:08:09 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (57.01%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -1,52 +1,25 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        # ans = []
-        # n = len(nums)
-
-        # def helper(temp,i):
-        #     if i > len(nums):
-        #         return
-            
-        #     if i == len(nums):
-        #         ans.append(temp[:])
-        #         return
-
-        #     temp.append(nums[i])
-
-        #     helper(temp,i + 1)
-
-        #     temp.pop()
-
-        #     helper(temp,i + 1)
-
-        
-        # helper([],0)
-        # return ans
-
-        ans = []
-        n = len(nums)
         nums.sort()
+        n = len(nums)
 
-        def helper(temp,i):
-            # if i > len(nums):
-            #     return
+        result = []
+
+        def backtracking(temp,index):
+
+            result.append(temp[:])
+
             
-            # if i == len(nums):
-            #     ans.append(temp[:])
-            #     return
-
-            #we do not need explicit conditions because the loop handles it, the function naturally returns after the loop is over
-
-            ans.append(temp[:])
-
-            for j in range(i,len(nums)):
-                if j > i and nums[j] == nums[j - 1]:
+            for j in range(index,len(nums)):
+                if j > index and nums[j] == nums[j - 1]:
                     continue
                 temp.append(nums[j])
-                helper(temp,j + 1)
-                temp.pop()  #.....again here we do not need another call like helper(temp,j + 1) because the loop handles it implicitly
+                backtracking(temp, j + 1)
+                temp.pop()
 
         
-        helper([],0)
-        return ans
+        backtracking([], 0)
+        return result
+
+            
 
