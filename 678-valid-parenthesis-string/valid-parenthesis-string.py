b2df37bd7ef04d58f3ba74e5f99be018dcb251f8
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-29 10:33:26 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (84.07%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.py
+++ 678-valid-parenthesis-string/valid-parenthesis-string.py
@@ -1,20 +1,18 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        stack = []
+        bracket_stack = []
         star_stack = []
-        count =0 
 
         i = 0
 
         while i < len(s):
             if s[i] == "(":
-                stack.append(i)
+                bracket_stack.append(i)
             elif s[i] == ")":
-                if stack:
-                    stack.pop()
+                if bracket_stack:
+                    bracket_stack.pop()
                 else:
                     if star_stack:
-                        count += 1
                         star_stack.pop()
                     else:
                         return False
@@ -22,24 +20,19 @@ class Solution:
                 star_stack.append(i)
             i += 1
         
-        if len(stack) == 0:
+
+        if not bracket_stack:
             return True
         else:
-            if len(star_stack) >= len(stack):
-                j = 0
-                c = 0
-                k = 0
-
-                while star_stack and stack and k < len(star_stack) and j < len(stack):
-                    if star_stack[k] > stack[j]:
-                        j += 1
-                        c += 1
-                    k += 1
-                
-                if c == len(stack):
-                    return True
+            while bracket_stack:
+                idx = bracket_stack.pop()
+                if star_stack:
+                    if star_stack[-1] > idx:
+                        star_stack.pop()
+                    else:
+                        return False
                 else:
                     return False
-            else:
-                return False
-        
+
+
+        return True 
