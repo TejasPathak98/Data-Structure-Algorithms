68b193aefc2df2dc7dd6de667a3edebd3a2525aa
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-29 10:25:43 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (15.79%) - LeetSync

--- 768-partition-labels/partition-labels.py
+++ 768-partition-labels/partition-labels.py
@@ -1,22 +1,25 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
         L = len(s)
-        s_dict = {s[i] : i for i in range(L)}
+        last_occ_map = {s[i] : i for i in range(len(s))} #last occ map
 
         ans = []
         i = 0
 
         while i < L:
             j = i
-            end = s_dict[s[j]]
+
+            end = last_occ_map[s[j]]
 
             while j < end:
-                if s_dict[s[j]] > end:
-                    end = s_dict[s[j]]
+                if last_occ_map[s[j]] > end:
+                    end = last_occ_map[s[j]]
                 j += 1
             
             ans.append(end - i + 1)
             i = end + 1
-        
 
-        return ans
+        return ans 
+            
+
+
