931988cf56f595603a1d7df66e1195b0ac0bb76d
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-04-30 13:42:05 -0700

    Time: 172 ms (68.07%) | Memory: 17.9 MB (81.05%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,10 +1,15 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        dp = [0] * (amount + 1)
-        dp[0] = 1 #only 1 way to make 0 amount
+        if amount == 0:
+            return 1
 
-        for coin in coins: # iterate over the number of coins and set the record straight about each coins contribution to each amount
-            for j in range(coin,amount + 1): # iterate over the amounts for each coin
-                dp[j] = dp[j] + dp[j - coin] #this is basically, the current dp[j](current no of ways) + (no of ways that we would get after taking into acc the coins contribution)
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+
+        for coin in coins:
+            for j in range(1,amount + 1):
+                if j >= coin:
+                    dp[j] += dp[j - coin]
+        
 
         return dp[amount]
