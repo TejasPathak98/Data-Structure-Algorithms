75fc24c0461c523ba4eb3401aeacd42db9fb7d5f
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-05-01 19:09:59 -0700

    Time: 370 ms (87.64%) | Memory: 19.8 MB (35.64%) - LeetSync

--- 744-network-delay-time/network-delay-time.py
+++ 744-network-delay-time/network-delay-time.py
@@ -1,34 +1,34 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        
+        best_time = {i : float('inf') for i in range(1,n + 1)}
+        best_time[k] = 0
+
         graph = defaultdict(list)
 
         for u,v,w in times:
             graph[u].append((v,w))
+
+
+
+        heap = []
+        heappush(heap, (0,k))
+
+        while heap:
+            cost,x = heappop(heap)
+
+            if best_time[x] < cost:
+                continue
         
-        time_dict = [float('inf')] * (n + 1)
-        time_dict[k] = 0
-
-        min_heap = []
-        heappush(min_heap,(0,k))
-
-        while min_heap:
-            cost,node = heappop(min_heap)
-
-            # if time_dict[node] < cost:
-            #     continue
-            
-            for nei,w in graph[node]:
-                new_cost = cost + w
-                if new_cost < time_dict[nei]:
-                    time_dict[nei] = new_cost
-                    heappush(min_heap,(new_cost,nei))
+            for nei, w in graph[x]:
+                new_cost = w + cost
+                if new_cost < best_time[nei]:
+                    best_time[nei] = new_cost
+                    heappush(heap, (new_cost,nei))
         
 
         
-        if any(x == float('inf') for x in time_dict[1:]):
-            return -1
-        else:
-            return max(time_dict[1:])
+        return max(best_time.values()) if max(best_time.values()) != float('inf') else -1
 
 
 
