4b6a129ae4c8af8381935bc9b45e60fef989b413
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-05-02 22:18:29 -0700

    Time: 65 ms (96.46%) | Memory: 31.8 MB (93.08%) - LeetSync

--- 2289-minimum-operations-to-make-the-array-alternating/minimum-operations-to-make-the-array-alternating.py
+++ 2289-minimum-operations-to-make-the-array-alternating/minimum-operations-to-make-the-array-alternating.py
@@ -2,13 +2,13 @@ class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
         if len(nums) == 1:
             return 0
-        
+
         even_numbers = Counter(nums[::2]).most_common(2) + [(0,0)]
         odd_numbers = Counter(nums[1::2]).most_common(2) + [(0,0)]
 
         if even_numbers[0][0] != odd_numbers[0][0]:
-            nochange = even_numbers[0][1] + odd_numbers[0][1]
+            no_change = even_numbers[0][1] + odd_numbers[0][1]
         else:
-            nochange = max(even_numbers[0][1] + odd_numbers[1][1],even_numbers[1][1] + odd_numbers[0][1]) 
-
-        return len(nums) - nochange
+            no_change = max(even_numbers[0][1] + odd_numbers[1][1],odd_numbers[0][1] + even_numbers[1][1])
+        
+        return len(nums) - no_change
