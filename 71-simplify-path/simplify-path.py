47caba9ea7691a6898725b3632ba75b2d5536e5b
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-05-03 19:14:00 -0700

    Time: 3 ms (35.89%) | Memory: 17.8 MB (76.61%) - LeetSync

--- 71-simplify-path/simplify-path.py
+++ 71-simplify-path/simplify-path.py
@@ -3,18 +3,17 @@ class Solution:
         l = path.split("/")
         l = [x for x in l if x != '.' and x != '']
 
-
         stack = []
         i = 0
 
         while i < len(l):
+
             if l[i] == "..":
                 if stack:
                     stack.pop()
             else:
                 stack.append(l[i])
             i += 1
-        
+
         return '/' + '/'.join(stack)
 
-        
