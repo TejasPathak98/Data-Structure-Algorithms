d331cd91b70b2b97348c4baf7bcd8327b1b4554d
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-05-03 19:48:39 -0700

    Time: 19 ms (76.76%) | Memory: 18.6 MB (77.11%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.py
+++ 402-remove-k-digits/remove-k-digits.py
@@ -1,6 +1,6 @@
 class Solution:
     def removeKdigits(self, num: str, k: int) -> str:
-        
+        #Monotonic Increasing Stack
         stack = []
 
         for digit in num:
@@ -9,14 +9,14 @@ class Solution:
                 k -= 1
             stack.append(digit)
 
-        while k and stack:
+        
+        while stack and k:
             stack.pop()
             k -= 1
-
-        print(stack)
-
-        if not stack or all(x == '0' for x in stack):
-            print("br")
-            return "0"
         
+        if not stack or all(x == "0" for x in stack):
+            return "0"
+
         return "".join(stack).lstrip("0")
+
+
