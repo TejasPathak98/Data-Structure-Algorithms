d77b05b2f77a1a8181ccea6798d8b6261e6c14a9
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-05-04 20:29:36 -0700

    Time: 0 ms (100.00%) | Memory: 18.1 MB (25.21%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.py
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.py
@@ -9,15 +9,19 @@ class Solution:
         """
         Do not return anything, modify root in-place instead.
         """
+        
         curr = root
+
         while curr:
+
             if curr.left:
                 prev = curr.left
                 while prev.right:
                     prev = prev.right
+                
                 prev.right = curr.right
                 curr.right = curr.left
                 curr.left = None
             curr = curr.right
+
         return root
-        
