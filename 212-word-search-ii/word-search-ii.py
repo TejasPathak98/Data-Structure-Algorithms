729ee12cb2d842bd49f82f0236c91d83ebca2b8d
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-05-04 18:28:38 -0700

    Time: 410 ms (95.04%) | Memory: 18.6 MB (89.21%) - LeetSync

--- 212-word-search-ii/word-search-ii.py
+++ 212-word-search-ii/word-search-ii.py
@@ -1,56 +1,56 @@
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        def build_trie(words): #O(WL) ; O(WL)
-            Trie = {}
-
+        def build_Trie():
+            root = {}
+            node = root
             for word in words:
-                curr = Trie
+                node = root
                 for ch in word:
-                    if ch not in curr:
-                        curr[ch] = {}
-                    curr = curr[ch]
-                curr['#'] = word
-        
-            return Trie
+                    if ch not in node:
+                        node = node.setdefault(ch,{})
+                    else:
+                        node = node[ch]
+                node['#'] = word
+            return root
 
-        
-        def dfs(x,y,Trie):
+        def dfs(x,y,trie):
             letter = board[x][y]
-            curr = Trie[letter]
-
-            word = curr.pop('#',False)
+            node = trie[letter]
+            word = node.pop('#',False)
             if word:
                 result.append(word)
-            
-            temp = board[x][y]
 
+            temp = board[x][y]
             board[x][y] = "*"
 
-            for dx,dy in [(0,1),(0,-1),(1,0),(-1,0)]:
+            for dx,dy in [(0,1),(1,0),(0,-1),(-1,0)]:
                 nx = x + dx
                 ny = y + dy
 
-                if 0 <= nx < m and 0 <= ny < n and board[nx][ny] in curr:
-                    dfs(nx,ny,curr)
+                if 0 <= nx < m and 0 <= ny < n and board[nx][ny] in node:
+                    dfs(nx,ny,node)
 
-            
             board[x][y] = temp
+            if not node:
+                trie.pop(letter)
 
-            if not curr:
-                Trie.pop(letter)
-
+        
         m = len(board)
         n = len(board[0])
-        Trie = build_trie(words)
+        trie = build_Trie()
         result = []
 
         for i in range(m):
             for j in range(n):
-                if board[i][j] in Trie:
-                    dfs(i,j,Trie)
-        
+                if board[i][j] in trie:
+                    dfs(i,j,trie)
 
         return result
 
 
-        #(4^L * MN)  + O(WL); O(L) + O(WL)
+
+
+
+
+
+
