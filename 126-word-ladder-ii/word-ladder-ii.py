cf1ca14f00e4eb501edb6607491365d3f970942d
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-05-04 19:02:25 -0700

    Time: 39 ms (33.13%) | Memory: 18.3 MB (64.61%) - LeetSync

--- 126-word-ladder-ii/word-ladder-ii.py
+++ 126-word-ladder-ii/word-ladder-ii.py
@@ -1,9 +1,10 @@
 class Solution:
     def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
-        depthMap = {}
         ans = []
+        depthMap = {}
 
-        def dfs(word,seq):
+        def dfs(seq,word):
+            print(seq)
             if word == beginWord:
                 ans.append(seq[::-1][:])
                 return
@@ -11,54 +12,44 @@ class Solution:
             steps = depthMap[word]
 
             for i in range(len(word)):
-                original_char = word[i]
-                for ch in "abcdefghijklmnopqrstuvwxyz":
-                    word = word[:i] + ch + word[i + 1:]
-
-                    if word in depthMap and depthMap[word] + 1 == steps:
-                        seq.append(word)
-                        dfs(word,seq)
+                for ch in 'abcdefghijklmnopqrstuvwxyz':
+                    new_word = word[:i] + ch + word[i + 1:]
+                    if new_word in depthMap and depthMap[new_word] + 1 == steps:
+                        seq.append(new_word)
+                        dfs(seq,new_word)
                         seq.pop()
-                        
-                word = word[:i] + original_char + word[i + 1:]
 
-        queue = deque([beginWord])
-        depthMap = {}
+        wordList = set(wordList)
         depthMap[beginWord] = 1
-        WordSet = set(wordList)
-        WordSet.discard(beginWord)
-
+        #wordList.discard(beginWord)
+        queue = deque([beginWord])
+        visited = set()
+        visited.add(beginWord)
 
         while queue:
 
             word = queue.popleft()
-            depth = depthMap[word]
+
+            steps = depthMap[word]
 
             if word == endWord:
                 break
             
-
             for i in range(len(word)):
-                original_char = word[i]
-                for ch in "abcdefghijklmnopqrstuvwxyz":
-                    word = word[:i] + ch + word[i + 1:]
+                for ch in 'abcdefghijklmnopqrstuvwxyz':
+                    new_word = word[:i] + ch + word[i + 1:]
+                    if new_word in wordList and new_word not in visited:
+                        queue.append(new_word)
+                        depthMap[new_word] = steps + 1
+                        visited.add(new_word)
 
-                    if word in WordSet:
-                        depthMap[word] = 1 + depth
-                        WordSet.discard(word)
-                        queue.append(word)
-
-                word = word[:i] + original_char + word[i + 1:]
-
-
+        
+        
         if endWord in depthMap:
-            dfs(endWord,[endWord])
+            dfs([endWord],endWord)
         
         return ans
-
-            
-
-            
+                    
 
 
 
