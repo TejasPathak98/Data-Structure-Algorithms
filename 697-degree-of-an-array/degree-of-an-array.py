e2d1aaf8c82dfc6c58312cf756f5897e50820038
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-05-10 16:34:39 -0700

    Time: 29 ms (36.47%) | Memory: 19.3 MB (82.54%) - LeetSync

--- 697-degree-of-an-array/degree-of-an-array.py
+++ 697-degree-of-an-array/degree-of-an-array.py
@@ -1,42 +1,18 @@
 class Solution:
     def findShortestSubArray(self, nums: List[int]) -> int:
-        counter = Counter(nums)
-        max_freq = max(counter.values())
+        count = Counter()
+        first = {}
+        last = {}
 
-        elements = []
+        for i in range(len(nums)):
+            count[nums[i]] += 1
 
-        for key,value in counter.items():
-            if value == max_freq:
-                elements.append(key)
+            if nums[i] not in first:
+                first[nums[i]] = i
+            
+            last[nums[i]] = i
         
-        min_length = float('inf')
+        max_freq = max(count.values())
 
-        for x in elements:
-            right = 0
-            left = 0
-            n = len(nums)
-            first_occ = False
-            count = 0
-
-            while right < n:
-
-                if nums[right] == x:
-                    if not first_occ:
-                        left = right
-                        first_occ = True
-                    count += 1
-                    if count == max_freq:
-                        min_length = min(min_length,right - left + 1)
-                        break
-
-                right += 1
-
-        
-        return min_length
-                    
-
-                
-
-
-
+        return min((last[x] - first[x] + 1) for x,fre in count.items() if fre == max_freq)
 
