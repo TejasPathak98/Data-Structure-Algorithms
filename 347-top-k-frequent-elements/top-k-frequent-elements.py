9fb0c213bd176bb245a1c526243f5626540ae880
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-05-11 22:11:44 -0700

    Time: 0 ms (100.00%) | Memory: 21.3 MB (61.96%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,5 +1,4 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         counter = Counter(nums)
-
-        return [number for number,_ in counter.most_common(k)]
+        return [x for x ,y in counter.most_common(k)]
