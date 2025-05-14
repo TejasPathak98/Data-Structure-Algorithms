f03d0b2e5827a2b63341a275ccce335bea5753f7
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-05-13 19:44:35 -0700

    Time: 19 ms (56.36%) | Memory: 17.9 MB (52.95%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -3,25 +3,31 @@ class Solution:
         l = 0
         r = 0
 
-        char_dict = defaultdict(int)
-        max_len = 0
+        if s == "":
+            return 0
+
+        max_len = float('-inf')
+        pos_dict = {}
 
         while r < len(s):
-            if s[r] not in char_dict:
-                char_dict[s[r]] = r
-                max_len = max(max_len,r - l + 1)
+            if s[r] not in pos_dict:
+                pos_dict[s[r]] = r
+                max_len = max(max_len,(r - l + 1))
                 r += 1
             else:
-                if char_dict[s[r]] >= l:
-                    prev_pos = char_dict[s[r]]
-                    char_dict[s[r]] = r
-                    l = prev_pos + 1
+                if pos_dict[s[r]] >= l:
+                    earlier_pos = pos_dict[s[r]]
+                    l = earlier_pos + 1
+                    pos_dict[s[r]] = r
+                    max_len = max(max_len,(r - l + 1))
                     r += 1
                 else:
-                    char_dict[s[r]] = r
-                    max_len = max(max_len,r - l + 1)
+                    pos_dict[s[r]] = r
+                    max_len = max(max_len,(r - l + 1))
                     r += 1
+
         
         return max_len
 
-                    
+
+
