a2d3788e71c03c8b514bcce3f482d24bf99123a7
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-05-15 00:50:36 -0700

    Time: 15 ms (84.40%) | Memory: 17.7 MB (67.97%) - LeetSync

--- 756-pour-water/pour-water.py
+++ 756-pour-water/pour-water.py
@@ -1,42 +1,38 @@
 class Solution:
     def pourWater(self, heights: List[int], volume: int, k: int) -> List[int]:
-        n = len(heights)
-
+        
         while volume:
-            #initial state
 
-            fill = k
             curr_height = heights[k]
+            fill_index = k
 
-            #go left
             l = k - 1
 
             while l >= 0:
                 if heights[l] > curr_height:
                     break
-                if heights[l] < curr_height:
+                if heights[l] < curr_height: #if we encounter a lower height, we stop there and then change the curr_height, this to ensure that the index gets filled first ahead of others who have same height(which are filled later)
                     curr_height = heights[l]
-                    fill = l
+                    fill_index = l
                 l -= 1
 
-            if fill == k:
 
+            if fill_index == k:
                 r = k + 1
-                while r < n:
+
+                while r < len(heights):
                     if heights[r] > curr_height:
                         break
                     if heights[r] < curr_height:
                         curr_height = heights[r]
-                        fill = r
+                        fill_index = r
                     r += 1
 
-            heights[fill] += 1
+            heights[fill_index] += 1
             volume -= 1
-
-            print(heights,volume)
 
         
         return heights
-
             
 
+                
