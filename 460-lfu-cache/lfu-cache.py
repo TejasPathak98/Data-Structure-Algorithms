4a00d032e64c620de4d9386e67aa3456ad71f5f3
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-05-14 23:59:28 -0700

    Time: 197 ms (25.44%) | Memory: 80.1 MB (42.25%) - LeetSync

--- 460-lfu-cache/lfu-cache.py
+++ 460-lfu-cache/lfu-cache.py
@@ -1,5 +1,3 @@
-from collections import OrderedDict
-
 class Node:
     def __init__(self,key,value):
         self.key = key
@@ -11,26 +9,24 @@ class LFUCache:
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.cache = {} #key -> Node
-        self.freq_map = defaultdict(OrderedDict) # freq -> (key -> Node) 
-        self.min_freq = 0
+        self.freq_map = defaultdict(OrderedDict) # freq -> (key -> Node)
+        self.min_freq = 1
 
+    
     def _update_freq_bucket(self,node):
         freq = node.freq
         key = node.key
 
         del self.freq_map[freq][key]
-        node.freq += 1
 
-        self.freq_map[freq + 1][key] = node
-
-        if not self.freq_map[freq]:
+        if len(self.freq_map[freq]) == 0:
             del self.freq_map[freq]
 
-            if self.min_freq == freq:
-                #self.min_freq = min(list(self.freq_map.keys()),default=1)
-                self.min_freq = freq + 1
+        node.freq += 1
+        self.freq_map[freq + 1][key] = node
 
-        
+        if self.min_freq == freq:
+            self.min_freq = min(self.freq_map.keys())
 
     def get(self, key: int) -> int:
         if key not in self.cache:
@@ -40,31 +36,27 @@ class LFUCache:
             self._update_freq_bucket(node)
             return node.value
         
+
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             node = self.cache[key]
             node.value = value
             self._update_freq_bucket(node)
         else:
-            if self.capacity == len(self.cache):
-                self.evict()
-            new_node = Node(key,value)
-            self.cache[key] = new_node
-            self.freq_map[1][key] = new_node
+            if len(self.cache) == self.capacity:
+                self._evict_cache()
+            node = Node(key,value)
+            self.freq_map[1][key] = node
             self.min_freq = 1
+            self.cache[key] = node
+    
 
+    def _evict_cache(self):
+        key, node = self.freq_map[self.min_freq].popitem(last=False)
+        del self.cache[key]
 
-    def evict(self):
-        if self.freq_map[self.min_freq]:
-            key, node = self.freq_map[self.min_freq].popitem(last=False)
-            del self.cache[key]
-
-        if not self.freq_map[self.min_freq]:
+        if len(self.freq_map[self.min_freq]) == 0:
             del self.freq_map[self.min_freq]
-        
-
-
-
 
 
         
