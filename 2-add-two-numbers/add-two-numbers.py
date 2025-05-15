4db7022d20ded140df4586d80571113733845630
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-05-15 00:57:47 -0700

    Time: 3 ms (72.83%) | Memory: 17.8 MB (56.66%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -5,37 +5,41 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        l3 = ListNode(-1)
+        dummy = l3
 
-        dummy = ListNode(-1)
-        l3 = dummy
-
-        Sum = 0
-        carry = 0
+        carry = 0 
 
         while l1 and l2:
             Sum = l1.val + l2.val + carry
-            l3.next = ListNode(Sum  % 10)
             carry = Sum // 10
+            l3.next = ListNode(Sum % 10)
             l3 = l3.next
             l1 = l1.next
             l2 = l2.next
-        
-        while l1:
-            Sum = l1.val + carry
-            l3.next = ListNode(Sum  % 10)
-            carry = Sum // 10
-            l3 = l3.next
-            l1 = l1.next
-        
-        while l2:
-            Sum = l2.val + carry
-            l3.next = ListNode(Sum  % 10)
-            carry = Sum // 10
-            l3 = l3.next
-            l2 = l2.next
+
+        if l1:
+            while l1:
+                Sum = l1.val + carry
+                carry = Sum // 10
+                l3.next = ListNode(Sum % 10)
+                l3 = l3.next
+                l1 = l1.next
+            
+        if l2:
+            while l2:
+                Sum = l2.val + carry
+                carry = Sum // 10
+                l3.next = ListNode(Sum % 10)
+                l3 = l3.next
+                l2 = l2.next
         
         if carry:
             l3.next = ListNode(carry)
 
         return dummy.next
+        
+
+
+
         
