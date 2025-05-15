f1238bf03def21e578acb1e97bad5a207505ccef
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-05-15 16:08:24 -0700

    Time: 3 ms (82.23%) | Memory: 17.6 MB (99.28%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -3,29 +3,28 @@ class Solution:
         m = len(grid)
         n = len(grid[0])
 
-        rotten = []
+        queue = deque()
 
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 2:
-                    rotten.append((i,j))
-        
-        queue = deque(rotten)
-        time = -1
+                    queue.append((i,j))
+
+        time = - 1
 
         while queue:
 
             for _ in range(len(queue)):
 
                 x,y = queue.popleft()
-                
-                for dx,dy in [(0,1),(0,-1),(1,0),(-1,0)]:
-                    x_ = x + dx
-                    y_ = y + dy
 
-                    if 0 <= x_ < m and 0 <= y_ < n and grid[x_][y_] == 1:
-                        queue.append((x_,y_))
-                        grid[x_][y_] = 2
+                for dx,dy in [(0,1),(0,-1),(1,0),(-1,0)]:
+                    nx = x + dx
+                    ny = y + dy
+
+                    if 0 <= nx < m and 0 <= ny < n and grid[nx][ny] == 1:
+                        queue.append((nx,ny))
+                        grid[nx][ny] = 2
 
             time += 1
 
@@ -34,11 +33,12 @@ class Solution:
             for j in range(n):
                 if grid[i][j] == 1:
                     return -1
-
-        all_zero = all(val == 0 for row in grid for val in row)
+        
+        all_zero = all(grid[i][j] == 0 for i in range(m) for j in range(n))
         if all_zero:
             return 0
-
+        
         return time
+
 
 
