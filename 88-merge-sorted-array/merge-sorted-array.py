44f3212bdf9dd0b283a81a5d5ee2264fba5520bd
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-05-16 00:24:30 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (43.68%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -3,42 +3,33 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        i = m + n - 1
-        j = m - 1
-        k = n - 1
+        
+        temp = []
 
-        if i == 0:
-            if n == 1:
-                nums1[0] = nums2[0]
-                return
+        for i in range(m):
+            temp.append(nums1[i])
+        
+        p1 = 0
+        p2 = 0
+        j = 0
+
+        while p1 < m and p2 < n:
+            if temp[p1] <= nums2[p2]:
+                nums1[j] = temp[p1]
+                j += 1
+                p1 += 1
             else:
-                return
+                nums1[j] = nums2[p2]
+                j += 1
+                p2 += 1
+        
+        while p1 < m:
+            nums1[j] = temp[p1]
+            j += 1
+            p1 += 1
+        
+        while p2 < n:
+            nums1[j] = nums2[p2]
+            j += 1
+            p2 += 1
 
-        while i >= 0 and j >= 0 and k >= 0:
-            if nums1[j] > nums2[k]:
-                nums1[i] = nums1[j]
-                j -= 1
-            else:
-                nums1[i] = nums2[k]
-                k -= 1
-            print(nums1)
-            i -= 1
-        
-        if j >= 0:
-            while j >= 0:
-                nums1[i] = nums1[j]
-                i -= 1
-                j -= 1
-                print(nums1)
-        
-        if k >= 0:
-            while k >= 0:
-                nums1[i] = nums2[k]
-                i -= 1
-                k -= 1
-        
-        
-        
-
-        
-                
