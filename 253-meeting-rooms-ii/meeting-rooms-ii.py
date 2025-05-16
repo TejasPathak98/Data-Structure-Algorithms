53ee84367d3fc279208c62752e70c169b792556b
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-05-15 19:23:49 -0700

    Time: 3 ms (86.77%) | Memory: 19.8 MB (75.67%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -1,41 +1,16 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        # intervals.sort()
-        # rooms = 1
-        # heap = []
-        # heappush(heap,intervals[0][1])
-
-        # for i in range(1,len(intervals)):
-        #     if intervals[i][0] < heap[0]:
-        #         heappush(heap, intervals[i][1])
-        #     else:
-        #         heapreplace(heap,intervals[i][1])
-
-        #     rooms = max(rooms,len(heap))
         
-        # return rooms
+        intervals.sort()
+        heap = []
 
-
-        starts = sorted([intervals[i][0]] for i in range(len(intervals)))
-        ends = sorted([intervals[i][1]] for i in range(len(intervals)))
-        startptr = 0
-        endptr = 0
-        used = 0
-        max_rooms =0 
-
-        while startptr < len(intervals):
-            if starts[startptr] < ends[endptr]: #is the next earliest meeting starting before the earliest end meeting time
-                used += 1
-                startptr += 1 
+        for interval in intervals:
+            if not heap:
+                heappush(heap,interval[1])
             else:
-                endptr += 1
-                used -= 1
-            
-            max_rooms = max(max_rooms,used)
+                if interval[0] < heap[0]:
+                    heappush(heap,interval[1])
+                else:
+                    heapreplace(heap, interval[1])
 
-        return max_rooms
-
-
-
-
-
+        return len(heap)
