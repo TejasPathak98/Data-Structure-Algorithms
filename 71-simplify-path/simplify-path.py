453e4885e5f9f72fa3d3ab8a5886ffdfef4c1174
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-05-15 18:15:42 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (76.62%) - LeetSync

--- 71-simplify-path/simplify-path.py
+++ 71-simplify-path/simplify-path.py
@@ -1,17 +1,8 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
         var = path.split('/')
-
-        print(var)
-
         var = [x for x in var if x not in (".","")]
-
-        print(var)
-
         var = var[::-1]
-
-        print(var)
-
         stack = []
 
         while var:
