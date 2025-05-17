98b99b88090218c0cd971098275cc23072c8ef49
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-05-16 21:06:57 -0700

    Time: 8 ms (53.68%) | Memory: 18.5 MB (33.03%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.py
+++ 437-path-sum-iii/path-sum-iii.py
@@ -6,23 +6,24 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        prefix_sums = defaultdict(int)
-        prefix_sums[0] = 1
+        prefix_sum = defaultdict(int)
+        prefix_sum[0] = 1
 
-        def dfs(root,curr_sum):
-            if not root:
+        def dfs(node,curr_sum):
+            if not node:
                 return 0
             
-            curr_sum += root.val
-            path_count = prefix_sums[curr_sum - targetSum]
+            curr_sum += node.val
+            paths = prefix_sum[curr_sum - targetSum]
 
-            prefix_sums[curr_sum] += 1
+            prefix_sum[curr_sum] += 1
 
-            path_count += dfs(root.left, curr_sum)
-            path_count += dfs(root.right,curr_sum)
+            paths += dfs(node.left,curr_sum)
+            paths += dfs(node.right,curr_sum)
 
-            prefix_sums[curr_sum] -= 1
+            prefix_sum[curr_sum] -= 1
 
-            return path_count
+            return paths
+
         
         return dfs(root,0)
