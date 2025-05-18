79245b56c5727092ea783504bb677e51f61852eb
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-05-17 18:27:23 -0700

    Time: 79 ms (25.02%) | Memory: 18.1 MB (18.95%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -9,19 +9,17 @@ class Solution:
         counter1 = Counter(s1)
         counter2 = Counter()
 
-        for i in range(n2):
-
+        for i in range(len(s2)):
             counter2[s2[i]] += 1
 
             if i >= n1:
+
                 counter2[s2[i - n1]] -= 1
 
                 if counter2[s2[i - n1]] == 0:
                     del counter2[s2[i - n1]]
-                
             
             if counter1 == counter2:
                 return True
-    
-
+        
         return False
