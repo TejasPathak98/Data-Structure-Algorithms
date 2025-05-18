e65d5ddf9f9a668313d2b64bdd042cf8464027d4
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-05-18 16:19:11 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (69.36%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,41 +1,23 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        if s[0] == "0":return 0
-        if len(s) == 1:return 1
-        if len(s) == 2:
-            if 10 <= int(s) <= 26:
-                if int(s) in (10,20):return 1
-                else: return 2
-            elif int(s) % 10 == 0:
-                return 0
-            else:
-                return 1
+        n = len(s)
+
+        if n == 0:
+            return 1
         
-        dp = [0] * len(s)
+        dp = [0] * (n + 1)
         dp[0] = 1
-        if 10 <= int(s[:2]) <= 26:
-            if int(s[:2]) in (10,20):
-                dp[1] = 1
-            else:dp[1] = 2
-        else:
-            if int(s[:2]) % 10 == 0:return 0
-            else: dp[1] = 1
+        dp[1] = 1 if s[0] != "0" else 0
+
+        print(dp)
+
+        for i in range(2,n + 1):
+            if s[i - 1] != "0":
+                dp[i] += dp[i - 1]
+
+            number = int(s[i - 2:i])
+            if 10 <= number <= 26:
+                dp[i] += dp[i - 2]
             
         
-        for i in range(2,len(s)):
-            if s[i] != "0":
-                dp[i] += dp[i - 1]
-                if 10 <= int(s[i - 1:i + 1]) <= 26:
-                    dp[i] += dp[i - 2]
-            else:
-                if int(s[i - 1:i + 1]) in (10,20):
-                    dp[i] += dp[i - 2]
-                else:
-                    return 0
-        
-        return dp[len(s) - 1]
-                
-
-
-
-        
+        return dp[n]
