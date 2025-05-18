fc057c3fab69142bda606f1b2b02f5287d9eb0f1
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-05-17 22:48:16 -0700

    Time: 10 ms (72.96%) | Memory: 27.6 MB (51.84%) - LeetSync

--- 355-design-twitter/design-twitter.py
+++ 355-design-twitter/design-twitter.py
@@ -2,44 +2,40 @@ class Twitter:
 
     def __init__(self):
         self.followerMap = defaultdict(set)
-        self.TweetsMap = defaultdict(list)
+        self.TweetMap = defaultdict(list)
         self.timer = 1
-        
-    def postTweet(self, userId: int, tweetId: int) -> None:
-        self.TweetsMap[userId].append((-self.timer,tweetId))
-        self.timer += 1
 
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.TweetMap[userId].append((-self.timer,tweetId))
+        self.timer += 1
+        
     def getNewsFeed(self, userId: int) -> List[int]:
-        max_heap = []
+        heap = []
         users = self.followerMap[userId] | {userId}
 
-        for u in users:
-            if self.TweetsMap[u]:
-                idx = len(self.TweetsMap[u]) - 1
-
-                time , tweetID  = self.TweetsMap[u][idx]
-                heappush(max_heap,(time,tweetID,u,idx))
+        for user in users:
+            if len(self.TweetMap[user]) > 0:
+                idx = len(self.TweetMap[user]) - 1
+                time , tweetID = self.TweetMap[user][idx]
+                heappush(heap, (time,tweetID,user,idx))
 
         result = []
 
-        while max_heap and len(result) < 10:
-            time,tweetID,UID,idx = heappop(max_heap)
-            result.append(tweetID)
+        while heap and len(result) < 10:
+            t,tweet_ID, u , ID = heappop(heap)
+            result.append(tweet_ID)
 
-            if idx - 1 >=0:
-                new_time , new_tweetID  = self.TweetsMap[UID][idx - 1]
-                heappush(max_heap,(new_time,new_tweetID,UID,idx - 1))
+            if ID - 1 >= 0:
+                new_time , new_tweetID = self.TweetMap[u][ID - 1]
+                heappush(heap, (new_time,new_tweetID,u,ID - 1))
         
         return result
 
     def follow(self, followerId: int, followeeId: int) -> None:
         self.followerMap[followerId].add(followeeId)
-
         
     def unfollow(self, followerId: int, followeeId: int) -> None:
-        if followeeId in self.followerMap[followerId]:
-            self.followerMap[followerId].remove(followeeId)
-        
+        self.followerMap[followerId].discard(followeeId)
 
 
 # Your Twitter object will be instantiated and called as such:
