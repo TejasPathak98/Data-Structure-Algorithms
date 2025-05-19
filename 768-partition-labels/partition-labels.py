0134d70e1331458e6ea81c4c3c7ba12e5e1b304f
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-05-18 18:07:52 -0700

    Time: 2 ms (93.83%) | Memory: 18 MB (15.71%) - LeetSync

--- 768-partition-labels/partition-labels.py
+++ 768-partition-labels/partition-labels.py
@@ -1,12 +1,14 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
         L = len(s)
-        last_occ_map = {s[i] : i for i in range(len(s))} #last occ map
 
-        ans = []
+        last_occ_map = {s[i] : i for i in range(len(s))}
+
         i = 0
+        ans = []
 
         while i < L:
+
             j = i
 
             end = last_occ_map[s[j]]
@@ -15,11 +17,9 @@ class Solution:
                 if last_occ_map[s[j]] > end:
                     end = last_occ_map[s[j]]
                 j += 1
-            
+
             ans.append(end - i + 1)
+
             i = end + 1
 
-        return ans 
-            
-
-
+        return ans
