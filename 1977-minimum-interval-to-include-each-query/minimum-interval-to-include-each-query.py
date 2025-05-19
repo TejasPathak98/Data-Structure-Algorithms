569ad6469a8b5d950cc06b021d18403376401d88
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-05-18 18:21:30 -0700

    Time: 380 ms (16.57%) | Memory: 60.8 MB (26.39%) - LeetSync

--- 1977-minimum-interval-to-include-each-query/minimum-interval-to-include-each-query.py
+++ 1977-minimum-interval-to-include-each-query/minimum-interval-to-include-each-query.py
@@ -1,25 +1,25 @@
 class Solution:
     def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
-        intervals.sort(key = lambda x : x[0])
+        intervals.sort()
         q = [(query,idx) for idx,query in enumerate(queries)]
-        q.sort(key = lambda x: x[0])
+        q.sort()
 
         i = 0
         result = [-1] * len(queries)
-        min_heap = []
+        heap = []
 
         for query,idx in q:
-            while i < len(intervals) and query >= intervals[i][0]:
-                heappush(min_heap,(intervals[i][1] - intervals[i][0] + 1,intervals[i][1]))
+            while i < len(intervals) and intervals[i][0] <= query:
+                heappush(heap, (intervals[i][1] - intervals[i][0] + 1 , intervals[i][1]))
                 i += 1
-            
-            while min_heap and min_heap[0][1] < query:
-                heappop(min_heap)
-            
-            if min_heap:
-                result[idx] = min_heap[0][0]
+
+            while heap and heap[0][1] < query:
+                heappop(heap)
+
+            if heap:
+                result[idx] = heap[0][0]
             else:
                 result[idx] = -1
-        
 
-        return result
+
+        return result 
