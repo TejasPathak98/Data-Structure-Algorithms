73ad36fa8f22ec41b9d899310fb8eac6a98bdc1e
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-05-22 13:49:57 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (20.96%) - LeetSync

--- 433-minimum-genetic-mutation/minimum-genetic-mutation.py
+++ 433-minimum-genetic-mutation/minimum-genetic-mutation.py
@@ -1,40 +1,47 @@
 class Solution:
     def minMutation(self, startGene: str, endGene: str, bank: List[str]) -> int:
-        queue = deque()
-        bank = set(bank)
-        queue.append(startGene)
+        #Bidirectional BFS
+
+        if endGene not in bank:
+            return -1
+        
+        startSet = set()
+        startSet.add(startGene)
+        endSet = set()
+        endSet.add(endGene)
+
         visited = set()
         visited.add(startGene)
+        gene_set = ['A','C','G','T']
 
         steps = 0
 
-        while queue:
+        while startSet and endSet:
 
-            for _ in range(len(queue)):
+            if len(startSet) > len(endSet):
+                startSet , endSet = endSet , startSet
 
-                gene = queue.popleft()
+            next_level = set()
+            
 
-                if gene == endGene:
-                    return steps
-                
-
-                for ch in ['A','C','G','T']:
-
-                    for i in range(len(gene)):
+            for gene in startSet:
+                for i in range(len(gene)):
+                    for ch in gene_set:
+                        if gene[i] == ch:
+                            continue
                         new_gene = gene[:i] + ch + gene[i + 1:]
 
+                        if new_gene in endSet:
+                            return steps + 1
                         if new_gene in bank and new_gene not in visited:
-
-                            queue.append(new_gene)
                             visited.add(new_gene)
+                            next_level.add(new_gene)
 
-            
+
+            startSet = next_level
             steps += 1
 
         
+
         return -1
 
-                
-
-
-        
