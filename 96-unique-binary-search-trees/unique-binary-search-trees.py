51400158b06d9d3343254e35ab037fda81af210d
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-05-24 15:25:21 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (79.17%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.py
+++ 96-unique-binary-search-trees/unique-binary-search-trees.py
@@ -7,10 +7,8 @@ class Solution:
         catalan[0] = 1
         catalan[1] = 1
 
-        for i in range(2,n +1):
+        for i in range(2,n + 1):
             for j in range(i):
-                catalan[i] += catalan[j]*catalan[i - j - 1]
-        
-        return catalan[n]
+                catalan[i] += catalan[j] * catalan[i - j - 1]
 
-        
+        return catalan[n]
