d44e2726e2a0b886ad61cc8eb4b83b2bdcc2ae40
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-06-01 20:19:13 -0700

    Time: 0 ms (100.00%) | Memory: 18.2 MB (15.08%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,21 +1,18 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
-
         result = []
 
         def backtracking(index):
             if index == n:
                 result.append(nums[:])
                 return
-            
-            for j in range(index,len(nums)):
-                nums[j] , nums[index] = nums[index] , nums[j]
+
+            for j in range(index,n):
+                nums[index] , nums[j] = nums[j] , nums[index]
                 backtracking(index + 1)
-                nums[j] , nums[index] = nums[index] , nums[j]
+                nums[index] , nums[j] = nums[j] , nums[index]
 
-        
         backtracking(0)
+
         return result
-
-
