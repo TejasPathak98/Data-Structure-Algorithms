47ffc54b14743587a98a7c50ad0679b5c528c78e
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-06-01 20:49:02 -0700

    Time: 2874 ms (90.36%) | Memory: 18 MB (20.99%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -3,25 +3,25 @@ class Solution:
         m = len(board)
         n = len(board[0])
 
-        def dfs(x,y,pos):
-            if pos == len(word) - 1:
+    
+        def dfs(x,y,word_pos):
+            if word_pos == len(word) - 1:
                 return True
 
-            letter = board[x][y]            
+            ch = board[x][y]
+
             board[x][y] = "#"
 
-            for dx,dy in [(0,1),(0,-1),(1,0),(-1,0)]:
+            for dx,dy in [(0,1),(1,0),(0,-1),(-1,0)]:
                 nx = x + dx
                 ny = y + dy
 
-                if 0 <= nx < m and 0 <= ny < n and board[nx][ny] == word[pos + 1]:
-                    if dfs(nx,ny,pos + 1):
+                if 0 <= nx < m and 0 <= ny < n and board[nx][ny] == word[word_pos + 1]:
+                    if dfs(nx,ny,word_pos + 1):
                         return True
-
-
-            board[x][y] = letter
+            
+            board[x][y] = ch
             return False
-
 
         for i in range(m):
             for j in range(n):
@@ -31,3 +31,4 @@ class Solution:
 
         
         return False
+
