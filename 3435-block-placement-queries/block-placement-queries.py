8b39380c43cd657617b6da729d128769fc105a4e
Tejas Vijay Pathak <40304370+TejasPathak98@users.noreply.github.com>
2025-06-05 22:01:02 -0700

    Time: 3075 ms (49.19%) | Memory: 77.6 MB (67.13%) - LeetSync

--- 3435-block-placement-queries/block-placement-queries.py
+++ 3435-block-placement-queries/block-placement-queries.py
@@ -1,67 +1,78 @@
-from typing import List
 from sortedcontainers import SortedList
 
 class Solution:
     def getResults(self, queries: List[List[int]]) -> List[bool]:
-        # Sorted list of all obstacles (initially with a virtual obstacle at position 0)
-        axis = SortedList()
-        max_x = max(q[1] for q in queries)  # Maximum x value to size the segment tree
-        gaps = SegTree(max_x)  # Segment tree to store max gap between consecutive obstacles
         
-        axis.add(0)        # Virtual start obstacle at position 0
-        gaps.update(0, 0)  # Gap at position 0 is zero
-        res = []
+        obstacles = SortedList()
+        result = []
+        max_x = max(q[1] for q in queries)
+        Segment_Tree = SegTree(max_x)
+
+        obstacles.add(0)
+        Segment_Tree.update(0,0)
 
         for q in queries:
             if q[0] == 1:
-                # Query type 1: Place an obstacle at position q[1]
-                x = q[1]
-                idx = axis.bisect(x)  # Position where x should be inserted
 
-                # Update the interval to the next obstacle if it exists
-                if idx < len(axis):
-                    nxt = axis[idx]
-                    gaps.update(nxt, nxt - x)
+                obstacle_value = q[1]
+                position_in_obstacles = obstacles.bisect(obstacle_value)
 
-                # Update the interval from the previous obstacle to x
-                prev = axis[idx - 1]
-                gaps.update(x, x - prev)
+                if position_in_obstacles < len(obstacles):
+                    nxt = obstacles[position_in_obstacles]
+                    Segment_Tree.update(nxt,nxt - obstacle_value)
+                
+                prev = obstacles[position_in_obstacles - 1]
+                Segment_Tree.update(obstacle_value,obstacle_value - prev)
 
-                axis.add(x)  # Add the new obstacle
+                obstacles.add(obstacle_value)
             else:
-                # Query type 2: Can we place a block of size sz in range [0, x]?
-                x, sz = q[1], q[2]
-                idx = axis.bisect(x)
-                prev = axis[idx - 1]  # Last obstacle before or at x
+                x = q[1]
+                sz = q[2]
+                max_gap = 0
 
-                # Max gap is either the gap from prev to x or the max known gap before prev
-                max_gap = max(x - prev, gaps.query(prev))
+                if sz > x:
+                    result.append(False)
+                    continue
 
-                # If block size ≤ max_gap, it can be placed
-                res.append(sz <= max_gap)
+                position_in_obstacles = obstacles.bisect(x)
+                prev = obstacles[position_in_obstacles - 1]
 
-        return res
+                max_gap = max(x - prev,Segment_Tree.query(prev))
 
+                result.append(max_gap >= sz)
+        
+        return result
+
+    
 class SegTree:
-    def __init__(self, n: int):
-        # Round up to the next power of 2 for full binary tree representation
-        self.n = 1 << (n.bit_length())
-        self.tree = [0] * (2 * self.n)
 
-    def update(self, ind: int, val: int):
-        # Set value at leaf node and update its parents
-        ind += self.n
-        self.tree[ind] = val
-        while ind > 1:
-            ind //= 2
-            self.tree[ind] = max(self.tree[ind * 2], self.tree[ind * 2 + 1])
+    def __init__(self,size):
+        self.n = 1 << (size.bit_length())
+        self.tree = [0] * (2*self.n)
 
-    def query(self, ind: int) -> int:
-        # Get max in range [0, ind]
-        ind += self.n
-        res = self.tree[ind]
-        while ind > 1:
-            if ind % 2 == 1:
-                res = max(res, self.tree[ind - 1])
-            ind //= 2
+    def update(self,index,value):
+        index += self.n
+        self.tree[index] = value
+
+        while index > 1:
+            index //= 2
+            self.tree[index] = max(self.tree[index*2],self.tree[index*2 + 1])
+    
+
+    def query(self,index):
+        index += self.n
+        res = self.tree[index]
+
+        while index > 1:
+            if index % 2 == 1:
+                res = max(res,self.tree[index - 1])
+            index //= 2
+        
         return res
+        
+
+#The segment tree is mapped to 0.....max_x (the whole set of possible values so we pass the obstacle value itseld and not hte index)
+
+
+
+                
